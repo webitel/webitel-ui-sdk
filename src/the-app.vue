49b7e0f9c87c3b5894b95e4bc4e5879801a34052
Daniil Lohvinov <nsblnr@gmail.com>
2020-07-13 11:51:10 +0300

    dev: init project, added typography [WTEL-1347]

---
+++ src/the-app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <playground/>
+  </div>
+</template>
+
+<script>
+  import Playground from './playground.vue';
+
+  export default {
+    name: 'the-app',
+    components: {
+      Playground,
+    },
+  };
+</script>
+
+<style lang="scss">
+</style>
