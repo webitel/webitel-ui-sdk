8212c93435087c300e98a2418eeb5d8b9a9f4ab6
Daniil Lohvinov <nsblnr@gmail.com>
2020-07-14 18:08:35 +0300

    des: added notification component [WTEL-1354]

---
+++ src/components/molecules/wt-notification/wt-notification.vue
@@ -0,0 +1,74 @@
+<template>
+  <article
+    class="wt-notification"
+    @click="$emit('close')"
+  >
+    <wt-icon
+      class="wt-notification__icon"
+      :icon="notificationIcon"
+      :color="notificationIconColor"
+    ></wt-icon>
+
+    <p class="wt-notification__text">
+      {{notification.text}}
+    </p>
+  </article>
+</template>
+
+<script>
+
+  export default {
+    name: 'wt-notification',
+    props: {
+      notification: {
+        type: Object,
+        required: true,
+      },
+    },
+    computed: {
+      notificationIcon() {
+        switch (this.notification.type) {
+          case 'info': return 'rounded_tick';
+          case 'error': return 'attention';
+          default: return '';
+        }
+      },
+      notificationIconColor() {
+        switch (this.notification.type) {
+          case 'info': return 'true';
+          case 'error': return 'false';
+          default: return '';
+        }
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .wt-notification {
+    @extend %typo-body-lg;
+    @include width-fit-content;
+
+    position: relative;
+    display: flex;
+    align-items: flex-start;
+    min-width: 30vw;
+    max-width: 40vw;
+    padding: 10px;
+    color: var(--notification-text-color);
+    background: var(--notification-bg-color);
+    border-radius: var(--border-radius);
+    box-shadow: var(--box-shadow);
+    cursor: pointer;
+  }
+
+  .wt-notification__icon {
+    margin-right: 10px;
+  }
+
+  .wt-notification__text {
+    flex-grow: 1;
+    align-self: center;
+    margin: 0;
+  }
+</style>
