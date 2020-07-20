af5cad2f74c655e96fb1d7c3e1cae6de5bea499c
Daniil Lohvinov <nsblnr@gmail.com>
2020-07-20 11:34:58 +0300

    dev: added dropdown reset [WTEL-1354]

--- src/components/molecules/wt-select/wt-select.vue
+++ src/components/molecules/wt-select/wt-select.vue
@@ -4,11 +4,10 @@
     :class="{'wt-select--disabled': disabled}"
   >
     <wt-label class="wt-select__label">{{label}}</wt-label>
-    <!--    <div class="hs-multiselect-wrap">-->
     <vue-multiselect
       class="wt-select__select"
       :value="value"
-      :options="options"
+      :options="selectOptions"
       :placeholder="placeholder || label"
       :multiple="multiple"
       :close-on-select="closeOnSelect"
@@ -23,7 +22,6 @@
       :searchable="searchable"
       @input="input"
       @search-change="fetch"
-      @open="isOpened = true"
       @close="close"
     >
       <template slot="caret">
@@ -33,16 +31,16 @@
       <template slot="tag" slot-scope="{ option }">
         <span class="multiselect__custom-tag">{{option[optionLabel]}}</span>
       </template>
-      <!--      <template slot="option" slot-scope="{ option }">-->
-      <!--        <div class="multiselect__option__content">-->
-      <!--          <span>{{option.name || option }}</span>-->
-      <!--          <icon class="multiselect__option__tick">-->
-      <!--            <svg class="icon icon-tick-sm sm">-->
-      <!--              <use xlink:href="#icon-tick-sm"></use>-->
-      <!--            </svg>-->
-      <!--          </icon>-->
-      <!--        </div>-->
-      <!--      </template>-->
+
+      <template slot="clear" slot-scope="{}">
+        <wt-icon-btn
+          class="wt-select__clear"
+          :class="{ 'hidden': !isValue }"
+          icon="remove-rounded"
+          size="sm"
+          @click.native="clearValue"
+        ></wt-icon-btn>
+      </template>
     </vue-multiselect>
     <!--    <validation-message-->
     <!--      class="cc-err-message"-->
@@ -104,12 +102,7 @@
         default: true,
       },
 
-      apiMode: {
-        type: Boolean,
-        default: false,
-      },
-
-      fetchMethod: {
+      search: {
         type: Function,
       },
 
@@ -133,14 +126,13 @@
         default: false,
       },
 
-      v: {
-        type: Object,
-      },
+      // v: {
+      //   type: Object,
+      // },
     },
 
     data: () => ({
       isLoading: false,
-      isOpened: false,
       fetchedOptions: [],
     }),
 
@@ -150,43 +142,50 @@
     },
 
     computed: {
-      opts() {
-        if (this.apiMode) {
+      selectOptions() {
+        if (!this.internalSearch) {
           return this.fetchedOptions;
         }
         return this.options;
       },
 
-      validation: {
-        get() {
-          return this.value;
-        },
-        set(value) {
-          if (this.v) this.v.$touch();
-          this.$emit('input', value);
-        },
+      isValue() {
+        if (Array.isArray(this.value)) return !!this.value.length;
+        if (typeof this.value === 'object') return !!Object.keys(this.value).length;
+        return !!this.value;
       },
+
+      // validation: {
+      //   get() {
+      //     return this.value;
+      //   },
+      //   set(value) {
+      //     if (this.v) this.v.$touch();
+      //     this.$emit('input', value);
+      //   },
+      // },
     },
 
     methods: {
       limitText: (count) => `${count}`,
 
       async fetch(search) {
-        if (this.apiMode) {
-          const response = await this.fetchMethod(search);
+        if (!this.internalSearch) {
+          const response = await this.search(search);
           this.fetchedOptions = response.items;
         }
       },
 
+      clearValue() {
+        this.input('');
+      },
+
       input(value) {
-        if (value) {
-          this.$emit('input', value);
-        }
+        this.$emit('input', value);
       },
 
       close() {
         this.$emit('closed');
-        this.isOpened = false;
       },
     },
   };
@@ -216,6 +215,15 @@
       transform: rotate(0);
       transition: var(--transition);
       z-index: var(--select-caret-z-index);
+    }
+
+    ::v-deep .wt-select__clear {
+      position: absolute;
+      top: var(--select-clear-top-pos);
+      right: var(--select-clear-right-pos);
+      transform: rotate(0);
+      transition: var(--transition);
+      z-index: var(--select-clear-z-index);
     }
 
     ::v-deep {
@@ -305,6 +313,10 @@
         fill: var(--icon--hover-color);
       }
 
+      ::v-deep .wt-select__clear .wt-icon__icon {
+        fill: var(--icon--hover-color);
+      }
+
       ::v-deep {
         .multiselect__tags {
           border-color: var(--form-border--hover-color);
@@ -350,7 +362,8 @@
        opacity: 1;
     }
 
-    .wt-select__arrow-caret ::v-deep .wt-icon__icon {
+    .wt-select__arrow-caret ::v-deep .wt-icon__icon,
+    .wt-select__clear ::v-deep .wt-icon__icon {
       fill: var(--icon--disabled-color);
     }
 
@@ -371,178 +384,4 @@
       }
     }
   }
-
-  /*
-  @import '../../../css/utils/variables';
-
-  $label-color: #000;
-  $list-option__hover: transparent;
-
-  .hs-multiselect .cc-label {
-    margin-bottom: (10px);
-  }
-
-  .hs-multiselect-wrap {
-    position: relative;
-    width: 100%;
-    height: (40px);
-  }
-
-  .hs-multiselect__arrow-down {
-    position: absolute;
-    top: 50%;
-    right: (3px);
-    transform: translateY(-50%);
-    pointer-events: none;
-
-    .icon {
-      fill: #000;
-      stroke: #000
-    }
-  }
-
-  .multiselect {
-    position: absolute;
-    width: 100%;
-
-    border: 1px solid $input-border-color;
-    border-radius: $border-radius;
-    background: #fff;
-
-    cursor: pointer;
-    transition: $transition;
-    box-sizing: border-box;
-
-    &.opened {
-      z-index: 1;
-    }
-
-    &:hover, &.opened {
-      border-color: #000;
-    }
-
-    // visible area
-    &__tags {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      height: (38px); // 40px - 2px outer borders
-      padding: $select-paddings;
-      box-sizing: border-box;
-
-      .multiselect__strong {
-        // @extend .count-badge;
-        font-weight: normal;
-      }
-
-      .multiselect__placeholder {
-        @extend .typo-input;
-        color: $label-color;
-      }
-
-      .multiselect__input {
-        @extend .typo-input;
-        outline: none;
-        border: none;
-      }
-
-      .multiselect__single {
-        @extend .typo-input;
-        display: inline-block;
-      }
-
-      .multiselect__tag {
-        @extend .typo-input;
-      }
-    }
-
-    // options
-    &__content-wrapper {
-      @extend .cc-scrollbar;
-      overflow: auto;
-    }
-
-    &__content {
-      width: 100%;
-    }
-
-    // empty/not found texts
-    li:not(.multiselect__element) {
-      @extend .typo-input;
-      padding: $select-paddings;
-    }
-
-    &__element {
-      @extend .typo-input;
-      width: 100%;
-      transition: $transition;
-      cursor: pointer;
-
-      .multiselect__option {
-        display: block;
-        border-radius: $border-radius;
-
-        &:hover {
-          background: $list-option__hover;
-        }
-
-        &__content {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          padding: $select-paddings;
-          padding-right: (8px);
-          box-sizing: border-box;
-        }
-
-        .multiselect__option__tick .icon {
-          fill: transparent;
-          stroke: transparent;
-        }
-
-        &:hover .multiselect__option__tick .icon {
-          fill: #000;
-          stroke: #000;
-        }
-
-        &.multiselect__option--selected
-        .multiselect__option__tick .icon {
-          fill: $true-color;
-          stroke: $true-color;
-        }
-      }
-    }
-
-    &:not(.multiselect--active) {
-      .multiselect__input {
-        display: none;
-      }
-    }
-
-    &--active {
-      .multiselect__tags-wrap,
-      .multiselect__strong {
-        display: none !important;
-      }
-    }
-  }
-
-  .hs-multiselect.disabled {
-    .hs-multiselect__arrow-down {
-      display: none;
-    }
-
-    .multiselect {
-      border: none;
-      outline: none;
-      cursor: text;
-    }
-  }
-
-  .text-center {
-    .multiselect__single {
-      margin: auto;
-    }
-  }
-  */
 </style>
