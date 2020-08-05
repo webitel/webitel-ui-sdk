9bbdbf1595009d9aab6b1e8bccdaf88dedbf16cb
dlohvinov <nsblnr@gmail.com>
2020-08-05 10:22:54 +0300

    fixed docs build version

--- docs/src/plugins/webitel-ui.js
+++ docs/src/plugins/webitel-ui.js
@@ -1,10 +1,18 @@
 import Vue from 'vue';
-import WebitelUI from '../../../dist/ui-sdk.common';
-import WebitelUIEn from '../../../src/locale/en/en';
-import WebitelUIRu from '../../../src/locale/ru/ru';
+
+import WebitelUI from '@webitel/ui-sdk/dist/ui-sdk.common';
+import WebitelUIEn from '@webitel/ui-sdk/src/locale/en/en';
+import WebitelUIRu from '@webitel/ui-sdk/src/locale/ru/ru';
+import '@webitel/ui-sdk/dist/ui-sdk.css';
+import '@webitel/ui-sdk/src/css/main.scss';
+
+// import WebitelUI from '../../../dist/ui-sdk.common';
+// import WebitelUIEn from '../../../src/locale/en/en';
+// import WebitelUIRu from '../../../src/locale/ru/ru';
+// import '../../../dist/ui-sdk.css';
+// import '../../../src/css/main.scss';
+
 import i18n from '../locale/i18n';
-import '../../../dist/ui-sdk.css';
-import '../../../src/css/main.scss';
 import eventBus from '../../../src/scripts/eventBus';
 
 Vue.use(WebitelUI, { eventBus });
