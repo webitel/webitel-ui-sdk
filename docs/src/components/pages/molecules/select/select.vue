2169b215bd1ee277f6e6562bb856bba58f380ee2
dlohvinov <nsblnr@gmail.com>
2020-08-05 14:46:57 +0300

    dev: updated wt-select: now reset event emits the same data type, as value

--- docs/src/components/pages/molecules/select/select.vue
+++ docs/src/components/pages/molecules/select/select.vue
@@ -74,149 +74,152 @@
 </template>
 
 <script>
-  import Prism from 'prismjs';
+import Prism from 'prismjs';
 
-  export default {
-    name: 'select-page',
-    data: () => ({
-      vValid: {
-        $error: false,
-        $dirty: true,
-      },
-      vInvalid: {
-        $error: true,
-        $dirty: true,
-        required: false,
-      },
-      value: '',
-      multipleValue: [],
-      options: [
-        { name: 'Vue.js1', language: 'JavaScript' },
-        { name: 'Adonis2', language: 'JavaScript' },
-        { name: 'Rails3', language: 'Ruby' },
-        { name: 'Sinatra4', language: 'Ruby' },
-        { name: 'Laravel5', language: 'PHP' },
-        { name: 'Phoenix6', language: 'Elixir' },
-        { name: 'Vue.js7', language: 'JavaScript' },
-        { name: 'Adonis8', language: 'JavaScript' },
-        { name: 'Rails9', language: 'Ruby' },
-        { name: 'Sinatra10', language: 'Ruby' },
-        { name: 'Laravel11', language: 'PHP' },
-        { name: 'Phoenix12', language: 'Elixir' },
-      ],
-      properties: [
-        {
-          value: 'type',
-          code: '<wt-select :value="value"></wt-select>',
-          type: 'any',
-        },
-        {
-          value: 'options',
-          code: '<wt-select :options="options"></wt-select>',
-          type: 'Array',
-          default: '[]',
-        },
-        {
-          value: 'label',
-          code: '<wt-input label="Label"></wt-input>',
-          type: 'String',
-          default: '',
-        },
-        {
-          value: 'placeholder',
-          code: '<wt-input placeholder="placeholder"></wt-input>',
-          type: 'String',
-          default: 'label value',
-        },
-        {
-          value: 'trackBy',
-          code: '<wt-input trackBy="id"></wt-input>',
-          type: 'String',
-          default: 'id',
-          description: 'Used to compare objects. Only use if options are objects.',
-        },
-        {
-          value: 'multiple',
-          code: '<wt-input multiple></wt-input>',
-          type: 'Boolean',
-          default: 'false',
-          description: 'Make multiselect',
-        },
-        {
-          value: 'closeOnSelect',
-          code: '<wt-input close-on-select></wt-input>',
-          type: 'Boolean',
-          default: 'true',
-        },
-        {
-          value: 'optionLabel',
-          code: '<wt-input track-by="id"></wt-input>',
-          type: 'String',
-          default: 'id',
-          description: 'Used to compare objects. Only use if options are objects.',
-        },
-        {
-          value: 'internalSearch',
-          code: '<wt-input internal-search></wt-input>',
-          type: 'Boolean',
-          default: 'true',
-          description: 'Decide whether to filter the results internally based on search query. Useful for async filtering, where we search through more complex data.',
-        },
-        {
-          value: 'search',
-          code: '<wt-input search="() => {}"></wt-input>',
-          type: 'Function',
-          description: 'External search api function',
-        },
-        {
-          value: 'disabled',
-          code: '<wt-input disabled></wt-input>',
-          type: 'Boolean',
-          default: 'false',
-        },
-        {
-          value: 'searchable',
-          code: '<wt-input searchable></wt-input>',
-          type: 'Boolean',
-          default: 'true',
-          description: 'Add / removes search input',
-        },
-        {
-          value: 'allowEmpty',
-          code: '<wt-input allow-empty></wt-input>',
-          type: 'Boolean',
-          default: 'false',
-          description: 'Allows to remove all selected values. Otherwise one must be left selected.',
-        },
-      ],
-      events: [
-        {
-          value: 'input',
-          params: [
-            { name: 'value', type: ['String', 'Array'] },
-          ],
-        },
-        {
-          value: 'reset',
-          description: 'Emitted on "reset" icon click, along with "input" (input is emitted first) ' +
-            'event with empty value',
-        },
-        {
-          value: 'closed',
-        },
-      ],
-      slots: [
-        {
-          name: 'label',
-          scope: [{ name: 'label' }],
-          description: 'Custom label slot',
-        },
-      ],
-    }),
-    mounted() {
-      Prism.highlightAll();
+export default {
+  name: 'select-page',
+  data: () => ({
+    vValid: {
+      $error: false,
+      $dirty: true,
     },
-  };
+    vInvalid: {
+      $error: true,
+      $dirty: true,
+      required: false,
+    },
+    value: '',
+    multipleValue: [],
+    options: [
+      { name: 'Vue.js1', language: 'JavaScript' },
+      { name: 'Adonis2', language: 'JavaScript' },
+      { name: 'Rails3', language: 'Ruby' },
+      { name: 'Sinatra4', language: 'Ruby' },
+      { name: 'Laravel5', language: 'PHP' },
+      { name: 'Phoenix6', language: 'Elixir' },
+      { name: 'Vue.js7', language: 'JavaScript' },
+      { name: 'Adonis8', language: 'JavaScript' },
+      { name: 'Rails9', language: 'Ruby' },
+      { name: 'Sinatra10', language: 'Ruby' },
+      { name: 'Laravel11', language: 'PHP' },
+      { name: 'Phoenix12', language: 'Elixir' },
+    ],
+    properties: [
+      {
+        value: 'type',
+        code: '<wt-select :value="value"></wt-select>',
+        type: 'any',
+      },
+      {
+        value: 'options',
+        code: '<wt-select :options="options"></wt-select>',
+        type: 'Array',
+        default: '[]',
+      },
+      {
+        value: 'label',
+        code: '<wt-input label="Label"></wt-input>',
+        type: 'String',
+        default: '',
+      },
+      {
+        value: 'placeholder',
+        code: '<wt-input placeholder="placeholder"></wt-input>',
+        type: 'String',
+        default: 'label value',
+      },
+      {
+        value: 'trackBy',
+        code: '<wt-input trackBy="id"></wt-input>',
+        type: 'String',
+        default: 'id',
+        description: 'Used to compare objects. Only use if options are objects.',
+      },
+      {
+        value: 'multiple',
+        code: '<wt-input multiple></wt-input>',
+        type: 'Boolean',
+        default: 'false',
+        description: 'Make multiselect',
+      },
+      {
+        value: 'closeOnSelect',
+        code: '<wt-input close-on-select></wt-input>',
+        type: 'Boolean',
+        default: 'true',
+      },
+      {
+        value: 'optionLabel',
+        code: '<wt-input track-by="id"></wt-input>',
+        type: 'String',
+        default: 'id',
+        description: 'Used to compare objects. Only use if options are objects.',
+      },
+      {
+        value: 'internalSearch',
+        code: '<wt-input internal-search></wt-input>',
+        type: 'Boolean',
+        default: 'true',
+        description: 'Decide whether to filter the results internally based on search query. Useful for async filtering, where we search through more complex data.',
+      },
+      {
+        value: 'search',
+        code: '<wt-input search="() => {}"></wt-input>',
+        type: 'Function',
+        description: 'External search api function',
+      },
+      {
+        value: 'disabled',
+        code: '<wt-input disabled></wt-input>',
+        type: 'Boolean',
+        default: 'false',
+      },
+      {
+        value: 'searchable',
+        code: '<wt-input searchable></wt-input>',
+        type: 'Boolean',
+        default: 'true',
+        description: 'Add / removes search input',
+      },
+      {
+        value: 'allowEmpty',
+        code: '<wt-input allow-empty></wt-input>',
+        type: 'Boolean',
+        default: 'false',
+        description: 'Allows to remove all selected values. Otherwise one must be left selected.',
+      },
+    ],
+    events: [
+      {
+        value: 'input',
+        params: [
+          { name: 'value', type: ['String', 'Array'] },
+        ],
+      },
+      {
+        value: 'reset',
+        params: [
+          { name: 'value', type: "['String', 'Array', 'Object'] -- depends on value prop datatype" },
+        ],
+        description: 'Emitted on "reset" icon click, along with "input" (input is emitted first) '
+          + 'event with empty value',
+      },
+      {
+        value: 'closed',
+      },
+    ],
+    slots: [
+      {
+        name: 'label',
+        scope: [{ name: 'label' }],
+        description: 'Custom label slot',
+      },
+    ],
+  }),
+  mounted() {
+    Prism.highlightAll();
+  },
+};
 </script>
 
 <style lang="scss" scoped>
--- src/components/molecules/wt-select/wt-select.vue
+++ src/components/molecules/wt-select/wt-select.vue
@@ -40,7 +40,7 @@
       </template>
 
       <template slot="tag" slot-scope="{ option }">
-        <span class="multiselect__custom-tag">{{option[optionLabel]}}</span>
+        <span class="multiselect__custom-tag">{{ option[optionLabel] }}</span>
       </template>
 
       <template slot="clear" slot-scope="{}">
@@ -56,361 +56,365 @@
     <wt-input-info
       v-if="isValidation"
       :invalid="invalid"
-    >{{validationText}}
+    >{{ validationText }}
     </wt-input-info>
   </div>
 </template>
 
 <script>
-  import VueMultiselect from 'vue-multiselect';
-  import debounce from '../../../scripts/debounce';
-  import validationMixin from '../../../mixins/validationMixin/validationMixin';
+import VueMultiselect from 'vue-multiselect';
+import debounce from '../../../scripts/debounce';
+import validationMixin from '../../../mixins/validationMixin/validationMixin';
 
-  export default {
-    name: 'wt-select',
-    components: {
-      VueMultiselect,
-    },
-    mixins: [validationMixin],
-    props: {
-      value: {},
+export default {
+  name: 'wt-select',
+  components: {
+    VueMultiselect,
+  },
+  mixins: [validationMixin],
+  props: {
+    value: {},
 
-      options: {
-        type: Array,
-        default: () => [],
-      },
-
-      label: {
-        type: String,
-        default: '',
-      },
-
-      placeholder: {
-        type: String,
-        default: '',
-      },
-
-      trackBy: {
-        type: String,
-        default: 'id',
-      },
-
-      multiple: {
-        type: Boolean,
-        default: false,
-      },
-
-      closeOnSelect: {
-        type: Boolean,
-        default: true,
-      },
-
-      optionLabel: {
-        type: String,
-        default: 'name',
-      },
-
-      internalSearch: {
-        type: Boolean,
-        default: true,
-      },
-
-      search: {
-        type: Function,
-      },
-
-      disabled: {
-        type: Boolean,
-        default: false,
-      },
-
-      searchable: {
-        type: Boolean,
-        default: true,
-      },
-
-      allowEmpty: {
-        type: Boolean,
-        default: true,
-      },
+    options: {
+      type: Array,
+      default: () => [],
     },
 
-    data: () => ({
-      isLoading: false,
-      fetchedOptions: [],
-    }),
-
-    created() {
-      this.fetch();
-      this.fetch = debounce(this.fetch);
+    label: {
+      type: String,
+      default: '',
     },
 
-    computed: {
-        hasLabel() {
-            return !!(this.label || this.$slots.label);
-        },
-
-      selectOptions() {
-        if (!this.internalSearch) {
-          return this.fetchedOptions;
-        }
-        return this.options;
-      },
-
-      isValue() {
-        if (Array.isArray(this.value)) return !!this.value.length;
-        if (typeof this.value === 'object') return !!Object.keys(this.value).length;
-        return !!this.value;
-      },
+    placeholder: {
+      type: String,
+      default: '',
     },
 
-    methods: {
-      limitText: (count) => `${count}`,
-
-      async fetch(search) {
-        if (!this.internalSearch) {
-          this.fetchedOptions = await this.search(search);
-        }
-      },
-
-      clearValue() {
-        this.input('');
-        this.$emit('reset', '');
-      },
-
-      input(value) {
-        this.$emit('input', value);
-      },
-
-      close() {
-        this.$emit('closed');
-      },
+    trackBy: {
+      type: String,
+      default: 'id',
     },
-  };
+
+    multiple: {
+      type: Boolean,
+      default: false,
+    },
+
+    closeOnSelect: {
+      type: Boolean,
+      default: true,
+    },
+
+    optionLabel: {
+      type: String,
+      default: 'name',
+    },
+
+    internalSearch: {
+      type: Boolean,
+      default: true,
+    },
+
+    search: {
+      type: Function,
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+
+    searchable: {
+      type: Boolean,
+      default: true,
+    },
+
+    allowEmpty: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  data: () => ({
+    isLoading: false,
+    fetchedOptions: [],
+  }),
+
+  created() {
+    this.fetch();
+    this.fetch = debounce(this.fetch);
+  },
+
+  computed: {
+    hasLabel() {
+      return !!(this.label || this.$slots.label);
+    },
+
+    selectOptions() {
+      if (!this.internalSearch) {
+        return this.fetchedOptions;
+      }
+      return this.options;
+    },
+
+    isValue() {
+      if (Array.isArray(this.value)) return !!this.value.length;
+      if (typeof this.value === 'object') return !!Object.keys(this.value).length;
+      return !!this.value;
+    },
+  },
+
+  methods: {
+    limitText: (count) => `${count}`,
+
+    async fetch(search) {
+      if (!this.internalSearch) {
+        this.fetchedOptions = await this.search(search);
+      }
+    },
+
+    clearValue() {
+      let value = '';
+      if (Array.isArray(this.value)) value = [];
+      else if (typeof this.value === 'object') value = {};
+      this.input(value);
+      this.$emit('reset', value);
+    },
+
+    input(value) {
+      this.$emit('input', value);
+    },
+
+    close() {
+      this.$emit('closed');
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  @import '~vue-multiselect/dist/vue-multiselect.min.css';
-  @import '../../../css/components/atoms/wt-badge/wt-badge';
+@import '~vue-multiselect/dist/vue-multiselect.min.css';
+@import '../../../css/components/atoms/wt-badge/wt-badge';
 
-  .wt-select {
-      width: 100%;
+.wt-select {
+  width: 100%;
+}
+
+.wt-label {
+  margin-bottom: var(--label-margin);
+  cursor: text;
+
+  .wt-select:hover &,
+  .wt-select:focus-within & {
+    color: var(--form-label--hover-color);
+  }
+}
+
+.multiselect {
+  cursor: pointer;
+
+  .wt-select__arrow-caret {
+    position: absolute;
+    top: var(--select-caret-top-pos);
+    right: var(--select-caret-right-pos);
+    transform: rotate(0);
+    transition: var(--transition);
+    z-index: var(--select-caret-z-index);
   }
 
-  .wt-label {
-    margin-bottom: var(--label-margin);
-    cursor: text;
+  ::v-deep .wt-select__clear {
+    position: absolute;
+    top: var(--select-clear-top-pos);
+    right: var(--select-clear-right-pos);
+    transform: rotate(0);
+    transition: var(--transition);
+    z-index: var(--select-clear-z-index);
+  }
 
-    .wt-select:hover &,
-    .wt-select:focus-within & {
-      color: var(--form-label--hover-color);
+  ::v-deep {
+    .multiselect__tags {
+      display: flex;
+      align-items: center;
+      min-height: var(--select-tags-min-height); // reset original 20px style
+      box-sizing: border-box;
+      padding: var(--select-tags-padding);
+      color: var(--form-input-color);
+      border: var(--select-tags-border);
+      border-color: var(--form-border-color);
+      border-radius: var(--border-radius);
+
+      .multiselect__input,
+      .multiselect__single {
+        @extend %typo-body-lg;
+        min-height: var(--select-input-min-height); // reset original 20px style
+        padding: 0;
+        margin: 0;
+        background: transparent;
+      }
+
+      .multiselect__strong {
+        @extend %typo-body-lg;
+        @extend .wt-badge;
+        position: absolute;
+        right: var(--select-badge-right-pos);
+        min-height: var(--select-input-min-height); // reset original 20px style
+        margin: 0;
+        font-weight: normal;
+        z-index: var(--select-badge-z-index);
+
+        &:before {
+          content: '+';
+        }
+      }
+
+      .multiselect__tags-wrap {
+        width: 100%;
+      }
+
+      .multiselect__custom-tag {
+        @extend %typo-body-lg;
+        color: var(--form-input-color);
+
+        // text overflow 3 dots
+        display: block;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+
+      .multiselect__placeholder {
+        padding: 0;
+        margin: 0;
+        color: var(--form-placeholder-color);
+
+        // text overflow 3 dots
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+
+    .multiselect__content-wrapper {
+      @extend %wt-distant-scrollbar;
+      border: var(--select-tags-border);
+      border-color: var(--form-border-color);
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+
+      .multiselect__option {
+        padding: var(--select-options-padding);
+
+        & > span {
+          @extend %typo-body-lg;
+          color: var(--form-placeholder-color);
+        }
+
+        &:after {
+          display: none;
+        }
+
+        &--highlight {
+          background: var(--select-option--hover-color);
+
+          & > span {
+            color: var(--form-input-color);
+          }
+        }
+
+        &--selected {
+          font-weight: normal;
+          background: var(--select-option--active-color)
+        }
+      }
     }
   }
 
-  .multiselect {
-    cursor: pointer;
-
-    .wt-select__arrow-caret {
-      position: absolute;
-      top: var(--select-caret-top-pos);
-      right: var(--select-caret-right-pos);
-      transform: rotate(0);
-      transition: var(--transition);
-      z-index: var(--select-caret-z-index);
+  .wt-select:hover & {
+    .wt-select__arrow-caret ::v-deep .wt-icon__icon {
+      fill: var(--icon--hover-color);
     }
 
-    ::v-deep .wt-select__clear {
-      position: absolute;
-      top: var(--select-clear-top-pos);
-      right: var(--select-clear-right-pos);
-      transform: rotate(0);
-      transition: var(--transition);
-      z-index: var(--select-clear-z-index);
+    ::v-deep .wt-select__clear .wt-icon__icon {
+      fill: var(--icon--hover-color);
     }
 
     ::v-deep {
       .multiselect__tags {
-        display: flex;
-        align-items: center;
-        min-height: var(--select-tags-min-height); // reset original 20px style
-        box-sizing: border-box;
-        padding: var(--select-tags-padding);
-        color: var(--form-input-color);
-        border: var(--select-tags-border);
-        border-color: var(--form-border-color);
-        border-radius: var(--border-radius);
+        border-color: var(--form-border--hover-color);
+      }
 
-        .multiselect__input,
-        .multiselect__single {
-          @extend %typo-body-lg;
-          min-height: var(--select-input-min-height); // reset original 20px style
-          padding: 0;
-          margin: 0;
-          background: transparent;
-        }
+      .multiselect__placeholder {
+        color: var(--form-placeholder--hover-color);
+      }
+    }
+  }
 
+  &--active {
+    .wt-select__arrow-caret {
+      transform: rotate(180deg);
+    }
+
+    .wt-select__arrow-caret ::v-deep .wt-icon__icon {
+      fill: var(--icon--hover-color);
+    }
+
+    ::v-deep {
+      .multiselect__tags {
+        border-color: var(--form-border--active-color);
+
+        .multiselect__tags-wrap,
         .multiselect__strong {
-          @extend %typo-body-lg;
-          @extend .wt-badge;
-          position: absolute;
-          right: var(--select-badge-right-pos);
-          min-height: var(--select-input-min-height); // reset original 20px style
-          margin: 0;
-          font-weight: normal;
-          z-index: var(--select-badge-z-index);
-
-          &:before {
-            content: '+';
-          }
-        }
-
-        .multiselect__tags-wrap {
-          width: 100%;
-        }
-
-        .multiselect__custom-tag {
-          @extend %typo-body-lg;
-          color: var(--form-input-color);
-
-          // text overflow 3 dots
-          display: block;
-          white-space: nowrap;
-          overflow: hidden;
-          text-overflow: ellipsis;
+          display: none;
         }
 
         .multiselect__placeholder {
-          padding: 0;
-          margin: 0;
-          color: var(--form-placeholder-color);
-
-          // text overflow 3 dots
-          white-space: nowrap;
-          overflow: hidden;
-          text-overflow: ellipsis;
-        }
-      }
-
-      .multiselect__content-wrapper {
-        @extend %wt-distant-scrollbar;
-        border: var(--select-tags-border);
-        border-color: var(--form-border-color);
-        border-radius: var(--border-radius);
-        transition: var(--transition);
-
-        .multiselect__option {
-          padding: var(--select-options-padding);
-
-          & > span {
-            @extend %typo-body-lg;
-            color: var(--form-placeholder-color);
-          }
-
-          &:after {
-            display: none;
-          }
-
-          &--highlight {
-            background: var(--select-option--hover-color);
-           & > span {
-              color: var(--form-input-color);
-            }
-          }
-
-          &--selected {
-            font-weight: normal;
-            background: var(--select-option--active-color)
-          }
+          color: var(--form-placeholder--active-color);
         }
       }
     }
+  }
+}
 
-    .wt-select:hover & {
-      .wt-select__arrow-caret ::v-deep .wt-icon__icon {
-        fill: var(--icon--hover-color);
+.wt-select--invalid,
+.wt-select--invalid:hover {
+  .wt-label {
+    color: var(--false-color);
+  }
+
+  .multiselect {
+    ::v-deep {
+      .multiselect__tags {
+        border-color: var(--false-color);
+        outline: none; // prevent outline overlapping false color
       }
+    }
+  }
+}
 
-      ::v-deep .wt-select__clear .wt-icon__icon {
-        fill: var(--icon--hover-color);
-      }
+.wt-select--disabled {
+  pointer-events: none;
 
-      ::v-deep {
-        .multiselect__tags {
-          border-color: var(--form-border--hover-color);
-        }
+  .multiselect--disabled {
+    background: none;
+    opacity: 1;
+  }
+
+  .wt-select__arrow-caret ::v-deep .wt-icon__icon,
+  .wt-select__clear ::v-deep .wt-icon__icon {
+    fill: var(--icon--disabled-color);
+  }
+
+  .multiselect {
+    ::v-deep {
+      .multiselect__tags {
+        background: var(--form-border--disabled-color);
+        border-color: var(--form-border--disabled-color);
 
         .multiselect__placeholder {
-          color: var(--form-placeholder--hover-color);
-        }
-      }
-    }
-
-    &--active {
-      .wt-select__arrow-caret {
-        transform: rotate(180deg);
-      }
-
-      .wt-select__arrow-caret ::v-deep .wt-icon__icon {
-        fill: var(--icon--hover-color);
-      }
-
-      ::v-deep {
-        .multiselect__tags {
-          border-color: var(--form-border--active-color);
-
-          .multiselect__tags-wrap,
-          .multiselect__strong {
-            display: none;
-          }
-
-          .multiselect__placeholder {
-            color: var(--form-placeholder--active-color);
-          }
+          color: var(--form-placeholder--disabled-color);
         }
       }
     }
   }
-
-  .wt-select--invalid,
-  .wt-select--invalid:hover {
-    .wt-label {
-      color: var(--false-color);
-    }
-
-    .multiselect {
-      ::v-deep {
-        .multiselect__tags {
-          border-color: var(--false-color);
-          outline: none; // prevent outline overlapping false color
-        }
-      }
-    }
-  }
-
-  .wt-select--disabled {
-    pointer-events: none;
-
-    .multiselect--disabled {
-       background: none;
-       opacity: 1;
-    }
-
-    .wt-select__arrow-caret ::v-deep .wt-icon__icon,
-    .wt-select__clear ::v-deep .wt-icon__icon {
-      fill: var(--icon--disabled-color);
-    }
-
-    .multiselect {
-      ::v-deep {
-        .multiselect__tags {
-          background: var(--form-border--disabled-color);
-          border-color: var(--form-border--disabled-color);
-
-          .multiselect__placeholder {
-            color: var(--form-placeholder--disabled-color);
-          }
-        }
-      }
-    }
-  }
+}
 </style>
