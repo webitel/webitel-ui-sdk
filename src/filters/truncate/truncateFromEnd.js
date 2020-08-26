e3261fecc140bacda89c889cf9d632eda7be43c5
dlohvinov <nsblnr@gmail.com>
2020-08-26 11:50:02 +0300

    fix: fixed truncFromEnd filter [WTEL-1291]

--- src/filters/truncate/truncateFromEnd.js
+++ src/filters/truncate/truncateFromEnd.js
@@ -1,7 +1,7 @@
 const truncateFromEnd = (value, length = 18) => {
   if (value) {
     if (value.length < length) return value;
-    return `${value.slice(value.length - (length - 3))}...`;
+    return `...${value.slice(value.length - (length - 3))}`;
   }
   return '';
 };
