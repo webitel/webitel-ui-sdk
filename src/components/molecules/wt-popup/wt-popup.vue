40b97767f7b0ca7bf39d50a9504d5e59333995af
dlohvinov <nsblnr@gmail.com>
2020-10-13 18:38:04 +0300

    dev: added margin to wt-buttons in popups [WTEL-1482]

--- src/components/molecules/wt-popup/wt-popup.vue
+++ src/components/molecules/wt-popup/wt-popup.vue
@@ -23,67 +23,75 @@
 </template>
 
 <script>
-  export default {
-    name: 'wt-popup',
-  };
+export default {
+  name: 'wt-popup',
+};
 </script>
 
 <style lang="scss" scoped>
-  .wt-popup {
-    position: fixed;
+.wt-popup {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: var(--popup-wrapper-z-index);
+
+  &__shadow {
+    position: absolute;
     top: 0;
     right: 0;
     bottom: 0;
     left: 0;
-    z-index: var(--popup-wrapper-z-index);
-
-    &__shadow {
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      background: var(--main-primary-accent-color);
-      opacity: var(--popup-shadow-opacity);
-      z-index: 0;
-    }
+    background: var(--main-primary-accent-color);
+    opacity: var(--popup-shadow-opacity);
+    z-index: 0;
   }
+}
 
-  .wt-popup__popup {
-    @extend %wt-scrollbar;
+.wt-popup__popup {
+  @extend %wt-scrollbar;
 
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  max-height: var(--popup-max-height);
+  padding: var(--popup-padding);
+  background: var(--popup-bg-color);
+  transform: translate(-50%, -50%);
+  border-radius: var(--border-radius);
+  box-shadow: var(--box-shadow);
+  overflow-y: auto;
+  z-index: 1;
+}
+
+.wt-popup__header {
+  position: relative;
+  padding: var(--popup-header-padding);
+
+  .wt-popup__close-btn {
     position: absolute;
-    top: 50%;
-    left: 50%;
-    max-height: var(--popup-max-height);
-    padding: var(--popup-padding);
-    background: var(--popup-bg-color);
-    transform: translate(-50%, -50%);
-    border-radius: var(--border-radius);
-    box-shadow: var(--box-shadow);
-    overflow-y: auto;
-    z-index: 1;
+    top: 0;
+    right: 0;
   }
+}
 
-  .wt-popup__header {
-    position: relative;
-    padding: var(--popup-header-padding);
+.wt-popup__main {
+  padding: var(--popup-main-padding);
+}
 
-    .wt-popup__close-btn {
-      position: absolute;
-      top: 0;
-      right: 0;
+.wt-popup__actions {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: var(--popup-actions-padding);
+
+  .wt-button {
+    margin-left: 20px;
+
+    &:first-child {
+      margin-left: 0;
     }
   }
-
-  .wt-popup__main {
-    padding: var(--popup-main-padding);
-  }
-
-  .wt-popup__actions {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: var(--popup-actions-padding);
-  }
+}
 </style>
