6b3bc3bcf63befa5c4e36926ae61e33ca1dcb4af
dlohvinov <nsblnr@gmail.com>
2021-07-13 14:13:25 +0300

    0.8.11: added WebitelApplications, Admin and Supervisor Sections enums [WTEL-1946]

---
+++ src/enums/WebitelApplications/WebitelApplications.enum.js
@@ -0,0 +1,10 @@
+const WebitelApplications = Object.freeze({
+  ADMIN: 'admin',
+  WORKSPACE: 'agent',
+  SUPERVISOR: 'supervisor',
+  HISTORY: 'history',
+  AUDIT: 'audit',
+  ANALYTICS: 'grafana',
+});
+
+export default WebitelApplications;
