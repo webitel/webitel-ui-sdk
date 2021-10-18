d5465f9f03e4de0c64d2137c1fa253d6df5b1d3e
Vasyl <vasjanja@gmail.com>
2021-10-18 18:45:10 +0300

    refactor: variables naming and comments [WPRO-219]

--- src/components/molecules/wt-slider/wt-slider.vue
+++ src/components/molecules/wt-slider/wt-slider.vue
@@ -6,7 +6,7 @@
     'wt-slider--vertical': vertical,
     }"
     :style="{ width: verticalHeight }"
-  >
+  > <!-- The row above is needed in order to set correct component width when slider is vertical -->
     <input
       type="range"
       class="wt-slider__slider"
@@ -49,13 +49,14 @@ export default {
       type: Number,
       default: 1,
     },
+    // Height of vertical slider (px):
     height: {
       type: Number,
       default: 100,
     },
   },
   computed: {
-    progressStyle() {
+    progressStyle() { // To achieve the correct color styling:
       const progressPercent = ((this.value - this.min) / (this.max - this.min)) * 100;
       return `linear-gradient(to right, var(--accent-color) ${progressPercent}%, var(--secondary-color) 0%)`;
     },
@@ -74,24 +75,24 @@ export default {
 <style lang="scss" scoped>
 
 .wt-slider {
-  width: var(--slider-width)
+  width: var(--wt-slider-width)
 }
 
 .wt-slider__slider {
-  box-sizing: var(--slider-box-sizing);
-  width: var(--slider-width);
-  height: var(--slider-height);
-  border: var(--slider-border);
+  box-sizing: var(--wt-slider-box-sizing);
+  width: var(--wt-slider-width);
+  height: var(--wt-slider-height);
+  border: var(--wt-slider-border);
   border-radius: var(--border-radius);
   cursor: pointer;
   -webkit-appearance: none;
   -moz-appearance: none;
 
   &::-webkit-slider-thumb {
-    width: var(--pointer-size);
-    height: var(--pointer-size);
-    border: var(--slider-border);
-    border-radius: var(--pointer-radius);
+    width: var(--wt-slider-pointer-size);
+    height: var(--wt-slider-pointer-size);
+    border: var(--wt-slider-border);
+    border-radius: var(--wt-slider-pointer-radius);
     background: var(--main-color);
     -webkit-appearance: none;
   }
@@ -101,10 +102,10 @@ export default {
   }
 
   &::-moz-range-thumb {
-    width: var(--pointer-size);
-    height: var(--pointer-size);
-    border: var(--slider-border);
-    border-radius: var(--pointer-radius);
+    width: var(--wt-slider-pointer-size);
+    height: var(--wt-slider-pointer-size);
+    border: var(--wt-slider-border);
+    border-radius: var(--wt-slider-pointer-radius);
     background: var(--main-color);
     -moz-appearance: none;
   }
@@ -137,8 +138,8 @@ export default {
 }
 
 .wt-slider--vertical {
-  transform: var(--slider-vertical-transform);
-  transform-origin: var(--slider-vertical-transform-origin);
+  transform: var(--wt-slider-vertical-transform);
+  transform-origin: var(--wt-slider-vertical-transform-origin);
 }
 
 </style>
