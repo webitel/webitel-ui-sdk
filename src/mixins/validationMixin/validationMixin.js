91c710063dc6a3670f5c9a4d93e7701ea80b182a
Vasyl <vasjanja@gmail.com>
2021-11-29 13:54:51 +0200

    feature: text extention added [WPRO-218]

--- src/mixins/validationMixin/validationMixin.js
+++ src/mixins/validationMixin/validationMixin.js
@@ -17,24 +17,25 @@ export default {
       return this.isValidation && this.v.$error;
     },
     validationText() {
+      let validationText = '';
       if (this.isValidation && this.invalid) {
-        if (this.v.required === false) return this.$t('validation.required');
-        if (this.v.numeric === false) return this.$t('validation.numeric');
-        if (this.v.email === false) return this.$t('validation.email');
-        if (this.v.gatewayHostValidator === false) return this.$t('validation.gatewayHostValidator');
-        if (this.v.ipValidator === false) return this.$t('validation.ipValidator');
-        if (this.v.macValidator === false) return this.$t('validation.macValidator');
-        if (this.v.minValue === false) return `${this.$t('validation.minValue')} ${this.v.$params.minValue.min}`;
-        if (this.v.maxValue === false) return `${this.$t('validation.maxValue')} ${this.v.$params.maxValue.max}`;
-        if (this.v.sipAccountValidator === false) return this.$t('validation.sipAccountValidator');
-        if (this.v.minLength === false) return `${this.$t('validation.minLength')} ${this.v.$params.minLength.min}`;
-        if (this.v.url === false) return `${this.$t('validation.url')}`;
+        if (this.v.required === false) validationText = this.$t('validation.required');
+        if (this.v.numeric === false) validationText = this.$t('validation.numeric');
+        if (this.v.email === false) validationText = this.$t('validation.email');
+        if (this.v.gatewayHostValidator === false) validationText = this.$t('validation.gatewayHostValidator');
+        if (this.v.ipValidator === false) validationText = this.$t('validation.ipValidator');
+        if (this.v.macValidator === false) validationText = this.$t('validation.macValidator');
+        if (this.v.minValue === false) validationText = `${this.$t('validation.minValue')} ${this.v.$params.minValue.min}`;
+        if (this.v.maxValue === false) validationText = `${this.$t('validation.maxValue')} ${this.v.$params.maxValue.max}`;
+        if (this.v.sipAccountValidator === false) validationText = this.$t('validation.sipAccountValidator');
+        if (this.v.minLength === false) validationText = `${this.$t('validation.minLength')} ${this.v.$params.minLength.min}`;
+        if (this.v.url === false) validationText = `${this.$t('validation.url')}`;
       }
       // eslint-disable-next-line no-restricted-syntax
       for (const { name, text } of this.customValidators) {
-        if (this.v[name] === false) return text;
+        if (this.v[name] === false) validationText = text;
       }
-      return '';
+      return validationText;
     },
   },
 };
