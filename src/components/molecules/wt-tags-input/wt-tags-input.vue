a12e19ebd68627e4afdc0ddb429e2bc066be4c4f
dlohvinov <nsblnr@gmail.com>
2022-04-22 17:41:25 +0300

    1.0.33: changed wt-tags-input separator [WTEL-2519]

--- src/components/molecules/wt-tags-input/wt-tags-input.vue
+++ src/components/molecules/wt-tags-input/wt-tags-input.vue
@@ -24,6 +24,7 @@
       :tags="tags"
       :autocomplete-items="autocompleteOptions"
       :placeholder="placeholder || label"
+      :separators="['   ']"
       v-on="listeners"
     >
       <template slot="tag-actions" slot-scope="{ index, performDelete }">
