04faa70cbd00babaa6c4c399c1d83ce3476ba38b
dlohvinov <nsblnr@gmail.com>
2022-12-08 14:43:07 +0200

    2.1.28: fixed wt-tabs layout [WTEL-3122]

--- src/components/molecules/wt-tabs/wt-tabs.vue
+++ src/components/molecules/wt-tabs/wt-tabs.vue
@@ -1,13 +1,13 @@
 <template>
   <nav class="wt-tabs">
     <button
-      class="wt-tab"
+      v-for="(tab) in tabs"
+      :key="tab.value"
       :class="{
         'wt-tab--highlight': tab.value === current.value
       }"
-      v-for="(tab) in tabs"
       :value="tab.text"
-      :key="tab.value"
+      class="wt-tab"
       type="button"
       @click="open(tab)"
     >
@@ -18,98 +18,100 @@
 
     <div class="wt-tabs__underline">
       <div
-        class="wt-tab__underline--highlight"
         :style="{
           width: `${activeLineWidth}px`,
           transform: `translateX(${activeLineOffset}px)`
         }"
+        class="wt-tab__underline--highlight"
       ></div>
     </div>
   </nav>
 </template>
 
 <script>
-  export default {
-    name: 'wt-tabs',
-    props: {
-      current: {
-        type: Object,
-        default: () => ({}),
-      },
-      tabs: {
-        type: Array,
-        default: () => [],
-      },
+export default {
+  name: 'wt-tabs',
+  props: {
+    current: {
+      type: Object,
+      default: () => ({}),
+    },
+    tabs: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  model: {
+    prop: 'current',
+    event: 'change',
+  },
+
+  data: () => ({
+    activeLineWidth: 0,
+    activeLineOffset: 0,
+  }),
+
+  methods: {
+    open(value) {
+      this.$emit('change', value);
+      this.moveActiveLine(value);
     },
 
-    model: {
-      prop: 'current',
-      event: 'change',
+    moveActiveLine(newValue) {
+      if (!this.current) return;
+      if (!this.$refs || !this.$refs[newValue] || !this.$refs[newValue][0]) return;
+      const element = this.$refs[newValue][0];
+      this.activeLineWidth = element.clientWidth;
+      this.activeLineOffset = element.offsetLeft;
     },
-
-    data: () => ({
-      activeLineWidth: 0,
-      activeLineOffset: 0,
-    }),
-
-    methods: {
-      open(value) {
-        this.$emit('change', value);
-        this.moveActiveLine(value);
-      },
-
-      moveActiveLine(newValue) {
-        if (!this.current) return;
-        if (!this.$refs || !this.$refs[newValue] || !this.$refs[newValue][0]) return;
-        const element = this.$refs[newValue][0];
-        this.activeLineWidth = element.clientWidth;
-        this.activeLineOffset = element.offsetLeft;
-      },
-    },
-  };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .wt-tabs {
-    position: relative;
+.wt-tabs {
+  position: relative;
+  display: flex;
+  flex-wrap: nowrap;
+  gap: var(--tab-gap);
+}
+
+.wt-tab {
+  @extend %typo-body-1;
+  position: relative;
+  z-index: var(--tab-z-index);
+  display: inline-block;
+  padding-bottom: var(--tab-padding);
+  cursor: pointer;
+  transition: var(--transition);
+  color: var(--tab-color);
+  border: none;
+  border-bottom: var(--tab-border);
+  border-bottom-color: transparent;
+  outline: none;
+  background: transparent;
+
+  &:hover,
+  &:focus, {
+    color: var(--tab--hover-color);
+    border-bottom-color: var(--tab--active-border-color);
   }
 
-  .wt-tab {
-    @extend %typo-body-1;
-    position: relative;
-    display: inline-block;
-    padding-bottom: var(--tab-padding);
-    margin: var(--tab-margin);
-    color: var(--tab-color);
-    background: transparent;
-    border: none;
-    border-bottom: var(--tab-border);
-    border-bottom-color: transparent;
-    transition: var(--transition);
-    cursor: pointer;
-    z-index: var(--tab-z-index);
-    outline: none;
-
-    &:hover,
-    &:focus, {
-      color: var(--tab--hover-color);
-      border-bottom-color: var(--tab--active-border-color);
-    }
-
-    &.wt-tab--highlight {
-      color: var(--tab--active-color);
-      border-bottom-color: var(--tab--active-border-color);
-    }
-
-    //// disables bold font resize on hover
-    &:after {
-      display: block;
-      content: attr(value);
-      font-weight: bold;
-      height: 0;
-      overflow: hidden;
-      visibility: hidden;
-    }
+  &.wt-tab--highlight {
+    color: var(--tab--active-color);
+    border-bottom-color: var(--tab--active-border-color);
   }
+
+  //// disables bold font resize on hover
+  &:after {
+    display: block;
+    visibility: hidden;
+    overflow: hidden;
+    height: 0;
+    content: attr(value);
+    font-weight: bold;
+  }
+}
 
 </style>
