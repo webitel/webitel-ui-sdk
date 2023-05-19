c4edebf017965bb81bef5786ce336f619b100367
Lera24 <bedulinavalera21@gmail.com>
2023-05-19 13:18:02 +0300

    fix: audit-form test[WTEL-3436]

--- src/modules/AuditForm/components/__tests__/audit-form.spec.js
+++ src/modules/AuditForm/components/__tests__/audit-form.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import AuditForm from '../audit-form.vue';
 import { generateQuestionSchema } from '../../schemas/AuditFormQuestionSchema';
 
@@ -12,55 +12,60 @@ useDestroyableSortable.mockImplementation(() => ({}));
 
 describe('AuditForm', () => {
   it('renders a component', () => {
-    const wrapper = shallowMount(AuditForm, {
+    const wrapper = mount(AuditForm, {
       props: {
         mode: '',
         questions: [],
       },
     });
+     wrapper.vm.$refs.auditQuestions.value.at(0).activateQuestion = jest.fn()
     expect(wrapper.isVisible()).toBe(true);
   });
-  it('add question button triggers update question event', () => {
-    const wrapper = shallowMount(AuditForm, {
+  it('add question button triggers update question event', async () => {
+    const wrapper = mount(AuditForm, {
       props: {
         mode: 'create',
         questions: [],
       },
     });
-    wrapper.findComponent('.audit-form__add-button').vm.$emit('click');
+    wrapper.vm.$refs.auditQuestions.value.at(0).activateQuestion = jest.fn()
+    await wrapper.findComponent('.audit-form__add-button').vm.$emit('click');
     expect(wrapper.emitted()['update:questions'][0][0])
     .toEqual([generateQuestionSchema({ required: true })]);
   });
-  it('delete event from child question emits update without passed question', () => {
-    const wrapper = shallowMount(AuditForm, {
+  it('delete event from child question emits update without passed question', async () => {
+    const wrapper = mount(AuditForm, {
       props: {
         mode: 'create',
         questions: [generateQuestionSchema()],
       },
     });
-    wrapper.findComponent({ name: 'audit-form-question' }).vm.$emit('delete', { key: 0 });
+    wrapper.vm.$refs.auditQuestions.value.at(0).activateQuestion = jest.fn()
+    await wrapper.findComponent({ name: 'audit-form-question' }).vm.$emit('delete', { key: 0 });
     expect(wrapper.emitted()['update:questions'][0][0])
     .toEqual([]);
   });
-  it('copy event from child question emits update with duplicated questions', () => {
+  it('copy event from child question emits update with duplicated questions', async () => {
     const question = generateQuestionSchema();
-    const wrapper = shallowMount(AuditForm, {
+    const wrapper = mount(AuditForm, {
       props: {
         mode: 'create',
         questions: [question],
       },
     });
-    wrapper.findComponent({ name: 'audit-form-question' }).vm.$emit('copy', { question, key: 0 });
+    wrapper.vm.$refs.auditQuestions.value.at(0).activateQuestion = jest.fn()
+    await wrapper.findComponent({ name: 'audit-form-question' }).vm.$emit('copy', { question, key: 0 });
     expect(wrapper.emitted()['update:questions'][0][0])
     .toEqual([question, question]);
   });
-  it('initializes result depending on passed questions', () => {
-    const wrapper = shallowMount(AuditForm, {
+  it('initializes result depending on passed questions',() => {
+    const wrapper = mount(AuditForm, {
       props: {
         mode: 'fill',
         questions: [generateQuestionSchema(), generateQuestionSchema(), generateQuestionSchema()],
       },
     });
+    wrapper.vm.$refs.auditQuestions.value.at(0).activateQuestion = jest.fn()
     expect(wrapper.emitted()['update:result'][0][0]).toEqual([null, null, null]);
   });
 });
