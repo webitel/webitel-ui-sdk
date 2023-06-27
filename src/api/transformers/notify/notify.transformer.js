be94cd4b34a3de1b443475560fd37d6cf8882bbd
dlohvinov <nsblnr@gmail.com>
2023-06-27 21:18:10 +0300

    3.2.4: notify transformer upgrade [WTEL-3425]

--- src/api/transformers/notify/notify.transformer.js
+++ src/api/transformers/notify/notify.transformer.js
@@ -1,13 +1,32 @@
 import eventBus from '../../../scripts/eventBus';
 
-const notifyTransformer = (payload) => {
-  if (payload instanceof Error) {
+const notifyTransformer = (notificationObject) => {
+  /*
+  if passed arg is function, then this notification - static content,
+  predefined before actual transformer is called in applyTransform flow
+   */
+  if (typeof notificationObject === 'function') {
+    /*
+    so, create a callback which will send notification with params, passed to it
+     */
+    const callback = ({ type, text }) => eventBus.$emit('notification', { type, text });
+
+    /*
+    and, then, return a function, which will be called in main applyTransform flow,
+    calling passed arg function with callback, and returning actual notify payload
+     */
+    return (payload) => {
+      notificationObject({ callback });
+      return payload;
+    };
+  }
+  if (notificationObject instanceof Error) {
     eventBus.$emit('notification', {
       type: 'error',
-      text: payload.response?.data?.detail || payload.response?.data?.message,
+      text: notificationObject.response?.data?.detail || notificationObject.response?.data?.message,
     });
   }
-  return payload;
+  return notificationObject;
 };
 
 export default notifyTransformer;
