d0ce2fa574ac767fe7de1f8befb46b20e7b22ee8
dlohvinov <nsblnr@gmail.com>
2023-07-11 19:02:02 +0300

    3.2.50: fixed debounce [WTEL-2261]

--- src/scripts/debounce.js
+++ src/scripts/debounce.js
@@ -1,2 +1,5 @@
-import debounce from 'lodash.debounce';
+import lodashDebounce from 'lodash.debounce';
+
+const debounce = (fn, wait = 1000, options) => lodashDebounce(fn, wait, options);
+
 export default debounce;
