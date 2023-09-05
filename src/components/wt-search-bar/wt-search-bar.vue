889fc9d38268036301a380e201565ac63e33611b
volodymyr <volodymyr.dechtiaruk@gmail.com>
2023-09-05 15:06:56 +0300

    feature: hint and invalid state added to wt-search [WTEL-3766]

--- src/components/wt-search-bar/wt-search-bar.vue
+++ src/components/wt-search-bar/wt-search-bar.vue
@@ -2,10 +2,18 @@
   <div
     :class="{
       'wt-search-bar--outline': outline,
+      'wt-search-bar--invalid': invalid,
     }"
     class="wt-search-bar"
   >
     <div class="wt-search-bar__wrapper">
+      <div class="wt-search-bar__search-icon">
+        <slot name="search-icon">
+          <wt-icon
+            icon="search"
+          ></wt-icon>
+        </slot>
+      </div>
       <input
         :placeholder="placeholder || $t('webitelUI.searchBar.placeholder')"
         :value="value"
@@ -14,27 +22,35 @@
         @input="handleInput($event.target.value)"
         @keyup="handleKeyup"
       >
-      <div class="wt-search-bar__search-icon">
-        <slot name="search-icon">
-          <wt-icon
-            icon="search"
-          ></wt-icon>
-        </slot>
+      <div class="wt-search-bar__icon-controls">
+        <wt-icon-btn
+          :class="{ 'hidden': !value }"
+          class="wt-search-bar__reset-icon-btn"
+          icon="close"
+          @click="handleInput('')"
+        ></wt-icon-btn>
+        <wt-hint
+          v-if="hint"
+        >{{ hint }}
+        </wt-hint>
+        <slot name="additional-actions"></slot>
       </div>
-      <wt-icon-btn
-        :class="{ 'hidden': !value }"
-        class="wt-search-bar__reset-icon-btn"
-        icon="close"
-        @click="handleInput('')"
-      ></wt-icon-btn>
     </div>
   </div>
 </template>
 
 <script setup>
 import debounce from '../../scripts/debounce';
+import { validationComposable } from '../../mixins/validationMixin/validationComposable';
 
 const props = defineProps({
+  v: {
+    type: Object,
+  },
+  customValidators: {
+    type: Array,
+    default: () => [],
+  },
   /**
    * Current search-bar value (`v-model`)
    */
@@ -52,7 +68,13 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  hint: {
+    type: String,
+    description: 'Adds hint icon + tooltip with text, passed as "hint" prop',
+  },
 });
+
+const { invalid } = validationComposable(props);
 
 const emit = defineEmits([
   'input',
@@ -89,25 +111,34 @@ function handleKeyup(event) {
 .wt-search-bar {
   cursor: text;
 
+  &.wt-search-bar--invalid {
+    .wt-search-bar__wrapper {
+      border-color: var(--false-color);
+      outline: none; // prevent outline overlapping false color
+    }
+    ::v-deep .wt-icon__icon {
+      fill: var(--false-color);
+    }
+    .wt-search-bar__input {
+      color: var(--false-color);
+    }
+  }
+
   .wt-search-bar__wrapper {
-    position: relative;
+    border: var(--input-border);
+    border-color: var(--form-border-color);
+    border-radius: var(--border-radius);
+    display: flex;
+    padding: calc(var(--spacing-xs) - 1px) var(--spacing-xs);
+    align-items: center;
+    gap: var(--spacing-xs);
   }
 
   .wt-search-bar__search-icon {
-    position: absolute;
-    top: 50%;
-    left: var(--input-icon-margin);
-    transform: translateY(-50%);
-    line-height: 0;
-    pointer-events: none;
+    display: flex;
   }
 
   .wt-search-bar__reset-icon-btn {
-    position: absolute;
-    top: 50%;
-    right: var(--input-icon-margin);
-    transform: translateY(-50%);
-
     .wt-search-bar:not(:focus-within) & {
       pointer-events: none;
       opacity: 0;
@@ -121,12 +152,11 @@ function handleKeyup(event) {
     display: block;
     box-sizing: border-box;
     width: 100%;
-    padding: var(--search-bar-padding);
+    padding: 0;
     transition: var(--transition);
     color: var(--form-input-color);
-    border: var(--input-border);
-    border-color: var(--form-border-color);
-    border-radius: var(--border-radius);
+    border: none;
+    outline: none;
 
     &:focus {
       @include wt-placeholder('focus');
@@ -168,6 +198,12 @@ function handleKeyup(event) {
   .wt-search-bar__input::-webkit-search-results-button,
   .wt-search-bar__input::-webkit-search-results-decoration {
     display: none;
+  }
+
+  .wt-search-bar__icon-controls {
+    display: flex;
+    align-items: center;
+    gap: var(--spacing-xs);
   }
 }
 </style>
---
+++ src/mixins/validationMixin/validationComposable.js
@@ -0,0 +1,37 @@
+import { computed } from 'vue';
+
+export function validationComposable(props) {
+  const isValidation = computed(() => !!props.v && !!Object.keys(props.v).length);
+  const invalid = computed(() => isValidation.value && props.v.$error);
+
+  const validationText = computed(() => {
+    let validationText = '';
+    if (this.isValidation && this.invalid) {
+      if (this.v.required?.$invalid) validationText = this.$t('validation.required');
+      else if (this.v.numeric?.$invalid) validationText = this.$t('validation.numeric');
+      else if (this.v.email?.$invalid) validationText = this.$t('validation.email');
+      else if (this.v.gatewayHostValidator?.$invalid) validationText = this.$t('validation.gatewayHostValidator');
+      else if (this.v.ipValidator?.$invalid) validationText = this.$t('validation.ipValidator');
+      else if (this.v.macValidator?.$invalid) validationText = this.$t('validation.macValidator');
+      else if (this.v.minValue?.$invalid) validationText = `${this.$t('validation.minValue')} ${this.v.minValue.$params.min}`;
+      else if (this.v.maxValue?.$invalid) validationText = `${this.$t('validation.maxValue')} ${this.v.maxValue.$params.max}`;
+      else if (this.v.sipAccountValidator?.$invalid) validationText = this.$t('validation.sipAccountValidator');
+      else if (this.v.minLength?.$invalid) validationText = `${this.$t('validation.minLength')} ${this.v.minLength.$params.min}`;
+      else if (this.v.url?.$invalid) validationText = `${this.$t('validation.url')}`;
+      else if (this.v.regExpValidator?.$invalid) validationText = `${this.$t('validation.regExpValidator')}`;
+      else if (this.v.sameAs?.$invalid) validationText = `${this.$t('validation.sameAs')}`;
+      else if (this.v.domainValidator?.$invalid) validationText = `${this.$t('validation.domainValidator')}`;
+    }
+    // eslint-disable-next-line no-restricted-syntax
+    for (const { name, text } of props.customValidators) {
+      if (this.v[name]?.$invalid) validationText = text;
+    }
+    return validationText;
+  });
+
+  return {
+    isValidation,
+    invalid,
+    validationText,
+  };
+}
