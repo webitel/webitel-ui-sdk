31e9704e8f485210dac0a95cdd272d93ac9f6a89
volodymyr <volodymyr.dechtiaruk@gmail.com>
2023-09-13 17:56:17 +0300

    fix: fixing wt-search-bar reactivity, valText fixed [WTEL-3766]

--- src/mixins/validationMixin/useValidation.js
+++ src/mixins/validationMixin/useValidation.js
@@ -9,26 +9,26 @@ export function useValidation({ v, customValidators }) {
   const validationText = computed(() => {
     let validationText = '';
     if (isValidation && invalid) {
-      if (v.required?.$invalid) validationText = t('validation.required');
-      else if (v.numeric?.$invalid) validationText = t('validation.numeric');
-      else if (v.email?.$invalid) validationText = t('validation.email');
-      else if (v.gatewayHostValidator?.$invalid) validationText = t('validation.gatewayHostValidator');
-      else if (v.ipValidator?.$invalid) validationText = t('validation.ipValidator');
-      else if (v.macValidator?.$invalid) validationText = t('validation.macValidator');
-      else if (v.minValue?.$invalid) validationText = `${t('validation.minValue')} ${v.minValue.$params.min}`;
-      else if (v.maxValue?.$invalid) validationText = `${t('validation.maxValue')} ${v.maxValue.$params.max}`;
-      else if (v.sipAccountValidator?.$invalid) validationText = t('validation.sipAccountValidator');
-      else if (v.minLength?.$invalid) validationText = `${t('validation.minLength')} ${v.minLength.$params.min}`;
-      else if (v.url?.$invalid) validationText = `${t('validation.url')}`;
-      else if (v.regExpValidator?.$invalid) validationText = `${t('validation.regExpValidator')}`;
-      else if (v.sameAs?.$invalid) validationText = `${t('validation.sameAs')}`;
-      else if (v.domainValidator?.$invalid) validationText = `${t('validation.domainValidator')}`;
-      else if (v.decimalValidator?.$invalid) validationText = `${t('validation.decimalValidator')} ${v.decimalValidator.$params.count}`;
-      else if (v.integer?.$invalid) validationText = `${t('validation.integer')}`;
+      if (v.value.required?.$invalid) validationText = t('validation.required');
+      else if (v.value.numeric?.$invalid) validationText = t('validation.numeric');
+      else if (v.value.email?.$invalid) validationText = t('validation.email');
+      else if (v.value.gatewayHostValidator?.$invalid) validationText = t('validation.gatewayHostValidator');
+      else if (v.value.ipValidator?.$invalid) validationText = t('validation.ipValidator');
+      else if (v.value.macValidator?.$invalid) validationText = t('validation.macValidator');
+      else if (v.value.minValue?.$invalid) validationText = `${t('validation.minValue')} ${v.value.minValue.$params.min}`;
+      else if (v.value.maxValue?.$invalid) validationText = `${t('validation.maxValue')} ${v.value.maxValue.$params.max}`;
+      else if (v.value.sipAccountValidator?.$invalid) validationText = t('validation.sipAccountValidator');
+      else if (v.value.minLength?.$invalid) validationText = `${t('validation.minLength')} ${v.value.minLength.$params.min}`;
+      else if (v.value.url?.$invalid) validationText = `${t('validation.url')}`;
+      else if (v.value.regExpValidator?.$invalid) validationText = `${t('validation.regExpValidator')}`;
+      else if (v.value.sameAs?.$invalid) validationText = `${t('validation.sameAs')}`;
+      else if (v.value.domainValidator?.$invalid) validationText = `${t('validation.domainValidator')}`;
+      else if (v.value.decimalValidator?.$invalid) validationText = `${t('validation.decimalValidator')} ${v.value.decimalValidator.$params.count}`;
+      else if (v.value.integer?.$invalid) validationText = `${t('validation.integer')}`;
     }
     // eslint-disable-next-line no-restricted-syntax
     for (const { name, text } of customValidators) {
-      if (v[name]?.$invalid) validationText = text;
+      if (v.value[name]?.$invalid) validationText = text;
     }
     return validationText;
   });
