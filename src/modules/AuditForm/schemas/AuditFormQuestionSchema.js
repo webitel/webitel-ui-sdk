62c047a23100d8572ae4013d9a8a3eefefda0bd8
Lera24 <bedulinavalera21@gmail.com>
2023-09-28 11:43:11 +0300

    feature: change default required value in AuditFormModule[WTEL-3871]

--- src/modules/AuditForm/schemas/AuditFormQuestionSchema.js
+++ src/modules/AuditForm/schemas/AuditFormQuestionSchema.js
@@ -1,7 +1,7 @@
 import { generateQuestionScoreSchema } from './AuditFormQuestionScoreSchema';
 
 // eslint-disable-next-line import/prefer-default-export
-export const generateQuestionSchema = ({ required = false } = {}) => ({
+export const generateQuestionSchema = ({ required = true } = {}) => ({
   required,
   question: '',
   ...generateQuestionScoreSchema(),
