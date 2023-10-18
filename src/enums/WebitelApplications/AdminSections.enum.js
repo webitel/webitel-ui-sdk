8dba29a44429d993b3788cb7b320674eb1029dad
volodymyr <volodymyr.dechtiaruk@gmail.com>
2023-10-18 18:39:31 +0300

    feature: adding single sing on section routes and locales [WTEL-3717]

--- src/enums/WebitelApplications/AdminSections.enum.js
+++ src/enums/WebitelApplications/AdminSections.enum.js
@@ -35,6 +35,7 @@ const AdminSections = Object.freeze({
   STORAGE: 'storage',                   // scope: storage_profile
   COGNITIVE_PROFILES: 'cognitive-profiles',  // scope: cognitive_profile
   EMAIL_PROFILES: 'email-profiles',  // scope: email_profile
+  SINGLE_SIGN_ON: 'single-sign-on',  // scope: single-sign-on
   IMPORT_CSV: 'import-csv',
   TRIGGERS: 'triggers',
 
