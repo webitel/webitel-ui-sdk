f282172dd2662562ad0224bbee59f8ec73250873
volodymyr <volodymyr.dechtiaruk@gmail.com>
2023-10-26 14:14:10 +0300

    feature: variable search validator changed [WTEL-3888]

--- src/validators/__tests__/variableSearchValidator.spec.js
+++ src/validators/__tests__/variableSearchValidator.spec.js
@@ -5,23 +5,27 @@ describe('Variable search validator', () => {
     const searchValue = 'height=100';
     expect(variableSearchValidator(searchValue)).toBe(true);
   });
-  it('Search with spacing', () => {
+  it('Search with spacing in value and key', () => {
    const searchValue = 'your height=5 feet 5½ inches (166 cm)';
     expect(variableSearchValidator(searchValue)).toBe(true);
   });
-  it('Search with spacing', () => {
+  it('Search with spacing in value', () => {
     const searchValue = 'height=5 feet 5½ inches (166 cm)';
+    expect(variableSearchValidator(searchValue)).toBe(true);
+  });
+  it('Search with spacing in value and spaces before key and after value', () => {
+    const searchValue = ' height=5 feet 5½ inches (166 cm) ';
     expect(variableSearchValidator(searchValue)).toBe(true);
   });
   it('Search with snake_case', () => {
     const searchValue = 'expected_output=valid_number';
     expect(variableSearchValidator(searchValue)).toBe(true);
   });
-  it('Wrong search with spacing near "=" sign', () => {
+  it('Wrong search with spacing after "=" sign', () => {
     const searchValue = 'height= 100';
     expect(variableSearchValidator(searchValue)).toBe(false);
   });
-  it('Wrong search with spacing near "=" sign', () => {
+  it('Wrong search with spacing before "=" sign', () => {
     const searchValue = 'height =100';
     expect(variableSearchValidator(searchValue)).toBe(false);
   });
--- src/validators/variableSearchValidator.js
+++ src/validators/variableSearchValidator.js
@@ -1,4 +1,18 @@
-export default function variableSearchValidator(value) {
-  const isValid = /^(\w+)=(?!\s).*?$/.test(value);
-  return !value || isValid;
+export default function variableSearchValidator(input) {
+  const splitInput = input.split('=');
+  if (splitInput.length !== 2) {
+    return false;
+  }
+
+  const [key, value] = splitInput;
+
+  if (key.endsWith(' ')) {
+    return false;
+  }
+
+  if (value.startsWith(' ')) {
+    return false;
+  }
+
+  return !input || true;
 };
