c726d2819cbd5d203c606ff404f2ff921c977367
volodymyr <volodymyr.dechtiaruk@gmail.com>
2023-11-20 14:29:00 +0200

    refactor: saveAsJSON script refactored [WTEL-3894]

--- src/scripts/saveAsJSON.js
+++ src/scripts/saveAsJSON.js
@@ -1,7 +1,9 @@
 import { saveAs } from 'file-saver';
 
-export const saveAsJSON = (name, content) => {
+const saveAsJSON = (name, content) => {
   const fileName = name.replace(/[ ,]+/g, '-');
   const file = new Blob([JSON.stringify(content, null, 4)], { type: 'application/json' });
   saveAs(file, fileName);
 };
+
+export default saveAsJSON;
