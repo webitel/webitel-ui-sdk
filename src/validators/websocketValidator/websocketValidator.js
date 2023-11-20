a80c8c90c6fb2fbb76624450f0ce2d6aa1186bc0
Daniil Lohvinov <nsblnr@gmail.com>
2023-11-20 22:35:12 +0200

    23.09.6: copied websocket validator from 23.12

---
+++ src/validators/websocketValidator/websocketValidator.js
@@ -0,0 +1,9 @@
+// That's strange but i haven't found any npm package with websocket validator
+export default (value) => {
+  try {
+    const url = new URL(value);
+    return (/^(wss?)/i).test(url.protocol);
+  } catch (e) {
+    return false;
+  }
+};
