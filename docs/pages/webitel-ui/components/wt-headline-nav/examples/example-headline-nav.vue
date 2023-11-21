a2fc310d9f71fdef54abc304e63cd21655cdaf87
dlohvinov <nsblnr@gmail.com>
2023-11-21 12:34:31 +0200

    docs: wt-headline-nav docs on vitepress [WTEL-3840]

--- docs/pages/webitel-ui/components/wt-headline-nav/examples/example-headline-nav.vue
+++ docs/pages/webitel-ui/components/wt-headline-nav/examples/example-headline-nav.vue
@@ -1,9 +1,17 @@
-```vue
-<wt-headline-nav
-  :path="[
+<script setup>
+
+</script>
+
+<template>
+  <wt-headline-nav
+    :path="[
               { name: 'Admin' },
               { name: 'Contact-center' },
               { name: 'Queues', route: 'https://dev.webitel.com/contact-center/queues' },
           ]"
-></wt-headline-nav>
-```
+  ></wt-headline-nav>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- docs/pages/webitel-ui/components/wt-headline-nav/wt-headline-nav-docs.vue
+++ docs/pages/webitel-ui/components/wt-headline-nav/wt-headline-nav-docs.vue
@@ -1,31 +1,5 @@
 <template>
   <section>
-    <h2>Headline Nav</h2>
-    <article>
-      <div
-        class="example-wrapper"
-        style="padding: 30px; background: #EAEAEA;"
-      >
-        <wt-headline-nav
-          :path="[
-            { name: 'Admin' },
-            { name: 'Contact-center' },
-            { name: 'Queues', route: 'https://dev.webitel.com/contact-center/queues' },
-          ]"
-        />
-        <pre><code
-          class="language-html"
->
-        &lt;wt-headline-nav
-          :path="[
-              { name: 'Admin' },
-              { name: 'Contact-center' },
-              { name: 'Queues', route: 'https://dev.webitel.com/contact-center/queues' },
-          ]"
-        &gt;&lt;/wt-headline-nav&gt;
-        </code></pre>
-      </div>
-    </article>
     <component-props
       :properties="properties"
     />
