de90f429a32cc7460ad5820edf5a0230d4c19891
dlohvinov <nsblnr@gmail.com>
2023-11-21 12:29:47 +0200

    docs: wt-button docs on vitepress [WTEL-3840]

---
+++ docs/pages/webitel-ui/components/wt-button/examples/example-buttons-different-color.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div style="display: grid; grid-template-columns: 140px; gap: var(--spacing-xs);">
+    <wt-button>Primary</wt-button>
+    <wt-button color="secondary">Secondary</wt-button>
+    <wt-button disabled>Disabled</wt-button>
+    <wt-button color="success">Success</wt-button>
+    <wt-button color="error">Error</wt-button>
+    <wt-button color="transfer">Transfer</wt-button>
+    <wt-button color="job">Job</wt-button>
+    <wt-button color="info">info</wt-button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ docs/pages/webitel-ui/components/wt-button/examples/example-wide-button.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <wt-button wide>Wide button</wt-button>
+</template>
+
+<style scoped lang="scss">
+
+</style>
