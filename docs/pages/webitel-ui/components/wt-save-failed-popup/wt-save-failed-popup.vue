a6375750211bc2dc9226f4cc4941e776733a0e61
volodymyr <volodymyr.dechtiaruk@gmail.com>
2023-11-24 17:54:36 +0200

    feature: save-failed-popup docs added [WTEL-3894]

---
+++ docs/pages/webitel-ui/components/wt-save-failed-popup/wt-save-failed-popup.vue
@@ -0,0 +1,46 @@
+<template>
+  <component-events
+    :events="events"
+  />
+</template>
+
+<script>
+import Prism from 'prismjs';
+export default {
+  name: 'FlowSaveFailedPopupDocs',
+  data() {
+    return {
+      events: [
+        {
+          value: 'close-popup',
+          description: 'Needed to close popup',
+        },
+        {
+          value: 'save',
+          description: 'Fires at "Retry" button inside popup',
+        },
+        {
+          value: 'download',
+          description: 'Fires at "Export to JSON" button inside popup',
+        },
+      ],
+    }
+  },
+  mounted() {
+    Prism.highlightAll();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.flow-save-filed-popup__wrapper {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+
+  .wt-icon {
+    margin-bottom: var(--spacing-sm);
+  }
+}
+</style>
