9fe3f4603aa9c367f0b325812027f7395c2e8a27
Daniil Lohvinov <nsblnr@gmail.com>
2023-11-29 17:56:54 +0200

    23.12.50-2: fixed useValidation work with reactive() v object [WTEL-3902]

--- src/mixins/validationMixin/useValidation.js
+++ src/mixins/validationMixin/useValidation.js
@@ -1,14 +1,27 @@
-import { computed } from 'vue';
+import { computed, isReactive, isRef } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-export function useValidation({ v, customValidators }) {
+export function useValidation({ v: inputV, customValidators: inputCustomValidators }) {
   const { t } = useI18n();
+
+  // support vue options api, where v is a reactive, not ref
+  let v = inputV;
+  let customValidators = inputCustomValidators;
+
+  if (isReactive(v)) {
+    v = computed(() => inputV);
+    customValidators = computed(() => inputCustomValidators);
+  }
+  // end
+
+  console.info(v, inputV, isRef(v), isReactive(inputV));
+
   const isValidation = computed(() => !!v && !!v.value && !!Object.keys(v.value).length);
   const invalid = computed(() => isValidation.value && v.value.$error);
 
   const validationText = computed(() => {
     let validationText = '';
-    if (isValidation.value && invalid) {
+    if (isValidation.value && invalid.value) {
       if (v.value.required?.$invalid) validationText = t('validation.required');
       else if (v.value.numeric?.$invalid) validationText = t('validation.numeric');
       else if (v.value.email?.$invalid) validationText = t('validation.email');
@@ -28,7 +41,7 @@ export function useValidation({ v, customValidators }) {
       else if (v.value.integer?.$invalid) validationText = `${t('validation.integer')}`;
     }
     // eslint-disable-next-line no-restricted-syntax
-    for (const { name, text } of customValidators) {
+    for (const { name, text } of customValidators.value) {
       if (v.value[name]?.$invalid) validationText = text;
     }
     return validationText;
