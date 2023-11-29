52948fdad68a76e440c42f80d7ce1e0df6322f6a
dlohvinov <nsblnr@gmail.com>
2023-11-29 18:27:01 +0200

    docs: wt-pagination docs [WTEL-3947]

---
+++ docs/pages/webitel-ui/components/wt-pagination/Readme.md
@@ -0,0 +1,20 @@
+<script setup>
+import Docs from './wt-pagination-docs.vue';
+import ExamplePagination from './examples/example-pagination.vue';
+</script>
+
+# WtPagination
+
+## Props
+::: raw
+<Docs/>
+:::
+
+## Example Pagination
+::: raw
+<ExamplePagination/>
+:::
+
+::: details Code
+<<< ./examples/example-pagination.vue
+:::
---
+++ docs/pages/webitel-ui/components/wt-pagination/examples/example-pagination.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const size = ref(10);
+const isNext = ref(true);
+const isPrev = ref(false);
+
+function alert(value) {
+  window.alert(value);
+}
+</script>
+
+<template>
+  <wt-pagination
+    :size="size"
+    :next="isNext"
+    :prev="isPrev"
+    @input="size = $event"
+    @next="alert('next')"
+    @prev="alert('prev')"
+  />
+</template>
+
+<style scoped lang="scss">
+
+</style>
