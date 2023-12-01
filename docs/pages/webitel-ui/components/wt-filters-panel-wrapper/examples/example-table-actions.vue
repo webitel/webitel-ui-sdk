aa7f790859ba9da5ff4fe326d5c0ce136ca1dc84
Daniil Lohvinov <nsblnr@gmail.com>
2023-12-01 11:53:13 +0200

    docs: wt-table-actions [WTEL-3931]

---
+++ docs/pages/webitel-ui/components/wt-filters-panel-wrapper/examples/example-table-actions.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <wt-filters-panel-wrapper>
+    <wt-select label="filter" />
+    <wt-select label="filter" />
+    <wt-select label="filter" />
+    <wt-select label="filter" />
+    <wt-select label="filter" />
+    <wt-select label="filter" />
+    <wt-select label="filter" />
+  </wt-filters-panel-wrapper>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- docs/pages/webitel-ui/components/wt-filters-panel-wrapper/wt-table-actions-docs.vue
+++ docs/pages/webitel-ui/components/wt-filters-panel-wrapper/wt-table-actions-docs.vue
@@ -1,32 +1,10 @@
 <template>
-  <section>
-    <h2>Filters panel wrapper</h2>
-    <article>
-      <div class="example-wrapper">
-        <wt-filters-panel-wrapper>
-          <wt-select label="filter" />
-          <wt-select label="filter" />
-          <wt-select label="filter" />
-          <wt-select label="filter" />
-          <wt-select label="filter" />
-          <wt-select label="filter" />
-          <wt-select label="filter" />
-        </wt-filters-panel-wrapper>
-        <pre><code class="language-html">
-          /* there's 7 select filters */
-          &lt;wt-filters-panel-wrapper
-             wt-select*7
-          &gt;&lt;/wt-filters-panel-wrapper&gt;
-        </code></pre>
-      </div>
-    </article>
-    <component-events
-      :events="events"
-    />
-    <component-slots
-      :slots="slots"
-    />
-  </section>
+  <component-events
+    :events="events"
+  />
+  <component-slots
+    :slots="slots"
+  />
 </template>
 
 <script>
