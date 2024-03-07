282736e5cfdfd0b58ae7c108f01469f867bee5af
Lera24 <bedulinavalera21@gmail.com>
2024-03-07 16:43:04 +0200

    @24.4.06-feature: add chatGatewayProvider enum[WTEL-3529]

---
+++ src/enums/ChatGatewayProvider/ChatGatewayProvider.enum.js
@@ -0,0 +1,10 @@
+const ChatGatewayProvider = Object.freeze({
+  TELEGRAM_BOT: 'telegram',
+  TELEGRAM_APP: 'gotd',
+  MESSENGER: 'messenger',
+  VIBER: 'viber',
+  WEBCHAT: 'webchat',
+  INFOBIP: 'infobip_whatsapp',
+});
+
+export default ChatGatewayProvider;
