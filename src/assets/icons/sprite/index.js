06eef4848e17ab37e1cfbe48b3f6196d6249de32
Sviatoslav Bar <baterfild2011@gmail.com>
2024-07-23 21:41:04 +0300

    Fix/ add custom chat icon [WTEL-4670]

--- src/assets/icons/sprite/index.js
+++ src/assets/icons/sprite/index.js
@@ -89,7 +89,7 @@ import './menu.svg';
 import './email.svg';
 import './mail.svg';
 import './mail--color.svg';
-import './custom.svg';
+import './messenger-custom.svg';
 import './messenger-facebook.svg';
 import './messenger-facebook--filled.svg';
 import './messenger-infobip.svg';
