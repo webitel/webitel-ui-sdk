eb45ef02ee02fb6d44d6c422ef2555b253146757
Sviatoslav Bar <baterfild2011@gmail.com>
2024-08-07 14:26:12 +0300

    Feat/ crm configuration enums

--- src/enums/WebitelApplications/WebitelApplications.enum.js
+++ src/enums/WebitelApplications/WebitelApplications.enum.js
@@ -1,11 +1,12 @@
 const WebitelApplications = Object.freeze({
-  ADMIN: 'admin',
-  AGENT: 'agent',
-  SUPERVISOR: 'supervisor',
-  HISTORY: 'history',
-  AUDIT: 'audit',
-  ANALYTICS: 'grafana',
-  CRM: 'crm',
+	ADMIN: "admin",
+	AGENT: "agent",
+	SUPERVISOR: "supervisor",
+	HISTORY: "history",
+	AUDIT: "audit",
+	ANALYTICS: "grafana",
+	CRM: "crm",
+	CONTACTS: "contacts",
 });
 
 export default WebitelApplications;
