5a0ab3f1395443ac85625d70c53c8bdfabd1e279
liza-pohranichna <haemoglobin0@gmail.com>
2024-09-06 18:35:46 +0300

    feature: added new component wt-provider-info [WTEL-4982]

---
+++ src/components/wt-provider-info/wt-provider-info.vue
@@ -0,0 +1,39 @@
+<template>
+  <article class="provider-info">
+    <wt-icon :icon="iconType[props.provider]" />
+    <p> {{ $t(`objects.messengers.${props.provider}`) }} </p>
+  </article>
+</template>
+
+<script setup>
+
+import ChatGatewayProvider from '../../../src/enums/ChatGatewayProvider/ChatGatewayProvider.enum.js';
+
+const props = defineProps({
+  provider: {
+    type: String,
+    default: '',
+  },
+});
+
+const iconType = {
+  [ChatGatewayProvider.TELEGRAM_BOT]: 'telegram-bot',
+  [ChatGatewayProvider.TELEGRAM_APP]: 'messenger-telegram',
+  [ChatGatewayProvider.MESSENGER]: 'meta',
+  [ChatGatewayProvider.VIBER]: 'messenger-viber',
+  [ChatGatewayProvider.WEBCHAT]: 'messenger-web-chat',
+  [ChatGatewayProvider.INFOBIP]: 'messenger-infobip',
+  [ChatGatewayProvider.CUSTOM]: 'custom-chat-gateway',
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+.provider-info {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+</style>
