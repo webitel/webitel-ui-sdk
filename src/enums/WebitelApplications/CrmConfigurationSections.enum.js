bf3d4da36378b933c474ca8e2ad23391bfc74c33
Sviatoslav Bar <baterfild2011@gmail.com>
2024-09-12 16:55:30 +0300

    feat/ fix brackets [WTEL-4740]

--- src/enums/WebitelApplications/CrmConfigurationSections.enum.js
+++ src/enums/WebitelApplications/CrmConfigurationSections.enum.js
@@ -1,13 +1,13 @@
 const CrmConfigurationSections = Object.freeze({
-	LOOKUPS: "lookups",
-	SLA: "sla",
-	SERVICE_CATALOG: "service-catalog",
-	PRIORITIES: "priorities",
-	STATUSES: "statuses",
-	CLOSURE_REASONS: "closure-reasons",
-	CONTACT_GROUPS: "contact-groups",
-	CASE_SOURCES: "case-sources",
-	CUSTOM_LOOKUPS: "custom-lookups",
+	LOOKUPS: 'lookups',
+	SLA: 'sla',
+	SERVICE_CATALOG: 'service-catalog',
+	PRIORITIES: 'priorities',
+	STATUSES: 'statuses',
+	CLOSURE_REASONS: 'closure-reasons',
+	CONTACT_GROUPS: 'contact-groups',
+	CASE_SOURCES: 'case-sources',
+	CUSTOM_LOOKUPS: 'custom-lookups',
 });
 
 export default CrmConfigurationSections;
