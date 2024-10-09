f0a217287e30af0ba035b8d3514fecc6b0ff0f6d
Sviatoslav Bar <baterfild2011@gmail.com>
2024-10-09 16:40:00 +0300

    feat/ move images to project [WTEL-4754]

--- src/modules/StartPage/components/the-start-page.vue
+++ src/modules/StartPage/components/the-start-page.vue
@@ -1,27 +1,20 @@
 <template>
-  <start-page-logo :image="logo"/>
-  <div class="start-page-wrapper">
-    <start-page-card
-      v-for="(card) of navCards"
-      :key="card.value"
-      :card="card"
-    />
-  </div>
+<start-page-logo :image="logo"/>
+<div class="start-page-wrapper">
+  <start-page-card
+    v-for="(card) of navCards"
+    :key="card.value"
+    :card="card"
+  />
+</div>
 </template>
 
 <script setup>
   import { computed } from 'vue';
   import { useI18n } from 'vue-i18n';
   import { useStore } from 'vuex';
-  import CrmSections from '@webitel/ui-sdk/src/enums/WebitelApplications/CrmSections.enum';
   import StartPageCard from './start-page-card.vue';
   import StartPageLogo from './start-page-logo.vue';
-  import ContactsSecLight from '../assets/contacts-section-light.svg';
-  import ContactsSecDark from '../assets/contacts-section-dark.svg';
-  import ConfigurationSecLight from '../assets/configuration-section-light.svg';
-  import ConfigurationSecDark from '../assets/configuration-section-dark.svg';
-  import StartLogoLight from '../assets/start-page-logo-light.svg';
-  import StartLogoDark from '../assets/start-page-logo-dark.svg';
 
   // Vuex store access
   const store = useStore();
@@ -33,25 +26,17 @@
       type: Array,
       required: true,
     },
+    logoPath: {
+      type: Object,
+      required: true,
+    }
   });
 
-  const theme = computed(() => store.state.appearance.theme);
   const darkMode = computed(() => store.getters['appearance/DARK_MODE']);
 
   const logo = computed(() => {
-    return darkMode.value ? StartLogoDark : StartLogoLight;
+    return darkMode.value ? props.logoPath.dark : props.logoPath.light;
   });
-
-  const cardSectionPic = {
-    [CrmSections.CONTACTS]: {
-      dark: ContactsSecDark,
-      light: ContactsSecLight,
-    },
-    [CrmSections.CONFIGURATION]: {
-      dark: ConfigurationSecDark,
-      light: ConfigurationSecLight,
-    },
-  };
 
   const navCards = computed(() => {
     return props.nav.map((navItem) => ({
@@ -59,7 +44,7 @@
       disabled: false, // Статус доступу до навігаційних карток
       name: t(`startPage.${navItem.value}.name`),
       text: t(`startPage.${navItem.value}.text`),
-      image: darkMode.value ? cardSectionPic[navItem.value].dark : cardSectionPic[navItem.value].light,
+      image: darkMode.value ? navItem.images.dark : navItem.images.light,
     }));
   });
 </script>
