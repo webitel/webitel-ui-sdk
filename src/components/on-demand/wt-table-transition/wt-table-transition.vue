b9729f735fde82100dc1fa99adbe90e804d18715
Sviatoslav Bar <baterfild2011@gmail.com>
2024-10-17 11:24:38 +0300

    feat/ table transition [WTEL-5306]

---
+++ src/components/on-demand/wt-table-transition/wt-table-transition.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    style="display:contents;"
+  >
+    <transition-slide
+      :offset="{
+              enter: ['-50%', 0],
+              leave: [0, 0]
+            }"
+      duration="5000"
+      mode="out-in"
+      appear
+    >
+    <slot />
+    </transition-slide>
+  </div>
+</template>
+
+<script setup>
+  import { TransitionSlide } from '@morev/vue-transitions';
+</script>
+
+<style lang="scss" scoped>
+</style>
