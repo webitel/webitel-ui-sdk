bd6757bb58edd4c7f0e53c5d6cc9c0466069d8a0
liza-pohranichna <haemoglobin0@gmail.com>
2024-11-15 17:33:42 +0200

    fix: added getFileUrl script [WTEL-5360](https://webitel.atlassian.net/browse/WTEL-5360)

---
+++ src/scripts/getFileUrl.js
@@ -0,0 +1,14 @@
+const getFileUrl = ({ id, type }) => {
+  const token = localStorage.getItem('access-token');
+
+  let link = `${import.meta.env.VITE_API_URL}/storage/file/${id}/download?access_token=${token}`;
+
+  if (type.includes('source')) {
+    const source = type.match(/source=[^;]+/)[0];
+    link = `${link}&${source}`;
+  }
+
+  return link;
+};
+
+export default getFileUrl;
