d189803bb4a0d86cb22718c10ca856c6b0d98e5b
liza-pohranichna <haemoglobin0@gmail.com>
2024-11-27 20:10:17 +0200

    feature: added docs for wt-intersection-observer and fixed type of observer prop [WTEL-5260](https://webitel.atlassian.net/browse/WTEL-5260)

---
+++ docs/pages/webitel-ui/components/wt-intersection-observer/Readme.md
@@ -0,0 +1,16 @@
+<script setup>
+import Specs from './component-specs.vue';
+</script>
+
+# WtIntersectionObserver
+
+## Specs
+<Specs />
+
+## Example Intersection Observer
+
+::: details Code
+
+<<< ./examples/intersection-observer.vue
+
+:::
---
+++ docs/pages/webitel-ui/components/wt-intersection-observer/examples/intersection-observer.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+const nextLoading = ref(false);
+const next = ref(true);
+
+const loadNext = async () => {
+  nextLoading.value = true;
+  console.log('observer was triggered');
+  nextLoading.value = false;
+}
+
+</script>
+
+<template>
+  <wt-intersection-observer
+    :next="next"
+    :loading="nextLoading"
+    @next="loadNext"
+  />
+</template>
+
+<style scoped lang="scss">
+
+</style>
