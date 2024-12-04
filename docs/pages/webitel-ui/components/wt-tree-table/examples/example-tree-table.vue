3085b750667f7b8626e1f3cf69b62179964a8d8a
Stanislav Kozak <s.kozak@webitel.com>
2024-12-04 11:17:24 +0200

    feat: implement docs for tree table [WTEL-5569](https://webitel.atlassian.net/browse/WTEL-5569)

---
+++ docs/pages/webitel-ui/components/wt-tree-table/examples/example-tree-table.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue';
+import service from './service.json'
+
+const services = ref([...service.items])
+
+const dataHeaders = ref([
+  {
+    "value": "name",
+    "locale": "objects.name",
+    "width": "320px",
+    "field": "name",
+    "sort": null
+  },
+  {
+    "value": "code",
+    "locale": "objects.code",
+    "width": "160px",
+    "field": "code",
+    "sort": null
+  },
+  {
+    "value": "description",
+    "locale": "objects.description",
+    "width": "320px",
+    "field": "presence",
+    "sort": null
+  },
+])
+</script>
+
+<template>
+  <wt-tree-table
+    :headers="dataHeaders"
+    :data="services"
+    children="service"
+    selectable
+    sortable
+  >
+  </wt-tree-table>
+</template>
