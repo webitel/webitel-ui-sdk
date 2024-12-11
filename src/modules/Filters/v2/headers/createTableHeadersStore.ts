f13b38496c53605a10c57cb379d6c7dfa51a7ad7
dlohvinov <nsblnr@gmail.com>
2024-12-11 22:52:00 +0200

    feat: pinia headers store updateSort method, + refactored "sort" computed [WTEL-5622]

--- src/modules/Filters/v2/headers/createTableHeadersStore.ts
+++ src/modules/Filters/v2/headers/createTableHeadersStore.ts
@@ -1,15 +1,18 @@
 import {computed, ref} from "vue";
 import {defineStore} from "pinia";
 import { sortToQueryAdapter } from "../../../../scripts";
+import {SortSymbols} from "../../../../scripts/sortQueryAdapters";
 
-export const createTableHeadersStore = (namespace: string) => {
+export const createTableHeadersStore = (namespace: string, {
+    headers: rawHeaders,
+}) => {
     const id = `${namespace}/headers`;
 
     return defineStore(id, () => {
-        const headers = ref([]);
+        const headers = ref(rawHeaders);
 
         const shownHeaders = computed(() => {
-            return headers.value.map((header) => header.show);
+            return headers.value.filter((header) => header.show);
         });
 
         const fields = computed(() => {
@@ -17,12 +20,57 @@ export const createTableHeadersStore = (namespace: string) => {
         });
 
         const sort = computed(() => {
-            return headers.value.find((header) => sortToQueryAdapter(header.sort));
+            const encodeSortQuery = ({ column, order }) => `${sortToQueryAdapter(order)}${column.field}`;
+
+            const sortedCol = headers.value.find((header) => header.sort);
+
+            return sortedCol ? encodeSortQuery({ column: sortedCol, order: sortedCol.sort }) : null;
         });
 
         const updateShownHeaders = () => {};
 
-        const updateSort = () => {};
+        const updateSort = (column) => {
+            const getNextSortOrder = (sort) => {
+                switch (sort) {
+                    case SortSymbols.NONE:
+                        return SortSymbols.ASC;
+                    case SortSymbols.ASC:
+                        return SortSymbols.DESC;
+                    case SortSymbols.DESC:
+                        return SortSymbols.NONE;
+                    default:
+                        return SortSymbols.ASC;
+                }
+            };
+
+            const changeHeadersSort = ({ headers, sortedHeader, order }) => {
+                return headers.map((header) => {
+                    if (header.sort === undefined) return header;
+
+                    // reset all headers by default
+                    let newSort = null;
+
+                    if (header.field === sortedHeader.field) {
+                        newSort = order;
+                    }
+
+                    return {
+                        ...header,
+                        sort: newSort,
+                    };
+                });
+            };
+
+            const order = getNextSortOrder(column.sort);
+
+            const newHeaders = changeHeadersSort({
+                headers: headers.value,
+                sortedHeader: column,
+                order,
+            });
+
+            headers.value = newHeaders;
+        };
 
         return {
             headers,
