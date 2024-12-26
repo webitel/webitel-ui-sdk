e4e58bef0736f12f746b6de7e2d2cb17928ec41a
Stanislav Kozak <s.kozak@webitel.com>
2024-12-26 15:41:30 +0200

    feat: update set key for child elements [WTEL-5569](https://webitel.atlassian.net/browse/WTEL-5569)

--- src/components/wt-tree-table-row/wt-tree-table-row.vue
+++ src/components/wt-tree-table-row/wt-tree-table-row.vue
@@ -1,36 +1,29 @@
 <template>
-  <tr
-    class="wt-tree-table-row"
-    :class="[
-      { 'wt-tree-table-row--alternate': rowPosition % 2 }
-    ]"
-  >
-    <td
-      v-for="(col, headerKey) of dataHeaders"
-      :key="headerKey"
-      class="wt-tree-table-td"
-    >
+  <tr class="wt-tree-table-row" :class="[{ 'wt-tree-table-row--alternate': rowPosition % 2 }]">
+    <td v-for="(col, headerKey) of dataHeaders" :key="headerKey" class="wt-tree-table-td">
       <div class="wt-tree-table-td__content">
-        <div
-          v-if="!headerKey"
-          class="wt-tree-table-td__icon-wrapper"
-        >
-          <div v-for="treeLine in lineCount"
-               :key="treeLine" class="wt-tree-table-row__tree-space"></div>
+        <div v-if="!headerKey" class="wt-tree-table-td__icon-wrapper">
+          <div
+            v-for="treeLine in lineCount"
+            :key="treeLine"
+            class="wt-tree-table-row__tree-space"
+          ></div>
           <!-- This two empty icons need to create space for nested elements -->
           <wt-icon-btn
             v-if="!data[childrenProp]"
-            :class="{'hidden': !data[childrenProp]}"
+            :class="{ hidden: !data[childrenProp] }"
             :icon="collapsed ? 'tree-expand' : 'tree-collapse'"
             @click="collapsed = !collapsed"
           />
           <wt-checkbox
             v-if="selectable"
             :selected="isSelectedRow"
-            @change="$emit('update:selected', {
-              data,
-              select: $event
-            })"
+            @change="
+              $emit('update:selected', {
+                data,
+                select: $event,
+              })
+            "
           />
           <wt-icon-btn
             v-if="data[childrenProp]"
@@ -38,34 +31,23 @@
             @click="collapsed = !collapsed"
           />
         </div>
-        <slot
-          :index="rowPosition"
-          :item="data"
-          :name="col.value"
-        >
+        <slot :index="rowPosition" :item="data" :name="col.value">
           {{ data[col.value] }}
         </slot>
       </div>
     </td>
 
-    <td
-      v-if="gridActions"
-      class="wt-tree-table-td__actions"
-    >
+    <td v-if="gridActions" class="wt-tree-table-td__actions">
       <div class="wt-tree-table-td__content">
-        <slot
-          :index="rowPosition"
-          :item="data"
-          name="actions"
-        />
+        <slot :index="rowPosition" :item="data" name="actions" />
       </div>
     </td>
   </tr>
 
   <template v-if="!collapsed">
     <wt-tree-table-row
-      v-for="child in data[childrenProp]"
-      :key="child.id"
+      v-for="(child, index) in data[childrenProp]"
+      :key="index"
       :row-position="rowPosition"
       :data-headers="dataHeaders"
       :data="child"
@@ -73,40 +55,30 @@
       :selected-elements="selectedElements"
       :children-prop="childrenProp"
       :nesting-level="nestingLevel + 1"
-      @update:selected="$emit('update:selected', {
-        data: $event.data,
-        select: $event.select
-      })"
+      @update:selected="
+        $emit('update:selected', {
+          data: $event.data,
+          select: $event.select,
+        })
+      "
     >
-      <template
-          v-for="(col, headerKey) of dataHeaders"
-          :key="headerKey"
-          #[col.value]="{ item }"
-      >
-        <slot
-          :index="rowPosition"
-          :item="item"
-          :name="col.value"
-        >
+      <template v-for="(col, headerKey) of dataHeaders" :key="headerKey" #[col.value]="{ item }">
+        <slot :index="rowPosition" :item="item" :name="col.value">
           <div>{{ item[col.value] }}</div>
         </slot>
       </template>
       <template #actions="{ item }">
-        <slot
-          :index="rowPosition"
-          :item="item"
-          name="actions"
-        />
+        <slot :index="rowPosition" :item="item" name="actions" />
       </template>
     </wt-tree-table-row>
   </template>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import WtCheckbox from '../wt-checkbox/wt-checkbox.vue';
-import WtIconBtn from '../wt-icon-btn/wt-icon-btn.vue';
-import type { TableHeader } from '../wt-table/types/table-header.ts';
+import { computed, ref } from "vue";
+import WtCheckbox from "../wt-checkbox/wt-checkbox.vue";
+import WtIconBtn from "../wt-icon-btn/wt-icon-btn.vue";
+import type { TableHeader } from "../wt-table/types/table-header.ts";
 
 const props = withDefaults(
   defineProps<{
@@ -138,7 +110,7 @@ const props = withDefaults(
   },
 );
 
-defineEmits(['update:selected']);
+defineEmits(["update:selected"]);
 
 const collapsed = ref(true);
 const lineCount = computed(() => {
@@ -151,7 +123,7 @@ const isSelectedRow = computed(() => {
 </script>
 
 <style lang="scss" scoped>
-@import '../../../src/css/main.scss';
+@import "../../../src/css/main.scss";
 
 .wt-tree-table-td {
   @extend %typo-body-1;
@@ -190,7 +162,7 @@ const isSelectedRow = computed(() => {
   }
 
   &--alternate {
-    background: var(--wt-tree-table-zebra-color)
+    background: var(--wt-tree-table-zebra-color);
   }
 }
 </style>
