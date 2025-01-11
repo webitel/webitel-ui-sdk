8fdfc0e9593ed48d937f6babe7637df67b32799a
Олександр Пальонний <s2567k@gmail.com>
2025-01-11 18:31:36 +0200

    attempt to fix plyr.scss errors

--- vite.config.ts
+++ vite.config.ts
@@ -103,17 +103,17 @@ export default ({ mode }) => {
           },
           // DETAILED ANSWER FOR THIS STRANGE STRUCTURE
           // FOR additionalData CAN BE FOUND AT THE LINK https://webitel.atlassian.net/browse/WTEL-5778?focusedCommentId=647516
-          additionalData: (content, filename) => {
-            // Suppress warnings for specific files or packages
-            if (/node_modules\/plyr/.test(filename)) {
-              return `
-              @import "plyr/src/sass/plyr.scss";
-              /* suppress warnings for this file */
-              $disable-import-warning: true;
-            `;
-            }
-            return content;
-          },
+          // additionalData: (content, filename) => {
+          //   // Suppress warnings for specific files or packages
+          //   if (/node_modules\/plyr/.test(filename)) {
+          //     return `
+          //     @import "plyr/src/sass/plyr.scss";
+          //     /* suppress warnings for this file */
+          //     $disable-import-warning: true;
+          //   `;
+          //   }
+          //   return content;
+          // },
         },
       },
     },
