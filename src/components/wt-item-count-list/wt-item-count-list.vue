8c5325a5e8ea60ac3ebe0ed59bd5f7acf13c0175
liza-pohranichna <haemoglobin0@gmail.com>
2025-01-28 17:38:01 +0200

    features: added new component wt-items-count-list  [WTEL-6105] (https://webitel.atlassian.net/browse/WTEL-6105)

---
+++ src/components/wt-item-count-list/wt-item-count-list.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wt-items-count-list">
+    <wt-tooltip
+      v-if="props.list.length"
+      :triggers="props.triggers"
+    >
+      <template #activator>
+        <wt-chip class="wt-items-count-list__count">
+          +{{ props.list.length }}
+        </wt-chip>
+      </template>
+      <slot name="list">
+        <p
+          v-for="item of list"
+          :key="item"
+        >
+          {{ item }}
+        </p>
+      </slot>
+    </wt-tooltip>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  list: {
+    type: Array,
+    required: true,
+  },
+  triggers: {
+    type: Array,
+    default: () => ['click'],
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.wt-items-count-list {
+  cursor: pointer;
+
+  &__count {
+    text-wrap: nowrap;
+  }
+}
+</style>
