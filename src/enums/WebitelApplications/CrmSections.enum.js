d225fda3507e27966b2f13e26941d641d26bb331
Daniil Lohvinov <nsblnr@gmail.com>
2025-01-30 12:42:19 +0200

    fix: wt applications enums case conversion [WTEL-5592](https://webitel.atlassian.net/browse/WTEL-5592)

--- src/enums/WebitelApplications/CrmSections.enum.js
+++ src/enums/WebitelApplications/CrmSections.enum.js
@@ -1,3 +1,5 @@
+import upperCase from 'lodash/upperCase.js';
+
 import { CrmSections } from './CrmSections';
 
 /**
@@ -6,5 +8,7 @@ import { CrmSections } from './CrmSections';
  * use ts enum instead (and don't forget to compile it)
  */
 export default Object.fromEntries(
-  Object.entries(CrmSections).map(([key, value]) => [key.toUpperCase(), value]),
+  Object.entries(CrmSections).map(([key, value]) => [
+    upperCase(key).replaceAll(' ', '_'),
+    value]),
 );
