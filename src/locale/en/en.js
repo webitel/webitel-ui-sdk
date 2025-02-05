964d3bea4b23e371f8b1a14021994bab34fd4b0a
Stas Kozak <kozak.stasan@gmail.com>
2025-02-05 10:08:33 +0200

    feat: implement locale for object [WTEL-5991](https://webitel.atlassian.net/browse/WTEL-5991)

--- src/locale/en/en.js
+++ src/locale/en/en.js
@@ -25,6 +25,7 @@ export default {
     filter: ({ plural }) => plural(['Filter', 'Filters']),
     total: 'Total',
     ok: 'Ok',
+    object: 'Object',
     save: 'Save',
     saveAs: 'Save as',
     saved: 'Saved',
