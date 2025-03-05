e6f722cf41ec81ed5e67765574c38f9842134aa8
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-03-05 18:50:54 +0200

    feat: orval config updates, added zod as project dep [WTEL-6397](https://webitel.atlassian.net/browse/WTEL-6397)

--- orval.config.ts
+++ orval.config.ts
@@ -6,26 +6,25 @@ export default defineConfig({
       target:
         'https://raw.githubusercontent.com/webitel/protos/main/swagger/api.json',
     },
-    // input: 'https://raw.githubusercontent.com/webitel/protos/main/swagger/api.json',
     output: {
       target: './src/api/orval',
       client: 'axios',
       mode: 'tags-split',
-      schemas: './_schemas',
+      // schemas: './src/api/schemas',
       override: {
-        // requestOptions: { smth11: 'smth' },
-        operationName: ({ operationId: operationName}) => {
-          if (operationName.includes('ServiceSearch')) {
+        operationName: ({operationId: operationName}) => {
+          // console.info(operationName);
+          if (operationName.includes('Search')) {
             return 'getList';
-          } else if (operationName.includes('ServiceRead')) {
+          } else if (operationName.includes('Read')) {
             return 'get';
-          } else if (operationName.includes('ServiceCreate')) {
+          } else if (operationName.includes('Create')) {
             return 'add';
-          } else if (operationName.includes('ServiceUpdate')) {
+          } else if (operationName.includes('Update')) {
             return 'update';
-          } else if (operationName.includes('ServiceDelete')) {
+          } else if (operationName.includes('Delete')) {
             return 'delete';
-          } else if (operationName.includes('ServicePatch')) {
+          } else if (operationName.includes('Patch')) {
             return 'patch';
           }
 
@@ -35,11 +34,61 @@ export default defineConfig({
         // transformer: (options: GeneratorVerbOptions): GeneratorVerbOptions => {
         //   console.info(JSON.stringify(options, null, 2));
         //   return options;
-        },
+        // },
+      },
     },
 
     hooks: {
       // afterAllFilesWrite: 'prettier --write',
     },
   },
+  zod: {
+    input: {
+      target:
+        // 'https://raw.githubusercontent.com/webitel/protos/main/swagger/api.json',
+        './openapiv2.json',
+    },
+    output: {
+      target: './src/api/orval/_zod',
+      client: 'zod',
+      mode: 'tags-split',
+      // schemas: './src/api/schemas',
+      override: {
+        zod: {
+          default: true,
+          coerce: {
+            response: [ 'boolean'],
+            query: ['string', 'number', 'boolean', 'bigint', 'date'],
+          },
+        },
+        operationName: ({operationId: operationName}) => {
+          // console.info(operationName);
+          if (operationName.includes('Search')) {
+            return 'getList';
+          } else if (operationName.includes('Read')) {
+            return 'get';
+          } else if (operationName.includes('Create')) {
+            return 'add';
+          } else if (operationName.includes('Update')) {
+            return 'update';
+          } else if (operationName.includes('Delete')) {
+            return 'delete';
+          } else if (operationName.includes('Patch')) {
+            return 'patch';
+          }
+
+          return operationName;
+        },
+        // NOTE: Use this to debug transformed options
+        // transformer: (options: GeneratorVerbOptions): GeneratorVerbOptions => {
+        //   console.info(JSON.stringify(options, null, 2));
+        //   return options;
+        // },
+      },
+    },
+
+    hooks: {
+      // afterAllFilesWrite: 'prettier --write',
+    },
+  }
 });
