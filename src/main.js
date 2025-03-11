c0407c0d596a5604fcf90eb3f8e2aedc2975b6d4
Stas Kozak <kozak.stasan@gmail.com>
2025-03-11 17:34:58 +0200

    feat: add primevue to project [WTEL-6394](https://webitel.atlassian.net/browse/WTEL-6394)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import '@webitel/styleguide/fonts';
 import 'vue-multiselect/dist/vue-multiselect.css';
 import './assets/icons/sprite';
 
+import { definePreset } from '@primeuix/themes';
+import Aura from '@primeuix/themes/aura';
+import PrimeVue from 'primevue/config';
 import { createApp } from 'vue';
 
 import Components from './components/index.js'; // init all components
@@ -19,6 +22,29 @@ Object.keys(Components).forEach((name) => {
 
 Object.keys(Directives).forEach((name) => {
   app.directive(name, Directives[name]);
+});
+
+const WebitelTheme = definePreset(Aura, {
+  primary: {
+    50: '{indigo.50}',
+    100: '{indigo.100}',
+    200: '{indigo.200}',
+    300: '{indigo.300}',
+    400: '{indigo.400}',
+    500: '{indigo.500}',
+    600: '{indigo.600}',
+    700: '{indigo.700}',
+    800: '{indigo.800}',
+    900: '{indigo.900}',
+    950: '{indigo.950}',
+  },
+});
+
+app.use(PrimeVue, {
+  prefix: 'p',
+  theme: {
+    preset: WebitelTheme,
+  },
 });
 
 app.mount('#app');
--- src/playground.vue
+++ src/playground.vue
@@ -1,3 +1,12 @@
-<script setup></script>
+<script setup>
+import PButton from 'primevue/button';
 
-<template></template>
+import WtButton from './components/wt-button/wt-button.vue';
+</script>
+
+<template>
+  <div>
+    <wt-button>Test</wt-button>
+    <p-button>Test</p-button>
+  </div>
+</template>
