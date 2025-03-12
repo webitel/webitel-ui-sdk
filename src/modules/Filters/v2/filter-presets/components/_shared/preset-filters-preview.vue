75a5f2451fb78bc39fde22c5b57bb85ad2371ae7
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-03-12 17:09:41 +0200

    feat: filter presets moved to webitel-ui  [WTEL-3751(https://webitel.atlassian.net/browse/WTEL-3751), WTEL-3752(https://webitel.atlassian.net/browse/WTEL-3752)]
    (cherry picked from commit da418a6886246400cd5d71fc06846d654e03b9d2)

---
+++ src/modules/Filters/v2/filter-presets/components/_shared/preset-filters-preview.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="save-preset-filters-preview">
+    <wt-label>
+      {{ t('webitelUI.filters.filterName') }}
+    </wt-label>
+    <dynamic-filter-panel-wrapper>
+      <template #filters>
+        <dynamic-filter-preview
+          v-for="(filter) of props.filters"
+          :key="filter.name"
+          :filter="filter"
+          dummy
+        >
+          <template #info>
+            <component
+              :is="getFilterFieldComponent(filter.name, 'previewField')"
+              :value="filter.value"
+            >
+            </component>
+          </template>
+        </dynamic-filter-preview>
+      </template>
+    </dynamic-filter-panel-wrapper>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import {useI18n} from "vue-i18n";
+import DynamicFilterPreview
+  from "../../../filters/components/preview/dynamic-filter-preview.vue";
+import DynamicFilterPanelWrapper
+  from "../../../filters/components/dynamic-filter-panel-wrapper.vue";
+import type {IFilter} from "../../../filters/types/Filter.d.ts";
+import {getFilterFieldComponent} from "../../../filters/components/values/filterComponentsMap";
+
+type Props = {
+  filters: IFilter[];
+};
+
+const props = defineProps<Props>();
+
+const { t } = useI18n();
+
+</script>
+
+<style scoped lang="scss">
+.preset-filters-preview {
+
+}
+</style>
---
+++ src/modules/Filters/v2/filters/components/values/filterComponentsMap.ts
@@ -0,0 +1,147 @@
+import {
+  AgentFilter,
+  AgentFilterPreview,
+  AmdResultFilter,
+  AmdResultFilterPreview,
+  ContactFilter,
+  ContactFilterPreview,
+  CreatedAtFromFilter,
+  CreatedAtFromFilterPreview,
+  CreatedAtToFilter,
+  CreatedAtToFilterPreview,
+  DirectionFilter,
+  DirectionFilterPreview,
+  HasRatingFilterValueField,
+  HasRatingFilterValuePreview,
+  GatewayFilter,
+  GatewayFilterPreview,
+  GranteeFilter,
+  GranteeFilterPreview,
+  CauseFilter,
+  CauseFilterPreview,
+  QueueFilter,
+  QueueFilterPreview,
+  RatedByFilter,
+  RatedByFilterPreview,
+  HasFileFilter,
+  HasFileFilterPreview,
+  ScoreFilter,
+  ScoreFilterPreview,
+  TagFilter,
+  TagFilterPreview,
+  TalkDurationFilter,
+  TalkDurationFilterPreview,
+  TeamFilter,
+  TeamFilterPreview,
+  TotalDurationFilter,
+  TotalDurationFilterPreview,
+  HasTranscriptionFilter,
+  HasTranscriptionFilterPreview,
+  UserFilter,
+  UserFilterPreview,
+  VariableFilter,
+  VariableFilterPreview,
+} from '../index';
+import {Component} from "vue";
+import type {FilterName} from "../../../index";
+
+
+interface FilterOptionsComponentsConfig {
+  [key: string]: {
+    valueField: Component
+    previewField: Component
+  }
+}
+const filterOptionsComponentsConfig: FilterOptionsComponentsConfig = {
+  agent: {
+    valueField: AgentFilter,
+    previewField: AgentFilterPreview,
+  },
+  amdResult: {
+    valueField: AmdResultFilter,
+    previewField: AmdResultFilterPreview,
+  },
+  contact: {
+    valueField: ContactFilter,
+    previewField: ContactFilterPreview,
+  },
+  createdAtFrom: {
+    valueField: CreatedAtFromFilter,
+    previewField: CreatedAtFromFilterPreview,
+  },
+  createdAtTo: {
+    valueField: CreatedAtToFilter,
+    previewField: CreatedAtToFilterPreview,
+  },
+  direction: {
+    valueField: DirectionFilter,
+    previewField: DirectionFilterPreview,
+  },
+  rated: {
+    valueField: HasRatingFilterValueField,
+    previewField: HasRatingFilterValuePreview,
+  },
+  gateway: {
+    valueField: GatewayFilter,
+    previewField: GatewayFilterPreview,
+  },
+  grantee: {
+    valueField: GranteeFilter,
+    previewField: GranteeFilterPreview,
+  },
+  cause: {
+    valueField: CauseFilter,
+    previewField: CauseFilterPreview,
+  },
+  queue: {
+    valueField: QueueFilter,
+    previewField: QueueFilterPreview,
+  },
+  ratedBy: {
+    valueField: RatedByFilter,
+    previewField: RatedByFilterPreview,
+  },
+  hasFile: {
+    valueField: HasFileFilter,
+    previewField: HasFileFilterPreview,
+  },
+  score: {
+    valueField: ScoreFilter,
+    previewField: ScoreFilterPreview,
+  },
+  tag: {
+    valueField: TagFilter,
+    previewField: TagFilterPreview,
+  },
+  talkDuration: {
+    valueField: TalkDurationFilter,
+    previewField: TalkDurationFilterPreview,
+  },
+  team: {
+    valueField: TeamFilter,
+    previewField: TeamFilterPreview,
+  },
+  totalDuration: {
+    valueField: TotalDurationFilter,
+    previewField: TotalDurationFilterPreview,
+  },
+  hasTranscription: {
+    valueField: HasTranscriptionFilter,
+    previewField: HasTranscriptionFilterPreview,
+  },
+  user: {
+    valueField: UserFilter,
+    previewField: UserFilterPreview,
+  },
+  variable: {
+    valueField: VariableFilter,
+    previewField: VariableFilterPreview,
+  },
+}
+
+export const getFilterFieldComponent = (filterName: FilterName, filterField: 'valueField' | 'previewField') => {
+  const filter = filterOptionsComponentsConfig[filterName];
+  return !filter ? '' : filter[filterField] || '';
+};
+
+export default filterOptionsComponentsConfig;
