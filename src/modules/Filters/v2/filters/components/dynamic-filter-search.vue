1a4686b1cc21399782c55940754fae5bf05f2d00
Олександр Пальонний <s2567k@gmail.com>
2025-04-01 21:53:11 +0300

    added new functionality for dynamic-filter-search [WTEL-6531](https://webitel.atlassian.net/browse/WTEL-6531)

--- src/modules/Filters/v2/filters/components/dynamic-filter-search.vue
+++ src/modules/Filters/v2/filters/components/dynamic-filter-search.vue
@@ -1,9 +1,11 @@
 <template>
   <wt-search-bar
     :placeholder="t('reusable.search')"
-    :search-mode="props.searchMode"
-    :search-mode-options="searchModeOptions"
+    :search-mode="currentSearchMode"
+    :search-mode-options="searchModeOpts"
+    :hint="multisearch && currentSearchMode?.hint"
     :value="model"
+    :v="multisearch && v$"
     @input="model = $event"
     @search="handleSearch"
     @update:search-mode="onSearchModeChange($event.value)"
@@ -18,18 +20,24 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { useVuelidate } from '@vuelidate/core';
+import {computed} from 'vue';
 import { useI18n } from 'vue-i18n';
 
 import WtSearchBar from '../../../../../components/wt-search-bar/wt-search-bar.vue';
+import {FilterSearch} from '../index';
 
 type ModelValue = string;
 const model = defineModel<ModelValue>();
 
+type SearchModeOptions = FilterSearch[] | Record<string, string>
+
 const props = defineProps<{
   searchMode: string;
-  searchModeOptions: Record<string, string>;
+  searchModeOptions: SearchModeOptions;
   showTextSearchIcon?: boolean;
+  name?: string;
+  multisearch?: boolean
 }>();
 
 const emit = defineEmits<{
@@ -39,6 +47,39 @@ const emit = defineEmits<{
 
 const { t } = useI18n();
 
+const searchModeOpts = computed<FilterSearch[]>(() => {
+  if (props.multisearch && props.searchModeOptions.length) {
+    return props.searchModeOptions
+  }
+
+  return Object.values(props.searchModeOptions).map((mode) => {
+    return {
+      value: mode,
+      text: t(`filters.search.${mode}`),
+    };
+  })
+});
+
+const currentSearchMode = computed(() =>
+  searchModeOpts.value.find(({ value }) => value === props.searchMode),
+);
+
+const v$ =
+  props.multisearch &&
+  useVuelidate(
+    computed(() => {
+      return {
+        model: {
+          ...(currentSearchMode.value?.v || {}),
+        },
+      };
+    }),
+    { model },
+    { $autoDirty: true },
+  );
+
+if (v$) v$.value.$touch();
+
 const onSearchModeChange = (value: string) => {
   emit('update:search-mode', value);
   model.value = '';
@@ -47,15 +88,6 @@ const onSearchModeChange = (value: string) => {
 const handleSearch = () => {
   emit('handle-search', model.value);
 };
-
-const searchModeOptions = computed(() =>
-  Object.values(props.searchModeOptions).map((mode) => {
-    return {
-      value: mode,
-      text: t(`filters.search.${mode}`),
-    };
-  }),
-);
 </script>
 
 <style lang="scss" scoped></style>
