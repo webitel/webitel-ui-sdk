236868467f9f1ca30d3cbe42aa5f5c734d74a067
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-04-22 17:54:00 +0300

    refactor: gh workflows [WTEL-6702](https://webitel.atlassian.net/browse/WTEL-6702)

--- tests/config/config.js
+++ tests/config/config.js
@@ -2,5 +2,8 @@ import { config } from '@vue/test-utils';
 
 import WebitelUi from '../../src/install.ts';
 import i18n from '../../src/locale/i18n.js';
+import axiosMock from '../../src/tests/mocks/axiosMock';
 
 config.global.plugins = [[WebitelUi, {}], i18n];
+
+vi.doMock('axios', axiosMock());
