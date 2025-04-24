31667c2a12f8bb2c7be6ce70b0a0d50848c489f2
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-04-24 14:18:17 +0300

    feat: add type extension and case assignee filter configurations
    [WTEL-6702](https://webitel.atlassian.net/browse/WTEL-6702)

--- packages/ui-datalist/src/modules/filters/modules/filterConfig/components/case-assignee/index.ts
+++ packages/ui-datalist/src/modules/filters/modules/filterConfig/components/case-assignee/index.ts
@@ -1,30 +1,11 @@
-import { contacts as ContactsAPI } from '@webitel/ui-sdk/api/clients/index';
-
-import { WtSysTypeFilterConfig } from '../../classes/FilterConfig';
-import { FilterOption } from '../../enums/FilterOption';
 import CaseAssigneeFilterValueField from './case-assignee-filter-value-field.vue';
 import CaseAssigneeFilterValuePreview from './case-assignee-filter-value-preview.vue';
+import { createCaseAssigneeFilterConfig } from './filterConfig';
 
-class CaseAssigneeFilterConfig extends WtSysTypeFilterConfig {
-  readonly name = FilterOption.CaseAssignee;
-  valueInputComponent = CaseAssigneeFilterValueField;
-  valuePreviewComponent = CaseAssigneeFilterValuePreview;
+export {
+  CaseAssigneeFilterValueField,
+  CaseAssigneeFilterValuePreview,
+  createCaseAssigneeFilterConfig,
+};
 
-  searchRecords(
-    params: object,
-    { filterValue } = {},
-  ): Promise<{ items: unknown[]; next?: boolean }> {
-    const id =
-      params.id?.list /* general logic from dynamic-filter-preview.vue*/ ||
-      params.id /* wt-select options loadings */ ||
-      filterValue?.list; /* newest and coolest, but not implemented on all filters 🥲 */
-
-    return ContactsAPI.getLookup({
-      ...params,
-      id,
-    });
-  }
-}
-
-export const createCaseAssigneeFilterConfig = () =>
-  new CaseAssigneeFilterConfig();
+export type * from './filterConfig';
