e5ae8560d249c53d6dc74be6c7e61f824cf853d7
Олександр Пальонний <s2567k@gmail.com>
2025-05-12 20:55:46 +0300

    feat: added portal gateway protocol [WTEL-6785](https://webitel.atlassian.net/browse/WTEL-6785)

--- src/enums/ChatGatewayProvider/ChatGatewayProvider.enum.js
+++ src/enums/ChatGatewayProvider/ChatGatewayProvider.enum.js
@@ -6,6 +6,7 @@ const ChatGatewayProvider = Object.freeze({
   WEBCHAT: 'webchat',
   INFOBIP: 'infobip_whatsapp',
   CUSTOM: 'custom',
+  PORTAL: 'portal',
 });
 
 export default ChatGatewayProvider;
