e1893e2465482c8460a96b909529166b93c7e039
Stas Kozak <kozak.stasan@gmail.com>
2025-05-15 16:32:17 +0300

    feat: update button from option api to composition api. Implement compatConfig to primevue components [WTEL-6394](https://webitel.atlassian.net/browse/WTEL-6394)

--- src/plugins/primevue/primevue.plugin.js
+++ src/plugins/primevue/primevue.plugin.js
@@ -3,6 +3,12 @@ import PrimeVue from 'primevue/config';
 
 import WebitelTheme from './theme/webitel-theme.js';
 
+const changeComponentCompactMode = (component) => {
+  component.compatConfig = { MODE: 3 };
+
+  return component;
+};
+
 const initPrimevue = (app) => {
   app.use(PrimeVue, {
     theme: {
@@ -13,7 +19,7 @@ const initPrimevue = (app) => {
     },
   });
 
-  app.component('PButton', PButton);
+  app.component('PButton', changeComponentCompactMode(PButton));
 };
 
 export default initPrimevue;
