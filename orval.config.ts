aca0220d4e4d03ad0c55487008a32144202a15e8
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-05-15 16:32:02 +0300

    orval.config.ts in progress [WTEL-6397](https://webitel.atlassian.net/browse/WTEL-6397)

--- orval.config.ts
+++ orval.config.ts
@@ -9,152 +9,27 @@ import {
 } from '@orval/axios';
 import { defineConfig } from 'orval';
 
+const sharedOutputTarget = './src/api/orval';
+const sharedGenFileExtension = 'gen.ts';
+
 export default defineConfig({
   main: {
     input: {
       target:
         // 'https://raw.githubusercontent.com/webitel/protos/main/swagger/api.json',
-        '../protos/swagger/engine.swagger.json',
+        '../protos/swagger/cases.swagger.json',
     },
     output: {
-      target: './src/api/orval',
+      target: sharedOutputTarget,
+      fileExtension: `.${sharedGenFileExtension}`,
       // client: 'axios',
-      client: () => {
-        const axiosBuilder = builder({ type: 'axios' })();
-        return {
-          ...axiosBuilder,
-          dependencies: (...params) => {
-            /**
-             * // default is:
-             * // ISSUE: https://github.com/orval-labs/orval/discussions/1373#discussioncomment-12735345
-             *    //   CODE:    // https://github.com/orval-labs/orval/blob/a154264719ccc49b3ab95dadbb3d62513110d8c3/packages/axios/src/index.ts#L22
-             *  {
-             *               exports: [
-             *                 {
-             *                   name: 'Axios',
-             *                   default: true,
-             *                   values: true,
-             *                   syntheticDefaultImport: true,
-             *                 },
-             *                 { name: 'AxiosRequestConfig' },
-             *                 { name: 'AxiosResponse' },
-             *                 { name: 'CreateAxiosDefaults' },
-             *               ],
-             *               dependency: 'axios',
-             *             },
-             */
-            const defaultDeps = getAxiosDependencies(...params).map((dep) => {
-              // if (dep.dependency === 'axios') {
-              //   return {
-              //     ...dep,
-              //     dependency: 'axalias',
-              //   };
-              // }
-
-              return dep;
-            });
-            // return [...defaultDeps];
-
-            return [
-              // default is:
-              // ISSUE: https://github.com/orval-labs/orval/discussions/1373#discussioncomment-12735345
-              //   CODE:    // https://github.com/orval-labs/orval/blob/a154264719ccc49b3ab95dadbb3d62513110d8c3/packages/axios/src/index.ts#L22
-              {
-                exports: [
-                  // {
-                  //   name: 'Axios',
-                  //   default: true,
-                  //   values: true,
-                  //   syntheticDefaultImport: true,
-                  // },
-                  { name: 'AxiosRequestConfig' },
-                  { name: 'AxiosResponse' },
-                  { name: 'CreateAxiosDefaults' },
-                ],
-                dependency: 'axios',
-              },
-              {
-                exports: [
-                  {
-                    name: 'axios',
-                    default: true,
-                    values: true,
-                    syntheticDefaultImport: true,
-                  },
-                ],
-                dependency: 'axalias',
-              },
-            ];
-          },
-          header: (params) => `
-
-            // --- header start
-            ${generateAxiosHeader(params)}
-            // --- header end
-
-          `,
-          footer: (params) => `
-
-            // --- footer start
-            ${generateAxiosFooter(params)}
-            // --- footer end
-
-          `,
-          title: (title) => `
-
-            // --- title start
-            ${generateAxiosTitle(title)}
-            // --- title end
-
-          `,
-          // extraFiles: () => `
-          //
-          //   // --- extraFiles start
-          //   ${axiosBuilder.extraFiles}
-          //   // --- extraFiles end
-          //
-          // `,
-
-          // ...axiosBuilder,
-          // dependencies: () => {
-          // return [
-          //   {
-          //     exports: [
-          //       {
-          //         name: 'Axios',
-          //         default: true,
-          //         values: true,
-          //         syntheticDefaultImport: true,
-          //       },
-          //       { name: 'AxiosRequestConfig' },
-          //       { name: 'AxiosResponse' },
-          //       { name: 'CreateAxiosDefaults' },
-          //     ],
-          //     dependency: 'axios',
-          //   },
-          // ];
-          // },
-          // header: () => {
-          //   //             return `
-          //   // export const createApiClient = (config?: CreateAxiosDefaults) => {
-          //   //   const axios = Axios.create(config);
-          //   //
-          //   // `;
-          // },
-          // footer: (params) => {
-          //   const result = generateAxiosFooter(params);
-          //   return result.replace(
-          //     /return {(.+?)}/,
-          //     (_, captured) => `return {${captured}, axios}`,
-          //   );
-          // },
-        };
-        // const axiosBuilder = builder({ type: 'axios-functions' })();
-        //
-        // return axiosBuilder;
-      },
+      client: axiosClient,
+      mock: true,
       mode: 'tags-split',
+      clean: true,
+      namingConvention: 'camelCase',
       indexFiles: true,
+      schemas: './src/api/orval/_schemas',
       docs: {
         configPath: './typedoc.config.mjs',
       },
@@ -168,8 +43,25 @@ export default defineConfig({
       },*/
       // schemas: './src/api/schemas',
       override: {
+        // mutator: {
+        // path: './src/api/utilz/withValidation.ts',
+        // name: 'withValidation',
+        // },
+        // requestOptions: true,
         // transformer: (verb) => {
-        //   console.info(verb);
+        //   const mutatedVerb = { ...verb };
+        //   // Add operationId to the config
+        //   if (mutatedVerb.request?.implementation) {
+        //     mutatedVerb.request.implementation =
+        //       mutatedVerb.request.implementation.replace(
+        //         /({[^}]+})/,
+        //         `{ ...($1), __operationId: '${verb.operationId}' }`,
+        //       );
+        //   }
+        //   return mutatedVerb;
+        // },
+        // transformer: (verb) => {
+        //   console.info(JSON.stringify(verb, null, 2));
         //   console.info('\n\n\n\n\n');
         //   return verb;
         // },
@@ -207,11 +99,11 @@ export default defineConfig({
     input: {
       target:
         // 'https://raw.githubusercontent.com/webitel/protos/main/swagger/api.json',
-        '../protos/swagger/engine.swagger.json',
+        '../protos/swagger/cases.swagger.json',
     },
     output: {
-      target: './src/api/orval/_zod',
-      // target: './src/api/orval',
+      target: sharedOutputTarget,
+      fileExtension: `.zod.${sharedGenFileExtension}`,
       client: 'zod',
       mode: 'tags-split',
       // schemas: './src/api/schemas',
@@ -275,3 +167,158 @@ export default defineConfig({
     },
   },*/
 });
+
+function axiosClient() {
+  const axiosBuilder = builder({ type: 'axios' })();
+  return {
+    ...axiosBuilder,
+    dependencies: (...params) => {
+      /**
+       * // default is:
+       * // ISSUE: https://github.com/orval-labs/orval/discussions/1373#discussioncomment-12735345
+       *    //   CODE:    // https://github.com/orval-labs/orval/blob/a154264719ccc49b3ab95dadbb3d62513110d8c3/packages/axios/src/index.ts#L22
+       *  {
+       *               exports: [
+       *                 {
+       *                   name: 'Axios',
+       *                   default: true,
+       *                   values: true,
+       *                   syntheticDefaultImport: true,
+       *                 },
+       *                 { name: 'AxiosRequestConfig' },
+       *                 { name: 'AxiosResponse' },
+       *                 { name: 'CreateAxiosDefaults' },
+       *               ],
+       *               dependency: 'axios',
+       *             },
+       */
+      const defaultDeps = getAxiosDependencies(...params).map((dep) => {
+        // if (dep.dependency === 'axios') {
+        //   return {
+        //     ...dep,
+        //     dependency: 'axalias',
+        //   };
+        // }
+
+        return dep;
+      });
+      // return [...defaultDeps];
+
+      return [
+        // default is:
+        // ISSUE: https://github.com/orval-labs/orval/discussions/1373#discussioncomment-12735345
+        //   CODE:    // https://github.com/orval-labs/orval/blob/a154264719ccc49b3ab95dadbb3d62513110d8c3/packages/axios/src/index.ts#L22
+        {
+          exports: [
+            // {
+            //   name: 'Axios',
+            //   default: true,
+            //   values: true,
+            //   syntheticDefaultImport: true,
+            // },
+            { name: 'AxiosRequestConfig' },
+            { name: 'AxiosResponse' },
+            { name: 'CreateAxiosDefaults' },
+          ],
+          dependency: 'axios',
+        },
+        {
+          exports: [
+            // {
+            //   name: 'axios',
+            //   default: true,
+            //   values: true,
+            //   syntheticDefaultImport: true,
+            // },
+            {
+              name: 'makeAxios',
+              values: true,
+            },
+          ],
+          dependency: 'axalias',
+        },
+        // {
+        //   dependency: './',
+        // },
+      ];
+    },
+    header: (params) => {
+      const operationNames = '';
+      return `
+
+            // --- header start
+             import { validateResponse } from '../utils/validate-response';
+            // ${'JSON.stringify(params, null, 2)'}
+              import * as veees from './${params.tag}.zod.gen';
+            ${generateAxiosHeader(params)}
+            
+            const axios = makeAxios({
+              clientParams: ${'JSON.stringify(params, null, 2)'},
+              v: veees,
+            });
+
+            // --- header end
+
+          `;
+    },
+    footer: (params) => `
+            // --- footer start
+            
+            const getList = ${JSON.stringify(params, null, 2)};
+            
+            ${generateAxiosFooter(params)}
+            // --- footer end
+          `,
+    title: (title) => `
+
+            // --- title start
+            ${generateAxiosTitle(title)}
+            // --- title end
+
+          `,
+    // extraFiles: () => `
+    //
+    //   // --- extraFiles start
+    //   ${axiosBuilder.extraFiles}
+    //   // --- extraFiles end
+    //
+    // `,
+
+    // ...axiosBuilder,
+    // dependencies: () => {
+    // return [
+    //   {
+    //     exports: [
+    //       {
+    //         name: 'Axios',
+    //         default: true,
+    //         values: true,
+    //         syntheticDefaultImport: true,
+    //       },
+    //       { name: 'AxiosRequestConfig' },
+    //       { name: 'AxiosResponse' },
+    //       { name: 'CreateAxiosDefaults' },
+    //     ],
+    //     dependency: 'axios',
+    //   },
+    // ];
+    // },
+    // header: () => {
+    //   //             return `
+    //   // export const createApiClient = (config?: CreateAxiosDefaults) => {
+    //   //   const axios = Axios.create(config);
+    //   //
+    //   // `;
+    // },
+    // footer: (params) => {
+    //   const result = generateAxiosFooter(params);
+    //   return result.replace(
+    //     /return {(.+?)}/,
+    //     (_, captured) => `return {${captured}, axios}`,
+    //   );
+    // },
+  };
+  // const axiosBuilder = builder({ type: 'axios-functions' })();
+  //
+  // return axiosBuilder;
+}
