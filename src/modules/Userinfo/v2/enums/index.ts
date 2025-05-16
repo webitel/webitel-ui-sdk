56a11bdaf9abca969f59111b5d3381cc2b9c3bbe
volodymyr <volodymyr.dechtiaruk@gmail.com>
2025-05-16 18:00:05 +0300

    refactor: code review refactor [WTEL-6827](https://webitel.atlassian.net/browse/WTEL-6827)

--- src/modules/Userinfo/v2/enums/index.ts
+++ src/modules/Userinfo/v2/enums/index.ts
@@ -2,7 +2,6 @@ import {
   CrudGlobalAction,
   SpecialGlobalAction,
 } from './GlobalActions/GlobalActions';
-import { RolePermissionsId } from './RolePermissions/RolePermissionsId';
 import { ScopeClass } from './ScopeClass/ScopeClass';
 
-export { CrudGlobalAction, RolePermissionsId,ScopeClass, SpecialGlobalAction };
+export { CrudGlobalAction, ScopeClass, SpecialGlobalAction };
