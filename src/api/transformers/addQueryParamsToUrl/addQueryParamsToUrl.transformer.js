d10f042c250f8760a4c58adee312892ed348b236
Stas Kozak <kozak.stasan@gmail.com>
2025-06-05 15:16:21 +0300

    feat: implement transformer for add query params to url [WTEL-6951](https://webitel.atlassian.net/browse/WTEL-6951)

---
+++ src/api/transformers/addQueryParamsToUrl/addQueryParamsToUrl.transformer.js
@@ -0,0 +1,17 @@
+// @author @stanislav-kozak
+// This function adds query parameters to a given URL.
+// Where queryArray is an array of query parameters in the format 'key=value'.
+const addQueryParamsToUrl = (queryArray) => (url) => {
+  let modifyUrl = url;
+
+  if (queryArray && queryArray.length > 0) {
+    if (modifyUrl.includes('?')) {
+      modifyUrl += '&' + queryArray.join('&');
+    }
+
+    modifyUrl += '?' + queryArray.join('&');
+  }
+
+  return modifyUrl;
+};
+export default addQueryParamsToUrl;
