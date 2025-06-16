08bfaa167e9f89d6abfc255d33563996b35fb037
Daniil Lohvinov <nsblnr@gmail.com>
2025-06-16 15:37:49 +0300

    chore: api-services/ gen types [WTEL-7046](https://webitel.atlassian.net/browse/WTEL-7046)

--- packages/api-services/types/utils/index.d.ts
+++ packages/api-services/types/utils/index.d.ts
@@ -1,2 +1,8 @@
-import z from 'zod';
-export declare const getFieldsToSendFromZodSchema: <T extends z.ZodTypeAny>(schema: T) => string[];
+import type { z } from 'zod/v4';
+export * from './gen';
+/**
+ * @author @dlohvinov
+ * @description
+ * Get default value for schema. Could be anything: object with default fields values, or primitive.
+ */
+export declare const getDefaultsFromZodSchema: (schema: z.ZodType, value?: unknown) => unknown;
