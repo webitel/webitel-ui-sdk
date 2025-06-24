d6cc102fb8c4a1f69a30d42e266710cda822087b
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-06-24 18:21:28 +0300

    fix: import generateInstance.js [WTEL-7100](https://webitel.atlassian.net/browse/WTEL-7100)

--- src/api/axios/generateInstance.js
+++ src/api/axios/generateInstance.js
@@ -1,4 +1,4 @@
-import { generateInstance } from '@webitel/api-services/src/api/axios';
+import { generateInstance } from '@webitel/api-services/api/axios';
 
 // todo: replace imports of this file with source file, rm this file
 export default generateInstance;
