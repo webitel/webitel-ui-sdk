4e2e08c9117017938f725b227463050f6a11cb3e
Олександр Пальонний <s2567k@gmail.com>
2025-07-09 17:28:17 +0300

    feat: implement navigation and user info in app header, enhance menubar styles [WTEL-6985](https://webitel.atlassian.net/browse/WTEL-6985)

--- docs/pages/packages/ui-sdk/components/wt-app-header/examples/example-app-header.vue
+++ docs/pages/packages/ui-sdk/components/wt-app-header/examples/example-app-header.vue
@@ -1,9 +1,67 @@
-<script setup></script>
+<script setup>
+import { CrmSections,WebitelApplications } from '@webitel/ui-sdk/enums';
+
+const apps = [
+  { name: WebitelApplications.AGENT, href: 'https://example.com' },
+  { name: WebitelApplications.AUDIT, href: 'https://example.com' },
+  { name: WebitelApplications.SUPERVISOR, href: 'https://example.com' },
+  { name: WebitelApplications.CRM, href: 'https://example.com' },
+  { name: WebitelApplications.ADMIN, href: 'https://example.com' },
+  { name: WebitelApplications.HISTORY, href: 'https://example.com' },
+  { name: WebitelApplications.ANALYTICS, href: 'https://example.com' },
+];
+
+const nav = [{
+  value: CrmSections.CONTACTS,
+  name: CrmSections.CONTACTS,
+  route: '/contacts',
+}]
+
+const user = {
+  name: 'My name',
+  preferredUsername: 'My username',
+};
+
+const buildInfo = {
+  release: '21.02',
+  build: 'AWS-12',
+};
+</script>
 
 <template>
   <wt-app-header>
     <template #start>
-      lorem ipsum
+      <wt-navigation-bar
+        current-app="admin"
+        :nav="nav"
+        dark-mode
+        :logo-route="''"
+      />
+
+      <wt-logo
+        dark-mode
+        :logo-href="''"
+      />
+
+      <wt-dark-mode-switcher />
+    </template>
+
+    <template #end>
+      <wt-switcher
+        :value="false"
+        label="Call center"
+      />
+
+      <wt-app-navigator
+        :apps="apps"
+        current-app="admin"
+        dark-mode
+      />
+
+      <wt-header-actions
+        :user="user"
+        :build-info="buildInfo"
+      />
     </template>
   </wt-app-header>
 </template>
