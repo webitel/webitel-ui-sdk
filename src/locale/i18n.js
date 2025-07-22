0b2be455e77b799ecf2661ab300c7f4514e2854c
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-07-22 17:15:53 +0300

    chore: lint:fix [DEV-5377](https://webitel.atlassian.net/browse/DEV-5377)

--- src/locale/i18n.js
+++ src/locale/i18n.js
@@ -3,10 +3,10 @@ import { createI18n } from 'vue-i18n';
 import en from './en/en.js';
 import es from './es/es.js';
 import kz from './kz/kz.js';
+import pl from './pl/pl';
 import ro from './ro/ro.js';
 import ru from './ru/ru.js';
 import uk from './uk/uk.js';
-import pl from './pl/pl';
 import uz from './uz/uz';
 import vi from './vi/vi';
 
