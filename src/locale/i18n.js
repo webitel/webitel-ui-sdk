cc1d4271b0e631e01e031cb84271c73e6e5c215f
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-07-23 12:05:23 +0300

    refactor: added locale files export index [DEV-5377](https://webitel.atlassian.net/browse/DEV-5377)

--- src/locale/i18n.js
+++ src/locale/i18n.js
@@ -1,26 +1,6 @@
 import { createI18n } from 'vue-i18n';
 
-import en from './en/en.js';
-import es from './es/es.js';
-import kz from './kz/kz.js';
-import pl from './pl/pl';
-import ro from './ro/ro.js';
-import ru from './ru/ru.js';
-import uk from './uk/uk.js';
-import uz from './uz/uz';
-import vi from './vi/vi';
-
-const messages = {
-  en,
-  es,
-  ru,
-  ro,
-  uk,
-  kz,
-  pl,
-  uz,
-  vi,
-};
+import { messages } from './index';
 
 export default createI18n({
   // legacy: false,
