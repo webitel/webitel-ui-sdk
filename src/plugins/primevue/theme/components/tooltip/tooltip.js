c10ea6dcb4aed28a7938f8e7d338461c0b31b315
Олександр Пальонний <s2567k@gmail.com>
2025-07-25 10:31:04 +0300

    fix: enhance tooltip styling with improved CSS properties [WTEL-7275](https://webitel.atlassian.net/browse/WTEL-7275)

--- src/plugins/primevue/theme/components/tooltip/tooltip.js
+++ src/plugins/primevue/theme/components/tooltip/tooltip.js
@@ -3,6 +3,18 @@ import { TooltipScheme } from '@webitel/styleguide/component-schemes';
 const tooltip = {
   ...TooltipScheme.sizes,
   colorScheme: TooltipScheme.colorScheme,
+
+  css: () => `
+      .p-tooltip-text {
+          white-space: pre-line;
+          word-break: normal;
+          background: var(--p-tooltip-background);
+          color: var(--p-tooltip-color);
+          padding: var(--p-tooltip-padding);
+          box-shadow: var(--p-tooltip-shadow);
+          border-radius: var(--p-tooltip-border-radius);
+      }
+  `,
 };
 
 export default tooltip;
