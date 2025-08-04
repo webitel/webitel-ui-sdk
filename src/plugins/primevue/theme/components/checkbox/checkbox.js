7fecd3020e3a4e0f4215df8971294ca9f6a3085e
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-08-04 16:06:30 +0300

    fix: update SVG fill to currentColor and adjust checkbox icon styles [WTEL-7274](https://webitel.atlassian.net/browse/WTEL-7274)

--- src/plugins/primevue/theme/components/checkbox/checkbox.js
+++ src/plugins/primevue/theme/components/checkbox/checkbox.js
@@ -6,25 +6,25 @@ const checkbox = {
 
   css: ({ dt }) => `
         .p-checkbox-box .wt-icon__icon {
-            fill: ${dt('checkbox.icon.color')};
+            color: ${dt('checkbox.icon.color')};
             width: ${dt('checkbox.icon.size')};
             height: ${dt('checkbox.icon.size')};
         }
 
         .p-checkbox-box .wt-icon--color-active .wt-icon__icon {
-            fill: ${dt('checkbox.icon.checkedColor')};
+            color: ${dt('checkbox.icon.checkedColor')};
         }
 
         .p-checkbox-box .wt-icon--color-disabled .wt-icon__icon {
-            fill: ${dt('checkbox.icon.disabledColor')};
+            color: ${dt('checkbox.icon.disabledColor')};
         }
 
         .p-checkbox:hover .wt-icon--color-active .wt-icon__icon {
-            fill: ${dt('checkbox.icon.checkedHoverColor')};
+            color: ${dt('checkbox.icon.checkedHoverColor')};
         }
 
         .wt-checkbox:has(.wt-label:hover) .wt-icon--color-active .wt-icon__icon {
-            fill: ${dt('checkbox.icon.checkedHoverColor')};
+            color: ${dt('checkbox.icon.checkedHoverColor')};
         }
         `,
 };
