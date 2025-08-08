b1dfc3fab9ad46cb0f201468f46db25da7b81662
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-08-08 09:35:11 +0300

    fix: return wt-chip class for custom style handling [WTEL-7278](https://webitel.atlassian.net/browse/WTEL-7278)

--- src/components/wt-chip/wt-chip.vue
+++ src/components/wt-chip/wt-chip.vue
@@ -1,6 +1,6 @@
 <template>
   <p-chip
-    :class="`p-chip-${color}`"
+    :class="`wt-chip p-chip-${color}`"
     :removable="removable"
   >
     <slot />
@@ -28,7 +28,6 @@ withDefaults(defineProps<WtProps>(), {
 
 <style lang="scss">
 @use './variables.scss';
-@use './wt-chip.scss';
 
 .wt-chip__close-icon {
   cursor: pointer;
