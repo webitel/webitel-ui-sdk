1d9fdb2987e6ae7099fc89b667ec3d6f82741075
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-08-11 10:04:04 +0300

    fix: add wrapper widht and height [WTEL-7363](https://webitel.atlassian.net/browse/WTEL-7363)

--- src/plugins/primevue/theme/components/radio/radio.js
+++ src/plugins/primevue/theme/components/radio/radio.js
@@ -3,6 +3,15 @@ import { RadioScheme } from '@webitel/styleguide/component-schemes';
 const radio = {
   ...RadioScheme.sizes,
   colorScheme: RadioScheme.colorScheme,
+
+  css: ({dt}) => `
+    .p-radiobutton {
+      width: ${dt('radiobutton.root.wrapperWidth')};
+      height: ${dt('radiobutton.root.wrapperHeight')};
+      align-items: center;
+      justify-content: center;
+    }
+  `
 };
 
 export default radio;
