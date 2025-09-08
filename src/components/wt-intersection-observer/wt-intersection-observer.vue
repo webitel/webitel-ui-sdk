f76643485d576e10b18a1915eea8fd82e9784669
Oles Chorpita <chorpitaoles@gmail.com>
2025-09-08 12:19:13 +0300

    [WTEL-7513] revert changes

--- src/components/wt-intersection-observer/wt-intersection-observer.vue
+++ src/components/wt-intersection-observer/wt-intersection-observer.vue
@@ -7,61 +7,47 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount, nextTick } from 'vue'
-import { useIntersectionObserver } from '@vueuse/core'
+import { useIntersectionObserver } from '@vueuse/core';
+import { onMounted, onUnmounted, ref } from 'vue';
 
 const props = defineProps({
-  canLoadMore: { type: Boolean, default: true },
-  loading: { type: Boolean, default: false },
-  rootMargin: { type: String, default: '0px 0px 300px 0px' },
-  threshold:  { type: [Number, Array], default: 0 },
-})
+  canLoadMore: {
+    type: Boolean,
+    default: true
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-const emit = defineEmits(['next'])
+const emit = defineEmits(['next']);
 
-const intersectionTarget = ref<HTMLElement|null>(null)
-let stopObs: (() => void) | null = null
+const intersectionTarget = ref(null);
 
-const getScrollableParent = (el: HTMLElement | null): HTMLElement | null => {
-  let node: HTMLElement | null = el?.parentElement ?? null
-  while (node) {
-    const style = window.getComputedStyle(node)
-    const canScrollY = /(auto|scroll)/.test(style.overflowY)
-    const hasScrollableArea = node.scrollHeight > node.clientHeight
-    if (canScrollY && hasScrollableArea) return node
-    node = node.parentElement
-  }
-  return null
-}
+let stopObs;
 
-const initObserver = () => {
-  if (!intersectionTarget.value) return
-  const rootEl = getScrollableParent(intersectionTarget.value)
-
-  const { stop } = useIntersectionObserver(
-    intersectionTarget.value,
-    ([{ isIntersecting }]) => {
-      if (isIntersecting && props.canLoadMore && !props.loading) {
-        emit('next')
-      }
-    },
-    { root: rootEl ?? undefined, rootMargin: props.rootMargin, threshold: props.threshold }
-  )
-  stopObs = stop
-}
-
-onMounted(async () => {
+onMounted(() => {
   /**
    *
    * Note, observer triggers at init, so it should be used also as init function
    * however, current filters module version is initializing list by itself, so we need to refactor filters ASAP
    */
+  const { stop } = useIntersectionObserver(
+    intersectionTarget.value,
+    ([{ isIntersecting }]) => {
+      if (isIntersecting && props.canLoadMore) {
+        emit('next');
+      }
+    },
+  );
 
-  await nextTick()
-  requestAnimationFrame(() => initObserver())
-})
+  stopObs = stop;
+});
 
-onBeforeUnmount(() => stopObs?.())
+onUnmounted(() => {
+  stopObs();
+});
 </script>
 
 <style scoped lang="scss">
