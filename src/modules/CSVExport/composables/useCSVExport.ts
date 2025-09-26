5d404c4110182b87606a7e5a58a8b4de4d014021
Олександр Пальонний <s2567k@gmail.com>
2025-09-26 17:15:45 +0300

    refactor: added types [WTEL-7615](https://webitel.atlassian.net/browse/WTEL-7615)

--- src/modules/CSVExport/composables/useCSVExport.ts
+++ src/modules/CSVExport/composables/useCSVExport.ts
@@ -2,7 +2,21 @@ import applyTransform, { notify } from '@webitel/ui-sdk/api/transformers/index';
 import { computed, Ref, ref } from 'vue';
 import { useRoute } from 'vue-router';
 
-import CSVExport from '../CSVExport.js';
+import CSVExport from '../CSVExport';
+
+export type CSVRow = Record<string, any>;
+
+export type FetchMethod = (params: Record<string, any>) => Promise<{
+  items: CSVRow[];
+  next: boolean;
+}>;
+
+interface CSVExportOptions {
+  filename?: string;
+  delimiter?: string;
+}
+
+const CSV_EXPORT_BATCH_SIZE = 5000;
 
 export function useCSVExport({ selected }: { selected: Ref<number[]> }) {
   const CSVExportInstance = ref<null | InstanceType<typeof CSVExport>>(null);
@@ -18,15 +32,18 @@ export function useCSVExport({ selected }: { selected: Ref<number[]> }) {
 
   const isAnySelected = computed(() => !!selected.value.length);
 
-  function initCSVExport(fetchMethod, options) {
+  function initCSVExport(
+    fetchMethod: FetchMethod,
+    options: CSVExportOptions = {},
+  ) {
     CSVExportInstance.value = new CSVExport(fetchMethod, options);
   }
 
-  async function exportCSV(exportParams?) {
+  async function exportCSV(exportParams?: Record<string, any>) {
     const routeQuery = route.query;
     const params = {
       ...(exportParams || routeQuery),
-      size: 5000,
+      size: CSV_EXPORT_BATCH_SIZE,
     };
 
     if (isAnySelected.value) {
