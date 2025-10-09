fba1621c66d5b1e858b65d313821a3e231d07cc3
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-10-09 13:55:22 +0300

    feat: add WtGalleria component and integrate with PrimeVue [WTEL-7754](https://webitel.atlassian.net/browse/WTEL-7754)

---
+++ src/components/wt-galleria/wt-galleria.vue
@@ -0,0 +1,38 @@
+<template>
+  <p-galleria 
+    v-model:visible="visible"
+    :value="value"
+    full-screen
+    circular
+    show-item-navigators
+    :num-visible="5"
+  >
+    <template #item="{item}: {item: WtGalleriaItem}">
+        <img :src="item.src" alt="Gallery item" />
+    </template>
+    <template #thumbnail="{item}: {item: WtGalleriaItem}">
+        <img :src="item.thumbnailSrc" alt="Gallery thumbnail" class="wt-galleria__thumbnail" />
+    </template>
+  </p-galleria>
+</template>
+
+<script setup lang="ts">
+import type { WtGalleriaItem } from './types/WtGalleria.d.ts';
+
+import { defineModel, defineProps, ref} from 'vue';
+interface Props {
+  value: WtGalleriaItem[];
+}
+
+const props = defineProps<Props>();
+
+const visible = defineModel<boolean>('visible', { required: true });
+</script>
+
+<style lang="scss" scoped>
+.wt-galleria__thumbnail {
+  width: 80px;
+  height: 50px;
+  object-fit: cover;
+}
+</style>
---
+++ src/plugins/primevue/theme/components/galleria/galleria.js
@@ -0,0 +1,8 @@
+import { GalleriaScheme } from '@webitel/styleguide/component-schemes';
+
+const galleria = {
+  ...GalleriaScheme.sizes,
+  colorScheme: GalleriaScheme.colorScheme,
+};
+
+export default galleria;
