aa78131e2d2a53791c2c52e1ebda07938ac10cad
Oles Chorpita <chorpitaoles@gmail.com>
2025-10-14 18:14:29 +0300

    feat: added filed for pause cause popup and send it comment to request [WTEL-7670](https://webitel.atlassian.net/browse/WTEL-7670)

--- src/modules/AgentStatusSelect/api/agent-status.js
+++ src/modules/AgentStatusSelect/api/agent-status.js
@@ -3,12 +3,13 @@ import { AgentServiceApiFactory } from 'webitel-sdk';
 const AgentStatusAPIFactory = ({ instance, OpenAPIConfig }) => {
   const service = new AgentServiceApiFactory(OpenAPIConfig, '', instance);
 
-  const patchAgentStatus = async ({ agentId, status, pauseCause }) => {
+  const patchAgentStatus = async ({ agentId, status, pauseCause, statusComment }) => {
     try {
       const res = await service.updateAgentStatus(agentId, {
         status,
         id: agentId,
         payload: pauseCause,
+        status_comment: statusComment
       });
       return { success: !!res };
     } catch (err) {
