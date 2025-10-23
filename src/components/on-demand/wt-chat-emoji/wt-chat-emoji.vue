dbf3dc6de1cda45e6e938b43b5ee7d45d0cf74ed
Lera24 <bedulinavalera21@gmail.com>
2025-10-23 11:21:13 +0300

    Fix: add new component wt-chat-emoji and small fix icons[WTEL-7889](https://webitel.atlassian.net/browse/WTEL-7889)

---
+++ src/components/on-demand/wt-chat-emoji/wt-chat-emoji.vue
@@ -0,0 +1,94 @@
+<template>
+  <div
+    v-clickaway="closePicker"
+    class="wt-chat-emoji"
+  >
+    <wt-rounded-action
+      :size="size"
+      color="secondary"
+      icon="chat-emoji"
+      rounded
+      wide
+      @click="togglePicker"
+    />
+    <div
+      v-if="isOpened"
+      ref="emojiPickerWrapper"
+      class="wt-chat-emoji__wrapper"
+    ></div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onBeforeUnmount, nextTick } from 'vue';
+import * as EmojiPicker from 'emoji-picker-element/picker'; ///!not delete
+
+const props = defineProps({
+  size: {
+    type: String,
+    default: 'md',
+  },
+});
+
+const emit = defineEmits(['insert-emoji']);
+
+const emojiPickerWrapper = ref(null);
+const isOpened = ref(false);
+const picker = ref(null);
+
+const initPicker = async () => {
+  if (picker.value) return;
+
+  picker.value = document.createElement('emoji-picker');
+
+  appendPicker();
+  picker.value.addEventListener('emoji-click', emitEmojiClickEvent);
+};
+
+const appendPicker = () => {
+  if (emojiPickerWrapper.value && picker.value) {
+    emojiPickerWrapper.value.appendChild(picker.value);
+  }
+};
+
+const emitEmojiClickEvent = (event) => {
+  const { unicode } = event.detail;
+  emit('insert-emoji', unicode);
+  closePicker();
+};
+
+const closePicker = () => {
+  isOpened.value = false;
+  if (picker.value && emojiPickerWrapper.value) {
+    picker.value.removeEventListener('emoji-click', emitEmojiClickEvent);
+    emojiPickerWrapper.value.removeChild(picker.value);
+    picker.value = null;
+  }
+};
+
+const togglePicker = async () => {
+  isOpened.value = !isOpened.value;
+  if (isOpened.value) {
+    await nextTick();
+    await initPicker();
+  } else {
+    closePicker();
+  }
+};
+
+onBeforeUnmount(() => {
+  closePicker();
+});
+</script>
+
+<style lang="scss" scoped>
+.wt-chat-emoji {
+  position: relative;
+  width: 100%;
+
+  ::v-deep emoji-picker {
+    --background: var(--content-wrapper-color);
+    --border-color: var(--secondary-color);
+  }
+}
+</style>
