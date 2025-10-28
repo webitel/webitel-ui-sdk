0338496ad084dd298d1ef3f9296ec686f65bf7a1
Oles Chorpita <chorpitaoles@gmail.com>
2025-10-28 16:55:02 +0200

    fix: create api for call history api in api service [WTEL-7974]

---
+++ packages/api-services/src/api/clients/callHistory/callHistory.ts
@@ -0,0 +1,54 @@
+import { getCallService } from '@webitel/api-services/gen';
+import { getDefaultGetListResponse, getDefaultGetParams } from '../../defaults';
+import {
+	applyTransform,
+	camelToSnake,
+	merge,
+	notify,
+	snakeToCamel,
+	starToSearch,
+} from '../../transformers';
+
+const getCallHistoryList = async ({ options, ...params }) => {
+	const listParams = applyTransform(params, [
+		merge(getDefaultGetParams()),
+		starToSearch('search'),
+	]);
+	try {
+		const response = await getCallService().searchHistoryCall(listParams, options);
+		const { items, next } = applyTransform(response.data, [
+			snakeToCamel(),
+			merge(getDefaultGetListResponse()),
+		]);
+		return { items, next };
+	} catch (err) {
+		throw applyTransform(err, [notify]);
+	}
+};
+
+const getCallHistoryListPost = async ({ data, options }) => {
+	const body = applyTransform(data, [camelToSnake()]);
+	try {
+		const response = await getCallService().searchHistoryCallPost(body, options);
+		const { items, next } = applyTransform(response.data, [
+			snakeToCamel(),
+			merge(getDefaultGetListResponse()),
+		]);
+		return { items, next };
+	} catch (err) {
+		throw applyTransform(err, [notify]);
+	}
+};
+
+const getCallHistoryLookup = (params) =>
+	getCallHistoryList({
+		...params,
+		fields: params?.fields || ['id', 'destination', 'state', 'created_at'],
+	});
+
+export const CallHistoryAPI = {
+	getList: getCallHistoryList,
+	getListPost: getCallHistoryListPost,
+	getLookup: getCallHistoryLookup,
+};
+
