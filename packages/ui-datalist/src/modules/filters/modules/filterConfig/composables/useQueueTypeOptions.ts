260fd8a682c1c38ae112de1977336083d908c7ab
Lera24 <bedulinavalera21@gmail.com>
2025-11-05 16:12:54 +0200

    fix: useQueueTypeOptions[WTEL-7980](https://webitel.atlassian.net/browse/WTEL-7980)

--- packages/ui-datalist/src/modules/filters/modules/filterConfig/composables/useQueueTypeOptions.ts
+++ packages/ui-datalist/src/modules/filters/modules/filterConfig/composables/useQueueTypeOptions.ts
@@ -5,30 +5,8 @@ import { useI18n } from 'vue-i18n';
 export function useQueueTypeOptions() {
   const { t } = useI18n();
 
-  const mapQueueTypeToLabel = (value: string) => {
-    switch (value) {
-      case QueueType.OFFLINE_QUEUE:
-        return t(`objects.queue.type.${QueueType.OFFLINE_QUEUE}`);
-      case QueueType.INBOUND_QUEUE:
-        return t(`objects.queue.type.${QueueType.INBOUND_QUEUE}`);
-      case QueueType.OUTBOUND_IVR_QUEUE:
-        return t(`objects.queue.type.${QueueType.OUTBOUND_IVR_QUEUE}`);
-      case QueueType.PREVIEW_DIALER:
-        return t(`objects.queue.type.${QueueType.PREVIEW_DIALER}`);
-      case QueueType.PROGRESSIVE_DIALER:
-        return t(`objects.queue.type.${QueueType.PROGRESSIVE_DIALER}`);
-      case QueueType.PREDICTIVE_DIALER:
-        return t(`objects.queue.type.${QueueType.PREDICTIVE_DIALER}`);
-      case QueueType.CHAT_INBOUND_QUEUE:
-        return t(`objects.queue.type.${QueueType.CHAT_INBOUND_QUEUE}`);
-      case QueueType.INBOUND_JOB_QUEUE:
-        return t(`objects.queue.type.${QueueType.INBOUND_JOB_QUEUE}`);
-      case QueueType.OUTBOUND_JOB_QUEUE:
-        return t(`objects.queue.type.${QueueType.OUTBOUND_JOB_QUEUE}`);
-      default:
-        return value;
-    }
-  };
+  const mapQueueTypeToLabel = (value: string) =>
+    t(`objects.queue.type.${value}`);
 
   const options = computed(() =>
     QueueType.map(({ value }) => ({
