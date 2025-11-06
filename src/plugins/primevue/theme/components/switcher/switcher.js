bcba0131c0408b845b7739bf5b6839a86d6d55d4
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-11-06 10:31:14 +0200

    fix: add label visibility to wt-switcher and update switcher CSS

--- src/plugins/primevue/theme/components/switcher/switcher.js
+++ src/plugins/primevue/theme/components/switcher/switcher.js
@@ -3,6 +3,12 @@ import { SwitcherScheme } from '@webitel/styleguide/component-schemes';
 const switcher = {
   ...SwitcherScheme.sizes,
   colorScheme: SwitcherScheme.colorScheme,
+
+  css: ({ dt }) => `
+    .p-toggleswitch {
+      min-width: ${dt('toggleswitch.width')};
+    }
+  `
 };
 
 export default switcher;
