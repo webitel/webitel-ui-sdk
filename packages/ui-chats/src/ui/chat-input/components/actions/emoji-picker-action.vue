2d7e21fb8e0ba21b73bea4f28334794a0a9c776e
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-11-12 15:00:50 +0200

    feat: ui-chats -> chat send, emoji, attachment actions [WTEL-8080](https://webitel.atlassian.net/browse/WTEL-8080)

---
+++ packages/ui-chats/src/ui/chat-input/components/actions/emoji-picker-action.vue
@@ -0,0 +1,20 @@
+<template>
+  <wt-chat-emoji
+    :size="size"
+    @insert-emoji="emit('insertEmoji', $event)"
+  />
+</template>
+
+<script setup lang="ts">
+import { WtChatEmoji } from '@webitel/ui-sdk/components';
+import { ComponentSize } from '@webitel/ui-sdk/enums';
+
+const props = defineProps<{
+  size: ComponentSize;
+}>();
+
+const emit = defineEmits<{
+  'insertEmoji': [unicode: string];
+}>();
+
+</script>
