3f9af0b8518c75bb81e947236f09d5926ac636ba
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-11-23 20:10:16 +0200

    feat: ui-chats/ added ChatAction enum quick-replies entry [WTEL-8080](https://webitel.atlassian.net/browse/WTEL-8080)

--- packages/ui-chats/src/ui/chat-input/enums/ChatAction.enum.ts
+++ packages/ui-chats/src/ui/chat-input/enums/ChatAction.enum.ts
@@ -2,6 +2,7 @@ export const ChatAction = {
   SendMessage: 'sendMessage',
   AttachFiles: 'attachFiles',
   EmojiPicker: 'emojiPicker',
+  QuickReplies: 'quickReplies',
 } as const;
 
 export type ChatAction = (typeof ChatAction)[keyof typeof ChatAction];
