e59b9741e8d19ce666d6d8bba4b6b1b38a7c9007
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-11-23 20:11:40 +0200

    feat: ui-chats/ WIP implement load more functionality in chat messages container [WTEL-8080](https://webitel.atlassian.net/browse/WTEL-8080)

--- packages/ui-chats/src/ui/messaging/components/chat-messages-container.vue
+++ packages/ui-chats/src/ui/messaging/components/chat-messages-container.vue
@@ -1,5 +1,14 @@
 <template>
-    <section class="chat-messages-container">
+    <section 
+    class="chat-messages-container"
+    v-element-size="handleMessagesContainerResize"
+    @click="focusOnTextField"
+    >
+        <wt-intersection-observer
+            :canLoadMore="hasMoreMessages"
+            :loading="isLoading"
+            @next="loadMore"
+        />
         <chat-message-component
             v-for="message in messages"
             :key="message.id"
@@ -9,11 +18,47 @@
 </template>
 
 <script setup lang="ts">
+import { inject, ref } from 'vue';
+import type { Emitter } from 'mitt';
+import { WtIntersectionObserver } from '@webitel/ui-sdk/components';
+import { vElementSize } from '@vueuse/components'; // for chat resize observer, when chat-messages-container size changes
+
 import { ChatMessageType } from '../types/ChatMessage.types';
 import ChatMessageComponent from '../modules/message/components/chat-message.vue';
+import { UiChatsEmitterEvents } from '../../utils/emitter';
+import type { ResultCallbacks } from '../../utils/ResultCallbacks.types';
 
+
+const uiChatsEmitter = inject<Emitter<UiChatsEmitterEvents>>('uiChatsEmitter');
 
 const props = defineProps<{
     messages: ChatMessageType[];
+    hasMoreMessages: boolean;
 }>();
+
+ const emit = defineEmits<{
+    'loadMore': [ResultCallbacks];
+ }>();
+
+ const isLoading = ref(false);
+
+ function loadMore() {
+    isLoading.value = true;
+    emit('loadMore', {
+        onSuccess: () => {
+            isLoading.value = false;
+        },
+        onError: () => {
+            isLoading.value = false;
+        },
+    });
+ }
+
+function focusOnTextField() {
+    uiChatsEmitter!.emit('focusOnTextField');
+}
+
+function handleMessagesContainerResize() {
+    console.log('handleMessagesContainerResize');
+}
 </script>
