461feca0530e47b44a75e009221731b695164e6a
liza-pohranichna <haemoglobin0@gmail.com>
2025-12-01 12:41:21 +0200

    refactor: remove export of ChatMessage component from index.ts [WTEL-8243]

--- packages/ui-chats/src/ui/index.ts
+++ packages/ui-chats/src/ui/index.ts
@@ -1,3 +1,2 @@
-export { default as ChatMessage } from '../ui/messaging/modules/message/components/chat-message.vue';
 export { default as ChatContainerComponent } from './chat-container.vue';
 export { ChatAction } from './chat-footer/modules/user-input/types/ChatAction.types';
