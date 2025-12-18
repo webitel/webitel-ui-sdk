02c74f9bb0a3d00e7e51bb95ecc905116f80246d
Stas Kozak <kozak.stasan@gmail.com>
2025-12-18 11:43:49 +0200

    feat: implement MimeTypes enum [WTEL-8338](https://webitel.atlassian.net/browse/WTEL-8338)

---
+++ src/enums/MimeTypes/MimeTypes.enum.js
@@ -0,0 +1,21 @@
+const MimeTypes = Object.freeze({
+  imageJpeg: 'image/jpeg',
+  imagePng: 'image/png',
+  imageGif: 'image/gif',
+  imageSvgXml: 'image/svg+xml',
+  imageWebp: 'image/webp',
+
+  videoMp4: 'video/mp4',
+  videoWebm: 'video/webm',
+  videoMpeg: 'video/mpeg',
+
+  audioMpeg: 'audio/mpeg',
+  audioWav: 'audio/wav',
+
+  applicationPdf: 'application/pdf',
+  applicationMsword: 'application/msword',
+  applicationDocx:
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+});
+
+export default MimeTypes;
