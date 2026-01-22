b27d15e8c6195a7fa63b75c9687c85b3df09ab64
Oles Chorpita <chorpitaoles@gmail.com>
2026-01-22 13:14:57 +0200

    fix: revety chants in useChatMessage [WTEL-8477][WTEL-8564]

--- packages/ui-chats/types/ui/messaging/modules/message/composables/useChatMessage.d.ts
+++ packages/ui-chats/types/ui/messaging/modules/message/composables/useChatMessage.d.ts
@@ -1,13 +1,12 @@
-import { type ComputedRef, type Ref } from 'vue';
-import type { ChatMessageType } from '../../../types/ChatMessage.types';
+import type { ChatMessageType } from "../../../types/ChatMessage.types";
 interface GetMessageResult {
-    prevMessage?: ChatMessageType;
-    message?: ChatMessageType;
-    nextMessage?: ChatMessageType;
+  prevMessage?: ChatMessageType;
+  message?: ChatMessageType;
+  nextMessage?: ChatMessageType;
 }
-export declare const useChatMessages: (messages: Ref<ChatMessageType[]> | ComputedRef<ChatMessageType[]>) => {
-    showAvatar: (index: number) => boolean;
-    getMessage: (index: number) => GetMessageResult;
-    showChatDate: (index: number) => boolean;
+export declare const useChatMessages: (chatMessages: ChatMessageType[]) => {
+  showAvatar: (index: number) => boolean;
+  getMessage: (index: number) => GetMessageResult;
+  showChatDate: (index: number) => boolean;
 };
 export {};
