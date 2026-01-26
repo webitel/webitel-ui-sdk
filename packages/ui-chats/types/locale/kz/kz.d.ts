407b9564acd30eeef07d3729897c3ca009eb3711
dlohvinov <32868304+dlohvinov@users.noreply.github.com>
2026-01-26 09:39:59 +0000

    Apply automatic changes

---
+++ packages/ui-chats/types/locale/kz/kz.d.ts
@@ -0,0 +1,11 @@
+declare const _default: {
+    "@webitel/ui-chats": {
+        ui: {
+            messaging: {
+                chatsFileBlocked: string;
+                textAreaPlaceholder: string;
+            };
+        };
+    };
+};
+export default _default;
