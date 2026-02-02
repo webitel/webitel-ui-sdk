0adf905ab2522a959ecba4432dae23badaa09f91
Yehor Hlukhov <egorgluhov68@gmail.com>
2026-02-02 12:22:44 +0200

    fix: timepicker usage [WTEL-8820](https://webitel.atlassian.net/browse/WTEL-8820)

--- packages/ui-datalist/src/modules/filters/modules/filterConfig/components/_shared/durations/duration-filter-value-field.vue
+++ packages/ui-datalist/src/modules/filters/modules/filterConfig/components/_shared/durations/duration-filter-value-field.vue
@@ -3,18 +3,18 @@
     <wt-timepicker
       v-if="model"
       :label="t('reusable.from')"
-      :value="model.from"
+      :model-value="model.from"
       :v="v$.model?.from"
       format="mm:ss"
-      @input="handleInput('from', $event)"
+      @update:model-value="handleInput('from', $event)"
     />
     <wt-timepicker
       v-if="model"
       :label="t('reusable.to')"
-      :value="model.to"
+      :model-value="model.to"
       :v="v$.model?.to"
       format="mm:ss"
-      @input="handleInput('to', $event)"
+      @update:model-value="handleInput('to', $event)"
     />
   </div>
 </template>
@@ -27,55 +27,65 @@ import { useI18n } from 'vue-i18n';
 const { t } = useI18n();
 
 type ModelValue = {
-  from: number;
-  to: number;
+	from: number;
+	to: number;
 };
 
 const model = defineModel<ModelValue>();
 
 const emit = defineEmits<{
-  'update:invalid': [boolean];
+	'update:invalid': [
+		boolean,
+	];
 }>();
 
 if (!model.value) {
-  model.value = {
-    from: 0,
-    to: 0,
-  };
+	model.value = {
+		from: 0,
+		to: 0,
+	};
 }
 
 const isValueEmpty = () => {
-  return !!model?.value?.to || !!model?.value?.from;
+	return !!model?.value?.to || !!model?.value?.from;
 };
 
 const v$ = useVuelidate(
-  computed(() => ({
-    model: {
-      from: {
-        required: isValueEmpty,
-      },
-      to: {
-        required: isValueEmpty,
-      },
-    },
-  })),
-  { model },
-  { $autoDirty: true },
+	computed(() => ({
+		model: {
+			from: {
+				required: isValueEmpty,
+			},
+			to: {
+				required: isValueEmpty,
+			},
+		},
+	})),
+	{
+		model,
+	},
+	{
+		$autoDirty: true,
+	},
 );
 v$.value.$touch();
 
 const handleInput = (key: keyof ModelValue, value: number) => {
-  const newValue = { ...model.value };
-  newValue[key] = value;
-  model.value = newValue;
+	const newValue = {
+		...model.value,
+	};
+	newValue[key] = value;
+	model.value = newValue;
 };
 
 watch(
-  () => v$.value.$invalid,
-  (invalid) => {
-    emit('update:invalid', invalid);
-  },
-  { immediate: true },
+	() => v$.value.$invalid,
+	(invalid) => {
+		emit('update:invalid', invalid);
+	},
+	{
+		immediate: true,
+	},
 );
 </script>
 
