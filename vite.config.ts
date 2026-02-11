bb5875d5df5d54adf8f13ba61cab317eff600c39
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2026-02-11 15:36:02 +0200

    chore: rm localhost 8080 port hardcode in viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -10,99 +10,118 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default (/*{ mode }*/) => {
-  return defineConfig({
-    build: {
-      lib: {
-        // Could also be a dictionary or array of multiple entry points
-        entry: resolve(__dirname, 'src/install.ts'),
-        name: 'ui-sdk',
-        // the proper extensions will be added
-        fileName: 'ui-sdk',
-      },
-      rollupOptions: {
-        // make sure to externalize deps that shouldn't be bundled
-        // into your library
-        external: ['vue', 'primevue', '@aliasedDeps/api-services/axios', 'lodash-es', 'zod', 'clipboard-copy'],
-        output: {
-          // Provide global variables to use in the UMD build
-          // for externalized deps
-          globals: {
-            vue: 'Vue',
-            primevue: 'PrimeVue',
-          },
-        },
-      },
-    },
-    server: {
-      port: 8080,
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern-compiler', // or "modern-compiler", "legacy",
-        },
-      },
-    },
-    resolve: {
-      alias: {
-        // vue: '@vue/compat',
-      },
-    },
-    plugins: [
-      tailwindcss(),
-      vue({
-        template: {
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2,
-            },
-            isCustomElement: (tag) => tag.startsWith('media-'),
-          },
-        },
-      }),
-      // viteStaticCopy({
-      //   targets: [
-      //     {
-      //       src: 'src/assets/icons/sprite/',
-      //       dest: 'img',
-      //     },
-      //   ],
-      // }),
-      // https://www.npmjs.com/package/vite-plugin-node-polyfills
-      nodePolyfills({
-        // are needed for csv-parse
-        include: ['buffer', 'stream'],
-        globals: {
-          Buffer: true, // can also be 'build', 'dev', or false
-        },
-      }),
-      checker({
-        typescript: false,
-        vueTsc: false,
-        // biome: true,
-      }),
+	return defineConfig({
+		build: {
+			lib: {
+				// Could also be a dictionary or array of multiple entry points
+				entry: resolve(__dirname, 'src/install.ts'),
+				name: 'ui-sdk',
+				// the proper extensions will be added
+				fileName: 'ui-sdk',
+			},
+			rollupOptions: {
+				// make sure to externalize deps that shouldn't be bundled
+				// into your library
+				external: [
+					'vue',
+					'primevue',
+					'@aliasedDeps/api-services/axios',
+					'lodash-es',
+					'zod',
+					'clipboard-copy',
+				],
+				output: {
+					// Provide global variables to use in the UMD build
+					// for externalized deps
+					globals: {
+						vue: 'Vue',
+						primevue: 'PrimeVue',
+					},
+				},
+			},
+		},
+		server: {
+			// host: true,  // uncomment me to enable localhost access by IP (including from other devices in the network)
+		},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					api: 'modern-compiler', // or "modern-compiler", "legacy",
+				},
+			},
+		},
+		resolve: {
+			alias: {
+				// vue: '@vue/compat',
+			},
+		},
+		plugins: [
+			tailwindcss(),
+			vue({
+				template: {
+					compilerOptions: {
+						compatConfig: {
+							MODE: 2,
+						},
+						isCustomElement: (tag) => tag.startsWith('media-'),
+					},
+				},
+			}),
+			// viteStaticCopy({
+			//   targets: [
+			//     {
+			//       src: 'src/assets/icons/sprite/',
+			//       dest: 'img',
+			//     },
+			//   ],
+			// }),
+			// https://www.npmjs.com/package/vite-plugin-node-polyfills
+			nodePolyfills({
+				// are needed for csv-parse
+				include: [
+					'buffer',
+					'stream',
+				],
+				globals: {
+					Buffer: true, // can also be 'build', 'dev', or false
+				},
+			}),
+			checker({
+				typescript: false,
+				vueTsc: false,
+				// biome: true,
+			}),
 
-      AutoImport({
-        include: [...globbySync('*.{ts,js,vue}')],
-        imports: {
-          vue: [
-            'ref',
-            'reactive',
-            'computed',
-            'watch',
-            'watchEffect',
-            'onMounted',
-            'onUnmounted',
-          ],
-          'vue-i18n': ['useI18n'],
-          'vue-router': ['useRouter', 'useRoute'],
-        },
-      }),
-    ],
-    test: {
-      globals: true,
-      environment: 'happy-dom',
-      setupFiles: ['./tests/config/config.js'],
-    },
-  });
+			AutoImport({
+				include: [
+					...globbySync('*.{ts,js,vue}'),
+				],
+				imports: {
+					vue: [
+						'ref',
+						'reactive',
+						'computed',
+						'watch',
+						'watchEffect',
+						'onMounted',
+						'onUnmounted',
+					],
+					'vue-i18n': [
+						'useI18n',
+					],
+					'vue-router': [
+						'useRouter',
+						'useRoute',
+					],
+				},
+			}),
+		],
+		test: {
+			globals: true,
+			environment: 'happy-dom',
+			setupFiles: [
+				'./tests/config/config.js',
+			],
+		},
+	});
 };
