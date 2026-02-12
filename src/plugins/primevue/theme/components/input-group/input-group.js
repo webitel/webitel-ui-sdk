81207683eba8f6c8759ae306d2bcf411208034ce
Yehor Hlukhov <egorgluhov68@gmail.com>
2026-02-12 16:55:13 +0200

    feat: enhance input styles and transitions for input group components [WTEL-8795](https://webitel.atlassian.net/browse/WTEL-8795)

--- src/plugins/primevue/theme/components/input-group/input-group.js
+++ src/plugins/primevue/theme/components/input-group/input-group.js
@@ -1,13 +1,37 @@
 import { InputGroupScheme } from '@webitel/styleguide/component-schemes';
 
+/*  author @HlukhovYe
+    selectors are including inputtext itself where .p-inputtext is sibling 
+    and inputnumber where .p-inputtext is inside of the sibling block
+*/
+const addonStateStyle = (inputState, cssProperty, dtToken, dt) => `
+  .p-inputtext${inputState} ~ .p-inputgroupaddon,
+  *:has(.p-inputtext${inputState}) ~ .p-inputgroupaddon,
+  .p-inputgroupaddon:has(~ .p-inputtext${inputState}),
+  .p-inputgroupaddon:has(~ * .p-inputtext${inputState}) {
+    ${cssProperty}: ${dt(dtToken)};
+  }
+`;
+
 const inputGroup = {
 	...InputGroupScheme.sizes,
 	colorScheme: InputGroupScheme.colorScheme,
 
+	// transition is needed to copy input component transition value
 	css: ({ dt }) => `
-    .p-inputtext.p-invalid:enabled:hover {
-      border-color: ${dt('inputtext.invalidHoverBorderColor')};
+    .p-inputgroupaddon {
+      transition: background ${dt('inputtext.transition.duration')},
+                  color ${dt('inputtext.transition.duration')},
+                  border-color ${dt('inputtext.transition.duration')},
+                  outline-color ${dt('inputtext.transition.duration')},
+                  box-shadow ${dt('inputtext.transition.duration')};
     }
+
+    ${addonStateStyle('.p-invalid', 'border-color', 'inputtext.invalid.borderColor', dt)}
+    ${addonStateStyle('.p-invalid:enabled:hover', 'border-color', 'inputtext.invalidHoverBorderColor', dt)}
+    ${addonStateStyle(':enabled:hover', 'border-color', 'inputtext.hover.borderColor', dt)}
+    ${addonStateStyle(':enabled:focus', 'border-color', 'inputtext.focus.borderColor', dt)}
+    ${addonStateStyle(':disabled', 'background', 'inputtext.disabled.background', dt)}
   `,
 };
 
