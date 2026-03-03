64d538dd5c4d4d4099abbd631fbf84e889a6e650
liza-pohranichna <haemoglobin0@gmail.com>
2026-03-03 16:23:39 +0200

    feat: enhance chat message player with video support and media source handling [WTEL-8723] (https://webitel.atlassian.net/browse/WTEL-8723)

--- packages/ui-chats/src/ui/messaging/modules/message/composables/useChatMessageFile.ts
+++ packages/ui-chats/src/ui/messaging/modules/message/composables/useChatMessageFile.ts
@@ -1,6 +1,6 @@
-import { computed, type Ref, toRef } from "vue";
+import { computed, type Ref, toRef } from 'vue';
 
-import type { ChatMessageFile } from "../../../types/ChatMessage.types";
+import type { ChatMessageFile } from '../../../types/ChatMessage.types';
 
 export function useChatMessageFile(
 	file: ChatMessageFile | Ref<ChatMessageFile>,
@@ -11,20 +11,29 @@ export function useChatMessageFile(
 		return fileRef.value?.mime;
 	});
 
+	const src = computed(() => {
+		return fileRef.value?.url || fileRef.value?.streamUrl;
+	});
+
 	const image = computed(() => {
-		const isImage = type.value?.includes("image");
-		const isHEIC = type.value?.includes("heic");
+		const isImage = type.value?.includes('image');
+		const isHEIC = type.value?.includes('heic');
 
 		if (isHEIC) return null;
 
 		return isImage && fileRef.value; //https://webitel.atlassian.net/browse/WTEL-6268
 	});
-
 	const media = computed(() => {
+		console.log('type:', type.value);
 		const isMedia =
-			type.value?.includes("audio") || type.value?.includes("video");
+			isIncludeMediaType(src.value) || isIncludeMediaType(type.value);
+
 		return isMedia && fileRef.value;
 	});
+
+	const isIncludeMediaType = (value: string) => {
+		return !!(value?.includes('audio') || value?.includes('video'));
+	};
 
 	const document = computed(() => {
 		return !media.value && !image.value && fileRef.value;
