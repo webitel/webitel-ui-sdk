7fa52255c49aa145d0c628dfaf02964b9d6c71b7
dlohvinov <32868304+dlohvinov@users.noreply.github.com>
2026-03-12 12:09:59 +0000

    Apply automatic changes

---
+++ packages/ui-chats/types/ui/image-viewer/image-viewer.vue.d.ts
@@ -0,0 +1,11 @@
+import type { ChatMessageFile } from '../messaging/types/ChatMessage.types';
+type __VLS_Props = {
+    media: ChatMessageFile | null;
+};
+declare const __VLS_export: import("vue").DefineComponent<__VLS_Props, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {
+    close: () => any;
+}, string, import("vue").PublicProps, Readonly<__VLS_Props> & Readonly<{
+    onClose?: () => any;
+}>, {}, {}, {}, {}, string, import("vue").ComponentProvideOptions, false, {}, any>;
+declare const _default: typeof __VLS_export;
+export default _default;
