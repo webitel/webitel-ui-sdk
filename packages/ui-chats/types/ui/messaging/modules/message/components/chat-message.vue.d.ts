2ad9f7a9d0d842c46575b1d80310199cb6833432
Lera24 <77548827+Lera24@users.noreply.github.com>
2026-04-06 09:31:50 +0000

    Apply automatic changes

--- packages/ui-chats/types/ui/messaging/modules/message/components/chat-message.vue.d.ts
+++ packages/ui-chats/types/ui/messaging/modules/message/components/chat-message.vue.d.ts
@@ -1,15 +1,15 @@
-import type { ChatMessageType } from "../../../types/ChatMessage.types";
+import type { ChatMessageType } from '../../../types/ChatMessage.types';
 type __VLS_Props = {
     message: ChatMessageType;
     showAvatar?: boolean;
     withoutAvatars?: boolean;
     username?: string;
 };
-declare var __VLS_1: {}, __VLS_44: {};
+declare var __VLS_1: {}, __VLS_45: {};
 type __VLS_Slots = {} & {
     'before-message'?: (props: typeof __VLS_1) => any;
 } & {
-    'after-message'?: (props: typeof __VLS_44) => any;
+    'after-message'?: (props: typeof __VLS_45) => any;
 };
 declare const __VLS_base: import("vue").DefineComponent<__VLS_Props, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {} & {
     clickOnImage: () => any;
@@ -18,9 +18,9 @@ declare const __VLS_base: import("vue").DefineComponent<__VLS_Props, {}, {}, {},
     onClickOnImage?: () => any;
     onInitializedPlayer?: (player: object) => any;
 }>, {
-    username: string;
     showAvatar: boolean;
     withoutAvatars: boolean;
+    username: string;
 }, {}, {}, {}, string, import("vue").ComponentProvideOptions, false, {}, any>;
 declare const __VLS_export: __VLS_WithSlots<typeof __VLS_base, __VLS_Slots>;
 declare const _default: typeof __VLS_export;
