c7d138e28f489db0904ff52fcd90e5b60bd80628
Lera24 <77548827+Lera24@users.noreply.github.com>
2026-04-23 13:58:17 +0000

    Apply automatic changes

--- packages/ui-chats/types/ui/messaging/composables/useDropzoneHandlers.d.ts
+++ packages/ui-chats/types/ui/messaging/composables/useDropzoneHandlers.d.ts
@@ -1,8 +1,8 @@
 import { type Ref } from 'vue';
 interface UseDropzoneHandlers {
-	isDropzoneVisible: Ref<boolean>;
-	handleDragEnter: () => void;
-	handleDragLeave: () => void;
+    isDropzoneVisible: Ref<boolean>;
+    handleDragEnter: () => void;
+    handleDragLeave: () => void;
 }
 export declare const useDropzoneHandlers: () => UseDropzoneHandlers;
 export {};
