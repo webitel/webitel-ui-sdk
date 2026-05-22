a2d3c1b664110babb5a70684d4090f58df8330dc
Yehor Hlukhov <egorgluhov68@gmail.com>
2026-05-22 14:52:18 +0300

    chore: add comments to postcssIsolateStyles plugin for clarity on style isolation issue [WTEL-9627](https://webitel.atlassian.net/browse/WTEL-9627
    )

--- docs/postcss.config.mjs
+++ docs/postcss.config.mjs
@@ -3,6 +3,13 @@ import { postcssIsolateStyles } from 'vitepress';
 
 export default {
 	plugins: [
+		/* 
+      @author HlukhovYe
+
+      see: https://github.com/vuejs/vitepress/issues/4425
+      This plugin fixes the issue with the isolation of styles in the documentation,
+      because this styles have higher priority than other more specific styles
+    */
 		postcssIsolateStyles({
 			includeFiles: [
 				/vp-doc\.css/,
