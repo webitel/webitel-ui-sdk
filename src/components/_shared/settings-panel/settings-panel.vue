48db52239a7b3de49d6b219d4dc2677cd90218b2
Олександр Пальонний <s2567k@gmail.com>
2026-05-29 22:39:41 +0300

    fix: fixed settings-panel wt-popover behavior [WTEL-9163](https://webitel.atlassian.net/browse/WTEL-9163)
    (cherry picked from commit b3ca3ccf4bd3cda02ce0372f22aa07ad8c4fe6e1)

---
+++ src/components/_shared/settings-panel/settings-panel.vue
@@ -0,0 +1,64 @@
+<template>
+	<wt-popover class="settings-panel">
+		<template #activator="{ toggle }">
+			<media-button class="settings-panel__button" @click="toggle">
+				<wt-icon-btn
+					icon="plyr-settings"
+					:size="ComponentSize.SM"
+				/>
+			</media-button>
+		</template>
+
+		<speed-settings
+			:model-value="modelValue.playbackRate"
+			@update:model-value="handlePlaybackRateUpdate"
+		/>
+	</wt-popover>
+</template>
+
+<script setup lang="ts">
+import { ComponentSize } from '../../../enums';
+import WtPopover from '../../wt-popover/wt-popover.vue';
+import SpeedSettings from './components/speed-settings/speed-settings.vue';
+
+export interface MediaSettings {
+	playbackRate: number;
+}
+
+interface Props {
+	modelValue: MediaSettings;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	modelValue: () => ({
+		playbackRate: 1,
+	}),
+});
+
+const emit = defineEmits<{
+	'update:modelValue': [
+		value: MediaSettings,
+	];
+}>();
+
+function handlePlaybackRateUpdate(playbackRate: number) {
+	emit('update:modelValue', {
+		...props.modelValue,
+		playbackRate,
+	});
+}
+</script>
+
+<style scoped>
+.settings-panel {
+	//display: flex;
+  //height: 20px;
+}
+
+.settings-panel__button {
+	display: flex;
+	align-items: center;
+	cursor: pointer;
+  height: 20px;
+}
+</style>
