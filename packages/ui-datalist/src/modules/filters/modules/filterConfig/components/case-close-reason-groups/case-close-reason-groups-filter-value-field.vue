a9b373fa4e640d23968623b4c566c6e932caa39d
Yehor Hlukhov <egorgluhov68@gmail.com>
2026-06-08 17:36:07 +0300

    fix: correct import order for WtSingleSelect and WtMultiSelect in case-close-reason-groups-filter-value-field.vue

--- packages/ui-datalist/src/modules/filters/modules/filterConfig/components/case-close-reason-groups/case-close-reason-groups-filter-value-field.vue
+++ packages/ui-datalist/src/modules/filters/modules/filterConfig/components/case-close-reason-groups/case-close-reason-groups-filter-value-field.vue
@@ -29,7 +29,7 @@
 <script lang="ts" setup>
 import { useVuelidate } from '@vuelidate/core';
 import { required } from '@vuelidate/validators';
-import { WtSingleSelect, WtMultiSelect } from '@webitel/ui-sdk/components';
+import { WtMultiSelect, WtSingleSelect } from '@webitel/ui-sdk/components';
 import { computed, onMounted, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 
