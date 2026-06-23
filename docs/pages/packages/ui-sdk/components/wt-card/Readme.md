e5f31fab037363ec709a01ef11a46f55b120dd09
Roman Zaritskyi <r.zaritskyi@steel.mil.ua>
2026-06-23 12:00:18 +0300

    chore: add wt-card to docs (https://webitel.atlassian.net/browse/WTEL-9735)[WTEL-9735]

---
+++ docs/pages/packages/ui-sdk/components/wt-card/Readme.md
@@ -0,0 +1,20 @@
+<script setup>
+import Specs from './component-specs.vue';
+import ExampleCard from './examples/example-card.vue';
+</script>
+
+# `wt-card.vue`
+
+## Specs
+
+<Specs />
+
+## Example Card
+
+::: raw
+<ExampleCard />
+:::
+
+::: details Code
+<<< ./examples/example-card.vue
+:::
---
+++ docs/pages/packages/ui-sdk/components/wt-card/examples/example-card.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <wt-card>
+    <wt-headline>
+      <template #title> Card title </template>
+    </wt-headline>
+    <p>
+      Card content goes here. The card provides border radius, padding and
+      elevation around its default slot.
+    </p>
+  </wt-card>
+</template>
+
+<style scoped lang="scss"></style>
