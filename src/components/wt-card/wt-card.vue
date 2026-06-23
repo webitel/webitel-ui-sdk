cc7ae1acf553ac8639a27708ff2c1f4923105191
Roman Zaritskyi <r.zaritskyi@steel.mil.ua>
2026-06-23 17:30:39 +0300

    fix: remove unused code (https://webitel.atlassian.net/browse/WTEL-9735)[WTEL-9735]

--- src/components/wt-card/wt-card.vue
+++ src/components/wt-card/wt-card.vue
@@ -4,15 +4,7 @@
   </div>
 </template>
 
-<script setup lang="ts">
-defineOptions({
-	name: 'WtCard',
-});
-</script>
-
 <style lang="scss" scoped>
-@use '../../css/main' as *;
-
 .wt-card {
   border-radius: var(--wt-card-border-radius);
   padding: var(--wt-card-padding);
