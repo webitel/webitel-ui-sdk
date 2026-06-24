70e0307e9a4f445a349f02f94aa72c945d21fbe4
Yehor Hlukhov <egorgluhov68@gmail.com>
2026-06-24 15:54:01 +0300

    fix: adding custom values in select logic [WTEL-9826](https://webitel.atlassian.net/browse/WTEL-9826)

--- src/components/_internals/composables/useSelect/useSelectCustomValues.ts
+++ src/components/_internals/composables/useSelect/useSelectCustomValues.ts
@@ -28,9 +28,9 @@ export const useSelectCustomValues = ({
 				]),
 			);
 		}
-		if (optionValue.value) {
+		if (dataKey.value) {
 			return {
-				[optionValue.value]: text,
+				[dataKey.value]: text,
 			};
 		}
 		return text;
