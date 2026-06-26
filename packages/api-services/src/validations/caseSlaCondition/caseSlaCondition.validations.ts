b7d98e1d4c1ea95d69b2363968bc705d236f350f
Lera24 <bedulinavalera21@gmail.com>
2026-06-26 15:23:22 +0300

    feature: add slaConditions validations[WTEL-9717](https://webitel.atlassian.net/browse/WTEL-9717)

--- packages/api-services/src/validations/caseSlaCondition/caseSlaCondition.validations.ts
+++ packages/api-services/src/validations/caseSlaCondition/caseSlaCondition.validations.ts
@@ -1,9 +1,19 @@
 import type { WebitelCasesSLACondition } from '@webitel/api-services/gen/models';
 import { z } from 'zod';
 import type { ZodShape } from '../types';
+import { requiredLookupSchema } from '../_shared/lookup.validations';
 
 export const caseSLAConditionSchema = z.object<
 	ZodShape<WebitelCasesSLACondition>
 >({
 	name: z.string().min(1),
+	priorities: z.array(requiredLookupSchema).min(1),
+	reactionTime: z.union([
+		z.string().min(1),
+		z.number().min(1),
+	]),
+	resolutionTime: z.union([
+		z.string().min(1),
+		z.number().min(1),
+	]),
 });
