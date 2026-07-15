0ec16bfb926f53fbdd3a0926c7b5cdbf9c6e9441
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2026-07-15 15:04:50 +0300

    feat(login form): Fixed lint issues ([WTEL-9938](https://webitel.atlassian.net/browse/WTEL-9938))

--- src/enums/WebitelApplications/WfmSections.d.ts
+++ src/enums/WebitelApplications/WfmSections.d.ts
@@ -1,6 +1,6 @@
 export declare const WfmSections: {
-    readonly MySchedules: "my-schedules";
-    readonly Agents: "agents";
-    readonly Schedules: "schedules";
+	readonly MySchedules: 'my-schedules';
+	readonly Agents: 'agents';
+	readonly Schedules: 'schedules';
 };
 export type WfmSections = (typeof WfmSections)[keyof typeof WfmSections];
