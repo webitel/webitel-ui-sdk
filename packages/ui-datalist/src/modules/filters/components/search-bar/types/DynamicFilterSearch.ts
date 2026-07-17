0416a6c856b25939bf2e9b036eff5b6131f45267
dlohvinov <32868304+dlohvinov@users.noreply.github.com>
2026-07-17 17:12:02 +0000

    ci: publish ui-datalist

--- packages/ui-datalist/src/modules/filters/components/search-bar/types/DynamicFilterSearch.ts
+++ packages/ui-datalist/src/modules/filters/components/search-bar/types/DynamicFilterSearch.ts
@@ -1,5 +1,5 @@
 import type { WtSearchBarSearchModeOption } from '@webitel/ui-sdk/components/wt-search-bar/types/WtSearchBar';
-import { FilterName } from '../../../classes/Filter';
+import type { FilterName } from '../../../classes/Filter';
 
 export type DynamicFilterSearchSearchModeOption =
 	WtSearchBarSearchModeOption<FilterName>;
