afb29eda9e666af299a80e500f09eb557112e543
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2026-08-06 14:26:53 +0300

    feat(Status type "online"): Renamed shared file  ([WTEL-10039](https://webitel.atlassian.net/browse/WTEL-10039))

--- packages/api-services/src/validations/_shared/skill.validations.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const skillSchema = z.object({
-	id: z.string().optional(),
-	name: z.string().optional(),
-});
--- packages/api-services/src/validations/onlineSkill/onlineSkill.validations.ts
+++ packages/api-services/src/validations/onlineSkill/onlineSkill.validations.ts
@@ -1,10 +1,10 @@
 import type { EngineOnlineSkills } from '@webitel/api-services/gen/models';
 import { z } from 'zod';
 import type { ZodShape } from '../types';
-import { skillSchema } from '../_shared/skill.validations';
+import { lookupSchema } from '../_shared/lookup.validations';
 
 export const onlineSkillSchema = z.object<ZodShape<EngineOnlineSkills>>({
 	name: z.string().min(1),
 	description: z.string().optional(),
-	skills: z.array(skillSchema).optional(),
+	skills: z.array(lookupSchema).optional(),
 });
