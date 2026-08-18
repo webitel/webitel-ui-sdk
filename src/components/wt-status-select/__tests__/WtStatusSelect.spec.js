1beda16f0e5a1f04128b35f09684dc1c5de544ec
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2026-08-18 17:51:12 +0300

    feat(online type): Added tests ([WTEL-10169](https://webitel.atlassian.net/browse/WTEL-10169))

--- src/components/wt-status-select/__tests__/WtStatusSelect.spec.js
+++ src/components/wt-status-select/__tests__/WtStatusSelect.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils';
 
+import AgentStatus from '../../../enums/AgentStatus/AgentStatus.enum.js';
 import WtSingleSelect from '../../wt-single-select/wt-single-select.vue';
 import WtStatusSelect from '../wt-status-select.vue';
 
@@ -11,6 +12,70 @@ describe('WtStatusSelect', () => {
 			},
 		});
 		expect(wrapper.classes('wt-status-select')).toBe(true);
+	});
+
+	it('keeps "online" option in the list when it is the current status', () => {
+		const wrapper = shallowMount(WtStatusSelect, {
+			props: {
+				status: AgentStatus.ONLINE,
+			},
+			stubs: {
+				WtSingleSelect,
+			},
+		});
+		expect(
+			wrapper.vm.availableOptions.some(
+				(opt) => opt.value === AgentStatus.ONLINE,
+			),
+		).toBe(true);
+	});
+
+	it('keeps "pause" option in the list when it is the current status', () => {
+		const wrapper = shallowMount(WtStatusSelect, {
+			props: {
+				status: AgentStatus.PAUSE,
+			},
+			stubs: {
+				WtSingleSelect,
+			},
+		});
+		expect(
+			wrapper.vm.availableOptions.some(
+				(opt) => opt.value === AgentStatus.PAUSE,
+			),
+		).toBe(true);
+	});
+
+	it('removes "offline" option from the list when it is the current status', () => {
+		const wrapper = shallowMount(WtStatusSelect, {
+			props: {
+				status: AgentStatus.OFFLINE,
+			},
+			stubs: {
+				WtSingleSelect,
+			},
+		});
+		expect(
+			wrapper.vm.availableOptions.some(
+				(opt) => opt.value === AgentStatus.OFFLINE,
+			),
+		).toBe(false);
+	});
+
+	it('always removes "break_out" option from the list', () => {
+		const wrapper = shallowMount(WtStatusSelect, {
+			props: {
+				status: AgentStatus.BREAK_OUT,
+			},
+			stubs: {
+				WtSingleSelect,
+			},
+		});
+		expect(
+			wrapper.vm.availableOptions.some(
+				(opt) => opt.value === AgentStatus.BREAK_OUT,
+			),
+		).toBe(false);
 	});
 
 	it('correctly represents duration', () => {
