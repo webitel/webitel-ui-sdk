72613f24a5d36ed3ec9a58dd46dddd4f46c5931f
Олександр Пальонний <s2567k@gmail.com>
2026-08-20 22:16:39 +0300

    fix: fixed lint-root check [WTEL-10196](https://webitel.atlassian.net/browse/WTEL-10196)

--- src/components/on-demand/wt-type-extension-value-input/wt-type-extension-value-input.vue
+++ src/components/on-demand/wt-type-extension-value-input/wt-type-extension-value-input.vue
@@ -111,7 +111,7 @@ const props = defineProps<{
 
 const { t } = useI18n();
 
-const label = computed(() => {
+const resolvedLabel = computed(() => {
 	return props.label || t(props.field?.name || 'vocabulary.labels');
 });
 
@@ -125,7 +125,7 @@ const value = computed<any>(() => {
 });
 
 const sharedChildrenProps = computed(() => ({
-	label: label.value,
+	label: resolvedLabel.value,
 	required: isRequired.value,
 	v: props.v,
 }));
