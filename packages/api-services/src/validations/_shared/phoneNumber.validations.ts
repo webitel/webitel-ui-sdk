c9c0bb75cfab813b27f992360149f296dd29e23b
Roman Zaritskyi <r.zaritskyi@steel.mil.ua>
2026-09-15 14:52:00 +0300

    test(api-services): cover phoneNumberSchema charset and empty value [WTEL-10374](https://webitel.atlassian.net/browse/WTEL-10374)

--- packages/api-services/src/validations/_shared/phoneNumber.validations.ts
+++ packages/api-services/src/validations/_shared/phoneNumber.validations.ts
@@ -22,6 +22,6 @@ export const phoneNumberSchema = z
 	.string()
 	.min(1)
 	.refine(
-		(value) => phoneNumberPattern.test(value),
+		(value) => value === '' || phoneNumberPattern.test(value),
 		i18nIssue('phoneNumberSymbolsValidator'),
 	);
