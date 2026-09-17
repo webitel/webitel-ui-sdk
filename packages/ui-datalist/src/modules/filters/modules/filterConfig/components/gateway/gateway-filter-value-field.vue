a6982fa94cd6334fdb7937fba4f5ab71fb9bfb0a
HlukhovYe <76591663+HlukhovYe@users.noreply.github.com>
2026-09-17 15:11:12 +0300

    Update packages/ui-datalist/src/modules/filters/modules/filterConfig/components/gateway/gateway-filter-value-field.vue
    Co-authored-by: webitel-review[bot] <318974087+webitel-review[bot]@users.noreply.github.com>

--- packages/ui-datalist/src/modules/filters/modules/filterConfig/components/gateway/gateway-filter-value-field.vue
+++ packages/ui-datalist/src/modules/filters/modules/filterConfig/components/gateway/gateway-filter-value-field.vue
@@ -57,7 +57,7 @@ const v$ = useVuelidate(
 );
 
 onMounted(() => {
-	if (!props?.disableValidation) v$.value.$touch();
+	if (!props.disableValidation) v$.value.$touch();
 });
 
 const searchGateway = async (params: { search?: string }) => {
